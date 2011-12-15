fd14ab36fdb1c7652f6b2d7b8e209e159ab821de
Rob Taylor <robt@hoopassociates.co.uk>
2011-12-15 17:43:21 +0000

    Fixed loading of client name from config

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+$docroot = explode('/', getcwd());
+$instance = explode('.', $docroot[count($docroot)-1]);
+
+return array(
+	'client_name'		=> 'Hoop Associates',
+	'environment'		=> 'dev',
+	'sledge_name'		=> $instance[0],
+	'sledge_instance'	=> $instance[1]
+);
+
+?>
