91a9b72089f10f0fb1f2f127bc4ad7d58cff81ea
Rob Taylor <robt@hoopassociates.co.uk>
2011-12-16 19:30:16 +0000

    Created more new chunk models, deleted the old ones

--- classes/model/chunk/tag.php
+++ classes/model/chunk/tag.php
@@ -1,18 +1,18 @@
 <?php
 
 /**
-* Text chunk model
+* Tag chunk model
 * @package Models
 * @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
 * @copyright 2011, Hoop Associates
 *
 */
-class Model_Chunk_Text extends ORM implements iSLot
+class Model_Chunk_Tag extends ORM implements iSLot
 {
 	/**
 	* Properties to create relationships with Kohana's ORM
 	*/
-	protected $_table_name = 'chunk_text';
+	protected $_table_name = 'chunk_tag';
 	
 }
 
