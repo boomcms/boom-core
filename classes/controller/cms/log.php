365c11f697114e755909863d7f59926a4ea23f89
Rob Taylor <robt@hoopassociates.co.uk>
2011-12-16 11:27:35 +0000

    Tidied up the activity log viewer

---
+++ classes/controller/cms/log.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+* Controller for displaying CMS logs such as the activity log.
+*
+* @package Sledge
+* @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
+* @copyright 2011, Hoop Associates Ltd
+*/
+class Controller_Cms_Log extends Cms_Controller_Template
+{
+	
+	/**
+	* Handles displaying the activity log.
+	*
+	*/
+	public function action_activitylog()
+	{
+		$last24hours = ORM::factory( 'activitylog' )->where( 'timestamp', '>=', time() - 86400 )->orderby( 'timestamp', 'desc' )->find_all();
+		$last50 = ORM::factory( 'activitylog' )->orderby( 'timestamp', 'desc' )->limit( 50 )->find_all();
+		
+		$this->template->subtpl_main = View::factory( 'cms/templates/tpl_activitylog' );
+		$this->template->subtpl_main->last24hours = $last24hours;
+		$this->template->subtpl_main->last50 = $last50;		
+	}
+}
+
+
+?>
