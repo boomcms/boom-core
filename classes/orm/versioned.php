1c2bfff04b48c1eadaa08ca52423c283c93b5128
Rob Taylor <robt@hoopassociates.co.uk>
2011-12-17 12:48:50 +0000

    Created a version ORM so that we can work with versioned tables transparently

---
+++ classes/orm/versioned.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+* @package ORM
+* @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
+* @copyright 2011, Hoop Associates
+*/
+
+abstract class ORM_Versioned extends Sledge_ORM_Versioned {}
+	
+?>
