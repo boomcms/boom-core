42f76060ca2a0c51767348236f715ce6282cfccb
Rob Taylor <robt@hoopassociates.co.uk>
2011-12-18 20:59:20 +0000

    Added methods to iSlot interface

--- classes/islot.php
+++ classes/islot.php
@@ -1,16 +1,19 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
-* Showable interface.
-* Enforces implementation of a show() method in slot classes.
+* iSlot interface
+* Ensures that all slot classes (models and cms/site decorators) implement the same methods for retrieving slot details.
 * @package Slots
 * @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
 * @copyright 2011, Hoop Associates
 */
-
 interface iSlot
 {
+
 	public function show();
+	public function getSlotname();
+	public function getTitle();
+	public function getPage();
 }
 
 ?>
