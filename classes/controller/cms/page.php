be943c8bb320705e5e21648a1986f071a457ac7e
Rob Taylor <robt@hoopassociates.co.uk>
2011-12-28 23:54:07 +0000

    Added delete method to cms page controller

--- classes/controller/cms/page.php
+++ classes/controller/cms/page.php
@@ -9,8 +9,40 @@
 */
 class Controller_Cms_Page extends Controller_Cms
 {
+	/**
+	* Object representing the current page.
+	* 
+	* @var object
+	* @access private
+	*/
+	private $_page;
+	
+	/**
+	* Load the current page.
+	* All of these methods should be called with a page ID in the params
+	* Before the methods are called we find the page so it can be used, clever eh?
+	*
+	* @return void
+	*/	
+	public function before()
+	{
+		parent::before();
+		
+		$page_id = $this->request->param( 'id' );
+		$page_id = (int) preg_replace( "/[^0-9]+/", "", $page_id );
+		$this->_page = ORM::factory( 'page', $page_id );
+	
+		if (!$this->_page->loaded())
+		{
+			// Do something.
+			exit();
+		}
+	}
+	
 	public function action_add()
 	{
+		$parent = $this->_page;
+		
 		// Find the URI we were called with.
 		$uri = $this->request->initial()->uri();
 		
@@ -29,8 +61,8 @@ class Controller_Cms_Page extends Controller_Cms
 	
 	public function action_save()
 	{
-	die('3');
-		$page = ORM::factory( 'page', $page_id );
+		$page = $this->_page;
+		
 		$page->version->template_rid = $template_rid;
 		$page->version->default_child_template_rid = $default_child_template_rid;
 		$page->version->prompt_for_child_template = $prompt_for_child_template;
@@ -41,16 +73,8 @@ class Controller_Cms_Page extends Controller_Cms
 	}
 	
 	public function action_clone()
-	{
-		$page_id = $this->request->param( 'id' );
-		$page_id = (int) preg_replace( "/[^0-9]+/", "", $page_id );
-		$page = ORM::factory( 'page', $page_id );
-		
-		if (!$page->loaded())
-		{
-			// Do something.
-			exit();
-		}
+	{		
+		$oldpage = $this->_page;
 
 		// Copy the versioned column values.
 		$newpage = ORM::factory( 'page' );
@@ -62,13 +86,21 @@ class Controller_Cms_Page extends Controller_Cms
 				$newpage->version->$column = $page->$column;			
 			}
 		}
-		
-		
 	}
 	
-	public function action_index()
+	/**
+	* Delete page controller.
+	*/
+	public function action_delete()
 	{
-		die( 'cms page index' );
+		// Call Model_Page::delete().
+		// Sets the version deleted column to true for this page and it's children.
+		// Also deletes the pages from the MPTT tree.
+		// Automatically calls Model_Page::save() to save changes.
+		$this->_page->delete();
+		
+		// Go back to the homepage.
+		Request::factory( '/' )->execute();
 	}
 }
 
