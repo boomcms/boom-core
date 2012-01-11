1b64024e8ac0dcc632b3b40d67909ab675ad4003
Rob Taylor <robt@hoopassociates.co.uk>
2012-01-11 15:21:24 +0000

    Removed a load of views which appear to have been replaced by the files in the ui directory

--- views/site/subtpl_quicklinks.php
+++
@@ -1,5 +0,0 @@
-<?php
-	# Copyright 2009, Hoop Associates Ltd
-	# Hoop Associates   www.thisishoop.com   mail@hoopassociates.co.uk
-?>
-<?= O::f('chunk_linkset_v')->get_chunk($this->page->id,'quicklinks','quicklinks');?>
