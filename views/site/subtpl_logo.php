77c7444d5180a0080be0a8c9bb029a349221a613
Rob Taylor <robt@hoopassociates.co.uk>
2012-01-12 16:39:53 +0000

    Removed some pointless looking templates

--- views/site/subtpl_logo.php
+++ views/site/subtpl_logo.php
@@ -1,5 +1,5 @@
 <h1 id="logo">
-	<a title="<?=Kohana::$config->load('config' )->get('client_name')?> home" href="/">
+	<a title="<?=Kohana::$config->load('config' )->get('client_name')?> Home" href="/">
 		<? $page->get_slot( 'asset', 'logo' ); ?>
 	</a>
 </h1>
