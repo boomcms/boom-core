83f9de90c9c40d859b8dd6041100ddccb7acdaaa
Rob Taylor <robt@hoopassociates.co.uk>
2012-01-13 11:58:49 +0000

    Changed some variable names in ui sub templates

--- views/ui/subtpl_tag_appliedto_thumb_row_asset.php
+++ views/ui/subtpl_tag_appliedto_thumb_row_asset.php
@@ -1,9 +1,9 @@
 <div class="thumb ui-state-active ui-corner-all">
 
-	<input type="checkbox" class="sledge-tagmanager-select-checkbox ui-helper-reset" id="asset-thumb-<?=$item->rid?>" />
+	<input type="checkbox" class="sledge-tagmanager-select-checkbox ui-helper-reset" id="asset-thumb-<?=$item->id?>" />
 
 	<a href="#asset/<?=$item->rid?>">
-		<img src="/_ajax/call/asset/get_asset/<?=$item->rid?>/100/100/85/1" />
+		<img src="/asset/view/<?=$item->id?>/100/100/85/1" />
 		<span class="caption"><?=$item->title?></span>
 		<span class="caption-overlay"></span>
 	</a>
