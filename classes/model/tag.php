4651def8cf02edb40ddb513afbd7924695cf157e
Rob Taylor <robt@hoopassociates.co.uk>
2012-01-20 12:32:05 +0000

    Added tag find or create method

--- classes/model/tag.php
+++ classes/model/tag.php
@@ -22,7 +22,31 @@ class Model_Tag extends ORM_Versioned {
 	);
 	
 	protected $_load_with = array('version');
+	
+	/**
+	* Finds the tag with a given name, creates the tag if it doesn't exist.
+	*
+	* @param string Tag name
+	* @return Model_Tag A tag with the given name.
+	*/
+	public function find_or_create( $name )
+	{
+		// Make sure we're not screwing around with an object loaded from the database.
+		if ($this->loaded())
+		{
+			$this->clear();
+		}
+		
+		$this->where( 'version.name', '=', $name )->find();
+		
+		if (!$this->loaded())
+		{
+			$this->name = $name;
+			$this->save();
+		}
+		
+		return $this;
+	}
 }
-
 
 ?>
