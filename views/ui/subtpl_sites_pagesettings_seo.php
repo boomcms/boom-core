bbf3c49ba227df55009cc68d9652e860a69eb2d2
Rob Taylor <robt@hoopassociates.co.uk>
2012-01-24 17:33:54 +0000

    Added permissions check to seo template

--- views/ui/subtpl_sites_pagesettings_seo.php
+++ views/ui/subtpl_sites_pagesettings_seo.php
@@ -7,7 +7,8 @@
 
 		<div id="sledge-pagesettings-seo-basic">
 			<table width="100%">
-				<?//if ($p['current_version']['Can edit page description']){?>
+				
+				<?if ($person->can( 'view', $page, 'description' )): ?>
 					<tr>
 						<td style="vertical-align:top">
 							<label for="description" class="ui-helper-clearfix">
@@ -17,10 +18,20 @@
 								<span class="ui-icon ui-helper-left ui-icon-help sledge-tooltip" title="A description of the description field."></span>
 							</label>
 						</td>
-						<td><textarea id="description" name="description" class="sledge-textarea"><?=$page->get_description() ?></textarea></td>
+						<td>
+							<? if ($person->can( 'edit', $page, 'description' )): ?>
+								<textarea id="description" name="description" class="sledge-textarea"><?=$page->get_description() ?>
+								</textarea>
+							<? 
+								else:
+									echo $page->get_description();
+								endif;
+							?>
+						</td>
 					</tr>
-				<?//}?>	
-		        <?//if ($p['current_version']['Can edit page keywords']){?>
+				<? endif; ?>	
+		        
+				<?if ($person->can( 'view', $page, 'keywords' )): ?>
 					<tr>
 						<td style="vertical-align:top">
 							<label for="keywords" class="ui-helper-clearfix">
@@ -30,84 +41,59 @@
 								<span class="ui-icon ui-icon-help ui-helper-left sledge-tooltip" title="Keywords description: please separate your keywords with a comma."></span>
 							</label>
 						</td>
-						<td><textarea name="keywords" id="keywords" class="sledge-textarea"><?=$page->get_keywords() ?></textarea></td>
+						<td>
+							<? if ($person->can( 'edit', $page, 'keywords' )): ?>
+								<textarea name="keywords" id="keywords" class="sledge-textarea"><?=$page->get_keywords() ?>
+								</textarea>
+							<?
+								else:
+									echo $page->get_keywords();
+								endif;
+							?>
+						</td>
 					</tr>
-				<?//}?>
+				<? endif; ?>
 			</table>
 		</div>
 
 		<div id="sledge-pagesettings-seo-advanced">
 			<table width="100%">
-				<?//if ($p['attributes']['Can edit page indexing']){?>
+				<? if ($person->can( 'view', $page, 'indexed' )): ?>
 					<tr>
 						<td>Allow page indexing</td>
 						<td>
-							<select name="indexed">
-								<option <?if ($page->indexed) echo "selected='selected' "; echo "value='1'>Yes</option>"; ?>
-								<option <?if (!$page->indexed) echo "selected='selected' "; echo "value='0'>No</option>"; ?>
-							</select>
+							<? if ($person->can( 'edit', $page, 'indexed' )): ?>
+								<select name="indexed">
+									<option <?if ($page->indexed) echo "selected='selected' "; echo "value='1'>Yes</option>"; ?>
+									<option <?if (!$page->indexed) echo "selected='selected' "; echo "value='0'>No</option>"; ?>
+								</select>
+							<?
+								else:
+									echo ($page->indexed)? 'Yes' : 'No';
+								endif;
+							?>
 						</td>
 					</tr>
-				<?//}?>
-				<?/*if ($p['attributes']['Can edit page sitemap priority']){?>
-					<tr>
-						<td>Sitemap priority</td>
-						<td>
-							<select name="sitemap_priority">
-								<?if ($page->rid == O::f('site_page')->get_homepage()->rid) {
-									$default_priority = Kohana::config('core.sitemap_default_priority');
-									if (!$default_priority) $default_priority = 0.5;?>
-									<option value="">Use default (<?=$default_priority?>)</option>
-								<?}else{?>
-									<option value="">Inherit from my parent</option>
-								<?}?>
-								<?for ($i=0.0; $i<=1.0; $i+=0.1) {?>
-									<option <?if ($page->sitemap_priority === $i) {?>selected="selected" <?}?> value="<?=$i?>"><?=$i?></option>
-								<?}?>
-							</select>
-							0.0 is lowest, 1.0 is highest
-						</td>
-					</tr>
-				<?}*/?>
-				<?/*if ($p['attributes']['Can edit page sitemap update frequency']){?>
-					<tr>
-						<td>Sitemap update frequency</td>
-						<td>
-							<select name="sitemap_update_frequency">
-								<?if ($page->rid == O::f('site_page')->get_homepage()->rid) {
-									$default_update_frequency = Kohana::config('core.sitemap_default_update_frequency');
-									if (!$default_update_frequency) $default_update_frequency = 'daily';?>
-									<option value="">Use default (<?=$default_update_frequency?>)</option>
-								<?}else{?>
-									<option value="">Inherit from my parent</option>
-								<?}?>
-								<?foreach (array('never','yearly','monthly','weekly','daily','hourly','always') as $period) {?>
-									<option <?if ($page->sitemap_update_frequency === $period) {?>selected="selected" <?}?> value="<?=$period?>"><?=$period?></option>
-								<?}?>
-							</select>
-						</td>
-					</tr>
-				<?}*/?>
-				<?//if ($p['attributes']['Can edit page hidden from search results']){?>
+				<? endif; ?>
+			
+				<?if ($person->can( 'view', $page, 'hidden_from_search_results' )): ?>
 					<tr>
 						<td>Hidden from search results</td>
 						<td>
-							<select name="hidden_from_search_results">
-								<option value="0">No</option>
-								<option value="1"<?if ($page->hidden_from_search_results):?> selected="selected"<? endif; ?>>Yes</option>
-							</select>
+							<? if ($person->can( 'edit', $page, 'hidden_from_search_results' )): ?>
+								<select name="hidden_from_search_results">
+									<option value="0">No</option>
+									<option value="1"<?if ($page->hidden_from_search_results):?> selected="selected"<? endif; ?>>Yes</option>
+								</select>
+							<?
+								else:
+									echo ($page->hidden_from_search_results)? 'Yes' : 'No';
+								endif;
+							?>
 						</td>
 					</tr>
-				<?/*}else if ($p['attributes']['Can view page hidden from search results']){?>
-					<tr>
-						<td>Hidden from search results</td>
-						<td>
-							<?=($page->hidden_from_search_results == 'y') ? 'Yes' : 'No'?>
-						</td>
-					</tr>
-				<?}*/?>
+				<? endif; ?>
 			</table>
 		</div>
 	</div>
-	<?//= $hidden_inputs; ?>
 </form>
