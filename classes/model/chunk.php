57d33d7865a2cf19dd722fc24a0cfd63b42c3dc6
Rob Taylor <robt@hoopassociates.co.uk>
2012-02-08 16:38:27 +0000

    Added comments to chunk and chunk_text models

--- classes/model/chunk.php
+++ classes/model/chunk.php
@@ -1,10 +1,23 @@
 <?php
-
 /**
-* Chunk model
+* Model for the chunk table.
+* The chunk table is only useful with one of the related tables (chunk_text for example for a text chunk).
+* This class has a custom constructor which takes an extra argument for the slot type and creates the table join.
+*
+* Table name: chunk
+*
+*************************** Table Columns ************************
+****	Name			****	Data Type	****	Description					
+****	id				****	integer		****	Primary key, auto increment
+****	slotname		****	string		****	Name of the slot that the chunk belongs to.
+****	active_vid		****	integer		****	The ID of the related chunk table.
+****	type			****	string		****	The type of chunk (text, feature, etc.)
+******************************************************************
+*
 * @package Models
 * @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
 * @copyright 2011, Hoop Associates
+*
 */
 class Model_Chunk extends ORM
 {
@@ -12,7 +25,6 @@ class Model_Chunk extends ORM
 	* Properties to create relationships with Kohana's ORM
 	*/
 	protected $_table_name = 'chunk';
-	protected $_belongs_to = array( 'chunk_text' => array( 'foreign_key' => 'id' ) );		
 	
 	/**
 	* Custom constructor for the slots tables.
--- classes/model/chunk/text.php
+++ classes/model/chunk/text.php
@@ -1,13 +1,24 @@
 <?php
-
 /**
-* Text chunk model
+* Model for the text chunk table.
+*
+* Table name: chunk_text
+*
+*************************** Table Columns ************************
+****	Name			****	Data Type	****	Description					
+****	id				****	integer		****	Primary key, auto increment
+****	text			****	string		****	The content of the text chunk.
+****	audit_person	****	integer		****	ID of the person who edited the chunk.
+****	audit_time		****	integer		****	Unix timestamp of when the chunk was edited.
+****	deleted			****	boolean		****	Whether the chunk has been deleted.
+******************************************************************
+*
 * @package Models
 * @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
 * @copyright 2011, Hoop Associates
 *
 */
-class Model_Chunk_Text extends ORM implements Interface_SLot
+class Model_Chunk_Text extends ORM implements Interface_Slot
 {
 	/**
 	* Properties to create relationships with Kohana's ORM
