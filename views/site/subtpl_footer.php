64bd94e8f9a7f64ff39f8090080b17a18c7bb929
Rob Taylor <robt@hoopassociates.co.uk>
2012-02-20 20:16:09 +0000

    Added header and footer subtemplates back to sledge module

---
+++ views/site/subtpl_footer.php
@@ -0,0 +1,14 @@
+<?php
+	# Copyright 2009, Hoop Associates Ltd
+	# Hoop Associates   www.thisishoop.com   mail@hoopassociates.co.uk
+?>
+<div id="footer">
+
+<?
+	/*foreach ( $footer_pages as $footer_page )
+	{
+		echo "<span><a href='" . $p->uri() . "'>" . $p->version->title . "</a></span>";
+	}*/
+?>
+
+</div>
