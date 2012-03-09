c09f0231f15b954ab360a513785f6c9f7e6d7f22
Rob Taylor <robt@hoopassociates.co.uk>
2012-03-09 17:41:24 +0000

    Finished making classes compatible with Kohana cascading file system

--- classes/permission/cms.php
+++ classes/permission/cms.php
@@ -1,32 +1,5 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
-/**
-*
-* @package Permissions
-* @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
-* @copyright 2011, Hoop Associates
-* 
-*/
-class Permission_Cms extends Permission
-{
-	public function __construct( Model_Person $person )
-	{
-		parent::__construct( $person );
-		
-		$this->_query->join( 'actions', 'inner' )
-					->on( 'permissions.action_id', '=', 'actions.id' );
-	}
-	/**
-	* Determines whether the person can do the desired CMS action.
-	* In this case $what is ignored - we just check whether they have access to the desired action
-	*/
-	public function can( $action )
-	{
-		$this->_query->where( 'actions.name', '=', $action );
-		$this->_result = $this->_query->execute();	
-			
-		return (bool) $this->_result->get( 'permission' );
-	}
-}
+class Permission_Cms extends Sledge_Permission_Cms {}
 
 ?>
--- classes/permission/page/property.php
+++ classes/permission/page/property.php
@@ -1,25 +1,5 @@
-<?php
+<?php defined('SYSPATH') or die('No direct script access.');
 
-/**
-*
-* @package Permissions
-* @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
-* @copyright 2011, Hoop Associates
-* 
-*/
-class Permission_Page_Property extends Permission_Page
-{
-	protected function __construct( Model_Person $person, $page, $property )
-	{
-		parent::__construct( $person, $page );
-		
-		$this->_query->join( 'actions', 'inner' )
-					->on( 'permissions.action_id', '=', 'actions.id' )
-					->or_where_open()
-					->or_where( 'actions.name', '=', "page $property" )
-					->or_where( 'actions.name', '=', 'page all' )
-					->or_where_close();
-	}
-}
+class Permission_Page_Property extends Sledge_Permission_Page_Property {}
 
 ?>
