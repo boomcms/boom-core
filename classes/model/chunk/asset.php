f6dd39ada1ddb331a6c86871730af5fad1bef1ca
Rob Taylor <robt@hoopassociates.co.uk>
2012-03-13 10:31:34 +0000

    Fixed asset chunks

--- classes/model/chunk/asset.php
+++ classes/model/chunk/asset.php
@@ -1,5 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Model_Chunk_Asset extends Sledge_Model_Asset_Chunk {}
+class Model_Chunk_Asset extends Sledge_Model_Chunk_Asset {}
 
 ?>
