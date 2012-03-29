e7324ae104f247395f0bb4d089a999aed35c6f4d
Rob Taylor <robt@hoopassociates.co.uk>
2012-03-29 11:19:32 +0100

    Updated comments

--- classes/orm/taggable.php
+++ classes/orm/taggable.php
@@ -1,10 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-/**
-* @package ORM
-* @author Hoop Associates	www.thisishoop.com	mail@hoopassociates.co.uk
-* @copyright 2011, Hoop Associates
-*/
 abstract class ORM_Taggable extends Sledge_ORM_Taggable {}
 	
 ?>
