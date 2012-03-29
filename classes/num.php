de8bb813e7a9413331444b22f420e38968cdb0a8
Rob Taylor <robt@hoopassociates.co.uk>
2012-03-29 13:29:24 +0100

    Fixes to API comments

--- classes/num.php
+++ classes/num.php
@@ -1,5 +1,8 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-
+/**
+* @package Kohana
+* @category Helpers
+*/
 class Num extends Sledge_Num {}
 	
 ?>
--- classes/text.php
+++ classes/text.php
@@ -1,5 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
+/**
+* @package Kohana
+* @category Helpers
+*/
 class Text extends Sledge_Text {}
 	
 ?>
