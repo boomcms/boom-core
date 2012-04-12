af06e6bd773e7ee866544844eaf1c65b8ab38669
Rob Taylor <robt@hoopassociates.co.uk>
2012-04-12 10:00:50 +0100

    Removed setup code (will use phing instead)

--- init.php
+++ init.php
@@ -105,42 +105,5 @@ Route::set('default', '(<controller>(/<action>(/<id>)))')
 		'controller' => 'site',
 		'action'     => 'index',
 	));
-
-// Sledge setup.	
-// Has the runtime database configuration been created?
-// Sledge apps ship with no database configuration.
-// If this sledge is newly cloned then we need to set the database connection information.
-// This checks for the sledge property in the database config which is set by our config setup script.
-/*if (!Kohana::$config->load( 'database.default.connection.sledge' ))
-{
-	echo Request::factory( 'setup/dbconfig' )->post( $_POST )->execute();
-	exit;			
-}
-
-// Has the Sledge environment config file been created?
-if (!Kohana::$config->load( 'sledge.environment' ))
-{
-	echo Request::factory( 'setup/sledgeconfig' )->post( $_POST )->execute();
-	exit;			
-}		
-
-// Check that the datbase exists, if not we offer to create it.
-try
-{
-	$db = Database::instance();
-	$db->connect();
-}
-catch (Database_Exception $e)
-{
-	// Is it a database not existing error?
-	if (preg_match( "/Unknown database '(.*)'/", $e->getMessage(), $matches ))
-	{
-		Request::factory( 'setup/database' )->query( $_GET )->execute();
-		exit;
-	}
-	
-	// It's some other error which we don't worry about here.
-	throw $e;
-}*/
 	
 ?>
