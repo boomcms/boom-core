4dec14f46b1ec8757fcd141779288d4b4be00fe9
Rob Taylor <robt@hoopassociates.co.uk>
2012-04-13 17:09:30 +0100

    Fixed auto login by disabling resetting failed login counter reset in Model_Person::complete_login()

--- classes/model/user/token.php
+++ classes/model/user/token.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
-class Model_User_Token extends Model_Auth_User_Token
+class Model_User_Token extends Sledge_Model_User_Token
 {
 	protected $_belongs_to = array( 'user' => array( 'model' => 'person', 'foreign_key' => 'user_id' ) );
 	
