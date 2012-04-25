a56e16e1db66feb8517b4d57ceb02fef2c1a47ed
Rob Taylor <robt@hoopassociates.co.uk>
2012-04-25 16:50:42 +0100

    Created Sledge::theme_css() method to make the jQuery UI theme changable depending on the logged in user

--- views/site/standard_template_editable.php
+++ views/site/standard_template_editable.php
@@ -33,7 +33,7 @@
 	<?= HTML::style( "sledge/js/tiny_mce/themes/advanced/skins/o2k7/ui.css" ) ?>
 	<?= HTML::style( "sledge/js/tiny_mce/themes/advanced/skins/o2k7/ui_silver.css" ) ?>
 	<?= HTML::style( "sledge/css/sledge.tagmanager.css" ) ?>
-	<?= HTML::style( "sledge/css/ui-redmond/jquery-ui.css" ) ?>
+	<?= HTML::style( "sledge/css/" . Sledge::theme_css() ) ?>
 	<?= HTML::style( "sledge/css/sledge.ui.css" ) ?>
 	<?= HTML::style( "sledge/css/cms.css" ) ?>
 </head>
