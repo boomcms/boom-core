ef6bbfcb45b48f32318b702cf9e4a4c8280e721e
Rob Taylor <robt@hoopassociates.co.uk>
2012-06-12 12:02:53 +0100

    Fixed bug in db migration script for disabling of version for template template

--- db/deltas/7-disable_versioning_of_template_table.sql
+++ db/deltas/7-disable_versioning_of_template_table.sql
@@ -2,7 +2,7 @@
 
 drop table template;
 alter table template_v rename to template;
-drop table template drop id;
+alter table template drop id;
 alter table template change rid id tinyint(3) unsigned primary key auto_increment;
 alter table template drop audit_person;
 alter table template drop audit_time;
