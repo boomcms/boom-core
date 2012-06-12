a7ab29489839c763a29c4f3c6fafb7b1bd7ba52d
Rob Taylor <robt@hoopassociates.co.uk>
2012-06-12 11:48:07 +0100

    Removed versioning of templates table

---
+++ db/deltas/7-disable_versioning_of_template_table.sql
@@ -0,0 +1,13 @@
+-- //
+
+drop table template;
+alter table template_v rename to template;
+drop table template drop id;
+alter table template change rid id tinyint(3) unsigned primary key auto_increment;
+alter table template drop audit_person;
+alter table template drop audit_time;
+
+-- //@UNDO
+
+
+-- //
