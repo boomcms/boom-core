f755844184d20086f662dc7414f83afe8f73bb58
Rob Taylor <robt@hoopassociates.co.uk>
2012-06-13 10:40:44 +0100

    Added awesome controller for displaying a twitter feed

---
+++ views/site/twitter/feed.php
@@ -0,0 +1,12 @@
+<div class="news-feed">
+	<h3>Latest Tweets</h3>
+	<ul>
+		<? foreach ($tweets as $tweet): ?>
+			<li class='twitter'>
+				<span class='date'><?= Date::fuzzy_span( strtotime( $tweet->created_at ) ) ?></span>
+				<?= $tweet->text ?>
+			</li>
+		<? endforeach; ?>
+		<li class="followus"><a href="http://twitter.com/<?= $screen_name ?>">Follow us on Twitter!</a></li>
+	</ul>
+</div>
