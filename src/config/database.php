f39f51a894fb98e8451741d524b0408b9d79ceda
Rob Taylor <robt@hoopassociates.co.uk>
2012-06-13 16:17:31 +0100

    Updated tests to use sqlite (they don't work though)

--- src/config/database.php
+++ src/config/database.php
@@ -1,22 +1,43 @@
-<?php 
+<?php
+
+if (Kohana::$environment === Kohana::TESTING)
+{
+	$default = array(
+			'type' => 'pdo',
+			'connection' => array(
+			        'dsn'        => 'sqlite::memory:',
+			        'persistent' => TRUE,
+			    ),
+			'primary_key'  => 'id',   // Column to return from INSERT queries, see #2188 and #2273
+			'schema'       => '',
+			'table_prefix' => '',
+			'charset'      => NULL,
+			'caching'      => FALSE,
+			'profiling'    => TRUE,
+		);
+}
+else
+{
+	$default = array(
+			'type' => 'mysql',
+			'connection' => array(
+				'hostname' => '@db.host@',
+				'username' => '@db.user@',
+				'password' => '@db.password@',
+				'persistent' => true,
+				'database' => '@db.name@',
+			),
+			'primary_key'  => 'id',   // Column to return from INSERT queries, see #2188 and #2273
+			'schema'       => '',
+			'table_prefix' => '',
+			'charset'      => 'utf8',
+			'caching'      => TRUE,
+			'profiling'    => TRUE,
+		);
+}
 
 return array(
-	'default' => array(
-		'type' => 'mysql',
-		'connection' => array(
-			'hostname' => '@db.host@',
-			'username' => '@db.user@',
-			'password' => '@db.password@',
-			'persistent' => true,
-			'database' => '@db.name@',
-		),
-		'primary_key'  => 'id',   // Column to return from INSERT queries, see #2188 and #2273
-		'schema'       => '',
-		'table_prefix' => '',
-		'charset'      => 'utf8',
-		'caching'      => TRUE,
-		'profiling'    => TRUE,
-	),
+	'default' => $default,
 
 	'old' => array(
  	   'type' => 'postgresql',
