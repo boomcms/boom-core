a99dc00c0455ea435b33f863e53f0da51191ec51
Rob Taylor <robt@hoopassociates.co.uk>
2012-06-14 12:25:26 +0100

    Added ability to specifiy a title for a page archive

--- views/site/archive/index.php
+++ views/site/archive/index.php
@@ -1,6 +1,6 @@
 <div id="archive" class="corner-10">
 	<div class="headerwrapper corner-10">
-		<h2><a href="<?=$parent->url();?>">Archive</a></h2>
+		<h2><a href="<?=$parent->url();?>"><?= $title ?></a></h2>
 	</div>
 
 	<ul>
