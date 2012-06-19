1f21593d570509ff0e896700441dbd018749bebc
Rob Taylor <robt@hoopassociates.co.uk>
2012-06-19 11:50:21 +0100

    Chunks mostly working with new code

--- views/site/slots/default/feature/centreimage.php
+++ views/site/slots/default/feature/centreimage.php
@@ -2,7 +2,7 @@
 	# Copyright 2009, Hoop Associates Ltd
 	# Hoop Associates   www.thisishoop.com   mail@hoopassociates.co.uk
 ?>
-<a class="feature corner-10 <?=$class?>" href="#">
+<a class="feature corner-10" href="#">
 	<span class="title">Feature Box</span>
 	<span class="feature-content">Click on me to add a feature here.</span>
 </a>
--- views/site/slots/default/feature/feed.php
+++ views/site/slots/default/feature/feed.php
@@ -2,7 +2,7 @@
 	# Copyright 2009, Hoop Associates Ltd
 	# Hoop Associates   www.thisishoop.com   mail@hoopassociates.co.uk
 ?>
-<li class="<?=$class?>">
+<li>
 	<h3>News article title</h3>
 	<p class="paragraph">Click here to feature a lead news story</p>
 </li>
