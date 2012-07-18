0ca170c61bc17ac67df79e25760b2c4712707d3f
Rob Taylor <robt@hoopassociates.co.uk>
2012-07-18 15:20:40 +0100

    Added a cache config file with default timeout of NULL (never expire) for memcache

---
+++ config/cache.php
@@ -0,0 +1,21 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'memcache' => array(
+		'driver'             => 'memcache',
+		'default_expire'     => NULL,
+		'compression'        => FALSE,              // Use Zlib compression (can cause issues with integers)
+		'servers'            => array(
+			array(
+				'host'             => 'localhost',  // Memcache Server
+				'port'             => 11211,        // Memcache port number
+				'persistent'       => FALSE,        // Persistent connection
+				'weight'           => 1,
+				'timeout'          => 1,
+				'retry_interval'   => 15,
+				'status'           => TRUE,
+			),
+		),
+		'instant_death'      => TRUE,               // Take server offline immediately on first fail (no retry)
+	),
+);
