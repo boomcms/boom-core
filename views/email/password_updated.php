bd591b4846ac1daf6cefd6221079126126c3d05c
Rob Taylor <robt@hoopassociates.co.uk>
2012-07-26 12:06:33 +0100

    Added an email notification when a user's password is changed

---
+++ views/email/password_updated.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Email template to notify a user that their password has been updated.
+*
+* Rendered by Controller_Cms_Account::action_password().
+*/
+?>
+
+Dear <?=$person->name()?>,
+
+Your password has been updated.
+
+Thank you.  
