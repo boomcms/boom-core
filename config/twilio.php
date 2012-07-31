ad9b9c9aa473773f78db5b8029edbfb7382edbdb
Rob Taylor <robt@hoopassociates.co.uk>
2012-07-31 10:21:49 +0100

    Added a twilio config file with test account details

---
+++ config/twilio.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'default'	=>	array(
+		'account_sid'	=>	'AC6115095706af21c7d245b25ac351d7db',
+		'auth_token'	=>	'37a526bf2415426ec421d8749fe0bacf',
+	)
+);
