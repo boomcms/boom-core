46c3dadad1ae798b9b2d898bc9c6434be5aa5696
Rob Taylor <robt@hoopassociates.co.uk>
2012-07-31 12:16:36 +0100

    Updated login to use verification code

--- config/twilio.php
+++ config/twilio.php
@@ -2,6 +2,7 @@
 
 return array(
 	'default'	=>	array(
+		'from'			=>	'+442033223548',
 		'account_sid'	=>	'AC6115095706af21c7d245b25ac351d7db',
 		'auth_token'	=>	'37a526bf2415426ec421d8749fe0bacf',
 	)
