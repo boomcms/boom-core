a0346c1e4465d5d4cdfdc0a625faf3491d71b0a0
Rob Taylor <robt@hoopassociates.co.uk>
2012-08-20 11:51:14 +0100

    Added styling for invisible pages in child page list template

--- views/plugin/page/children.php
+++ views/plugin/page/children.php
@@ -1,7 +1,15 @@
 <ul>
 	<? foreach ($pages as $p): ?>
 		<li>
-			<a href="<?= $p->url() ?>"><?= $p->title ?></a>
+			<a
+			<? 
+				if (! $p->is_visible()):
+					echo " class='leftnav-page-invisible'";
+				elseif ( ! $p->visible_in_leftnav):
+					echo " style='color: #ff0000'";
+				endif;
+			?>
+			 href="<?= $p->url() ?>"><?= $p->title ?></a>
 		</li>
 	<? endforeach; ?>
 </ul>
