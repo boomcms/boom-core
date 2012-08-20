53e54f9fd353b436e78ebc9724d904cf685a7e97
Rob Taylor <robt@hoopassociates.co.uk>
2012-08-20 10:00:58 +0100

    Fixed bugs with editing linksets

--- views/site/slots/default/linkset/quicklinks.php
+++ views/site/slots/default/linkset/quicklinks.php
@@ -1,12 +1,8 @@
-<?php
-	# Copyright 2009, Hoop Associates Ltd
-	# Hoop Associates   www.thisishoop.com   mail@hoopassociates.co.uk
-?>
 <div class="span-3" id="utilities">
 	<ul>
-		<li>
+		<li class="sledge-chunk-linkset-addlink">
 			<a rel="addlink" href="#">
-				<span class="chunk-linkset-addlink">Add link</span>
+				<span>Add link</span>
 			</a>
 		</li>
 	</ul>
