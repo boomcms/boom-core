6ea1889a10d821f78da5eda52ed66c5869b257b6
Rob Taylor <robt@hoopassociates.co.uk>
2012-08-20 17:17:20 +0100

    Fixed tag URLs in tag archive

--- views/plugin/archive/tag.php
+++ views/plugin/archive/tag.php
@@ -5,7 +5,7 @@
 			foreach ($tags as $tag):
 				$name = htmlentities($tag['name'], ENT_QUOTES);
 				echo "<li>";
-				echo "<a href='/blog/tag/", $name, "'>", $name;
+				echo "<a href='/blog/$section/$name'>$name";
 				echo " <span class='number'>(", $tag['count'], " ", Inflector::plural('page', $tag['count']);
 				echo ")</span>&nbsp;&raquo;</a></li>";
 			endforeach;
