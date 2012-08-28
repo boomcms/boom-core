5a5300620daa1d2582a472656352034d4f7552b8
Rob Taylor <robt@hoopassociates.co.uk>
2012-08-28 16:10:24 +0100

    Fixed broken link to /sledge/css/cms.css

--- views/site/standard_template_editable.php
+++ views/site/standard_template_editable.php
@@ -35,7 +35,7 @@
 	<?= HTML::style("sledge/css/sledge.tagmanager.css") ?>
 	<?= HTML::style("sledge/css/ui-" . $actual_person->theme . "/jquery-ui.css", array('id' => 'sledge-theme-css')) ?>
 	<?= HTML::style("sledge/css/sledge.ui.css") ?>
-	<?= HTML::style("/sledge/css/cms.css") ?>
+	<?= HTML::style("sledge/css/cms.css") ?>
 </head>
 <body>
 	<div id="sledge-wysiwyg-toolbar" class="mceEditor o2k7Skin o2k7SkinSilver"></div>
