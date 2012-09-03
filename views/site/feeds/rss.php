3a2dc74e12be3d4c3f076c1d46010d83bc0d1b05
Rob Taylor <robt@hoopassociates.co.uk>
2012-09-03 11:16:27 +0100

    Improved RSS template

--- views/site/feeds/rss.php
+++ views/site/feeds/rss.php
@@ -15,7 +15,7 @@
 * @copyright 2012, Hoop Associates
 */
 ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 	<channel>
 		<title><?= $page->title ?></title>
 		<link><?= $page->url()?></link>
@@ -25,32 +25,28 @@
 		<language>en-gb</language>
 		<pubDate><?= date('r', $page->visible_from) ?></pubDate>
 		<lastBuildDate><?= date('r', time()) ?></lastBuildDate>
-		
-		<?
-			foreach ($children as $p):
-				?>
-				<item>
-					<guid>
-						<?= $p->url() ?>
-					</guid>
-					<title>
-						<![CDATA[ <?= html_entity_decode($p->title, ENT_QUOTES, 'UTF-8') ?> ]]>
-					</title>
-					<link>
-						<?= $p->url() ?>
-					</link>
-					<description>
-						<![CDATA[ 
-							<?= html_entity_decode(strip_tags(Chunk::factory('text', 'standfirst')->text()), ENT_QUOTES, 'UTF-8') ?>
-						]]>
-					</description>
-					<pubDate>
-						<?= date('r', $p->visible_from) ?>
-					</pubDate>
-				</item>
-				
-				<?
-			endforeach;
-		?>
+		<atom:link href="<?= $page->url() ?>/.rss" rel="self" type="application/rss+xml" />
+
+		<? foreach ($children as $p): ?>
+			<item>
+				<guid>
+					<?= $p->url() ?>
+				</guid>
+				<title>
+					<![CDATA[ <?= html_entity_decode($p->title, ENT_QUOTES, 'UTF-8') ?> ]]>
+				</title>
+				<link>
+					<?= $p->url() ?>
+				</link>
+				<description>
+					<![CDATA[ 
+						<?= html_entity_decode(strip_tags(Chunk::factory('text', 'standfirst')->text()), ENT_QUOTES, 'UTF-8') ?>
+					]]>
+				</description>
+				<pubDate>
+					<?= date('r', $p->visible_from) ?>
+				</pubDate>
+			</item>
+		<? endforeach; ?>
 	</channel>
 </rss>
