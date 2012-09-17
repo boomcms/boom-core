0501c54cc45261aec27c8d5ee6326713b9249edd
Rob Taylor <robt@hoopassociates.co.uk>
2012-09-17 15:23:01 +0100

    Added basic video uploading to bits on the run

---
+++ config/botr.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+// Config file Bits on the Run API
+return array(
+	'key'		=>	'5K69jazO',
+	'secret'	=>	'vI1v6In6xVyxLCig5Uo46WD4',
+);
