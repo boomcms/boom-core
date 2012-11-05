7b8711ea6348d97c407d22f6766197e1056571e2
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-11-05 14:03:49 +0000

    Can't add <p> to all default text. Breaks standfirst and h1.

--- messages/chunks.php
+++ messages/chunks.php
@@ -2,7 +2,7 @@
 
 // Contains the text for default chunks to highlight that content can be inserted.
 return array(
-	'text' 		=>	'<p>Default text.</p>',
+	'text' 		=>	'Default text.',
 	'feature'	=>	'Click to feature a page here.',
 	'linkset'	=>	'',
 	'slideshow'	=>	'Click to insert a slideshow here.',
