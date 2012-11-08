3973576d8a17e80089efc088746fcaf1d73fe979
Rob Taylor <robt@hoopassociates.co.uk>
2012-11-08 16:06:32 +0000

    Removed login/logout route from init.php

--- init.php
+++ init.php
@@ -9,19 +9,6 @@ if (Kohana::$environment === Kohana::DEVELOPMENT)
 }
 
 /**
-* Defines a shortcut for /cms/account pages (login, logout, etc.) so that account doesn't have to be used in the URL.
-*
-*/
-Route::set('auth', 'cms/<action>',
-	array(
-		'action' => '(login|logout)'
-	))
-	->defaults(array(
-		'controller' => 'cms_auth'
-	));
-
-
-/**
 * Route for displaying / saving assets
 *
 */
