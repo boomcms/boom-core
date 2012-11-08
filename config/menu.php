5f25a69b7ecbe77a41d6c6857446e47f523127cb
Rob Taylor <robt@hoopassociates.co.uk>
2012-11-08 12:18:36 +0000

    Update menu config / views

--- config/menu.php
+++ config/menu.php
@@ -1,34 +1,37 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
 return array(
-	'topbar'	=>	array(
-		'home'		=> array(
-			'title'		=>	'Home',
-			'url'		=>	'/',
-			'priority'	=>	1,
-		),
-		'assets'	=> array(
-			'title'		=>	'Assets',
-			'url'		=>	'/cms/assets',
-			'action'	=>	'manage_assets',
-			'priority'	=>	3,
-		),
-		'people'	=>	array(
-			'title'		=>	'People',
-			'url'		=>	'/cms/people',
-			'action'	=>	'manage_people',
-			'priority'	=>	4,
-		),
-		'pages'	=>	array(
-			'title'		=>	'Pages',
-			'url'		=>	'/cms/pages',
-			'priority'	=>	2,
-		),		
-		'templates'	=>	array(
-			'title'		=>	'Templates',
-			'url'		=>	'/cms/templates',
-			'action'	=>	'manage_templates',
-			'priority'	=>	5,
+	'sledge'	=>	array(
+		'view_filename'		=>	'menu/sledge',
+		'items'			=>	array(
+			'home'		=>	array(
+				'title'		=>	'Home',
+				'url'		=>	'/',
+				'priority'	=>	1,
+			),
+			'assets'	=> array(
+				'title'		=>	'Assets',
+				'url'		=>	'/cms/assets',
+				'role'		=>	'manage_assets',
+				'priority'	=>	3,
+			),
+			'people'	=>	array(
+				'title'		=>	'People',
+				'url'		=>	'/cms/people',
+				'role'		=>	'manage_people',
+				'priority'	=>	4,
+			),
+			'pages'	=>	array(
+				'title'		=>	'Pages',
+				'url'		=>	'/cms/pages',
+				'priority'	=>	2,
+			),
+			'templates'	=>	array(
+				'title'		=>	'Templates',
+				'url'		=>	'/cms/templates',
+				'role'		=>	'manage_templates',
+				'priority'	=>	5,
+			),
 		),
 	),
 );
