6d2bb9cbcca97131853a3d90a36745c47513a0fc
Rob Taylor <robt@hoopassociates.co.uk>
2012-11-27 14:00:10 +0000

    Moved active_people check and editor iframe from page controller to editor controller

--- classes/Sledge/Controller/Cms/Page.php
+++ classes/Sledge/Controller/Cms/Page.php
@@ -68,70 +68,6 @@ class Sledge_Controller_Cms_Page extends Sledge_Controller
 	}
 
 	/**
-	 * Returns a json encoded array of the people currently viewing the same page is this user.
-	 * This is queried periodically with an AJAX request to tell the user when other people are viewing the same page.
-	 * The last_active time of the current user is also updated.
-	 *
-	 * When making the request the javascript sends the ID of the page that is open in the browser window.
-	 * This controller SHOULD return an array of people viewing that page.
-	 * The controller SHOULD check that the current user is recorded as viewing the page specificed. This prevents people typing things into their browser (or making scripts) to get information for which they have no need.
-	 * When the user is not recorded as viewing the specified page the controller SHOULD return a json encoded empty array.
-	 *
-	 * @return 	string	A json encoded array
-	 */
-	public function action_active_people()
-	{
-		// Update the time that we were last active on this page.
-		// We use to ensure that people who closed their browser and wandered off don't appear as editing the page.
-		$us = ORM::factory('Person_Page', array(
-			'person_id'	=>	$this->person->id,
-			'page_id'		=>	$this->page->id,
-		));
-
-		// Are they known to be viewing this page?
-		if ($us->loaded())
-		{
-			$us->last_active = $_SERVER['REQUEST_TIME'];
-			$us->save();
-
-			// Get the details of the other people viewing this page who were last active in the past 2 minutes
-			// The JS polls the server every 30 seconds at the moment so two moments allows for a couple of failed requests.
-			// We're interesed in their person ID, when they started looking at the page, and whether they've saved the page.
-			$details = DB::select('people_pages.person_id', 'people_pages.since', 'people_pages.saved', 'people.name')
-				->from('people_pages')
-				->join('people', 'inner')
-				->on('people_pages.person_id', '=', 'people.id')
-				->where('people_pages.page_id', '=', $us->page_id)
-				->where('people_pages.person_id', '!=', $this->person->id)
-				->where('people_pages.last_active', '>=', $_SERVER['REQUEST_TIME'] - 120)
-				->execute();
-
-			$people = array();
-
-			foreach ($details as $detail)
-			{
-				$people[] = array(
-					'id'			=>	$detail['person_id'],
-					'name'		=>	$detail['name'],
-					'since'		=>	$detail['since'],
-					'saved'		=>	(bool) $detail['saved'],
-				);
-			}
-
-			// Send it back to the JS
-			$response = $people;
-		}
-		else
-		{
-			$response = array();
-		}
-
-		$this->response
-			->headers('content-type', 'application/json')
-			->body(json_encode($response));
-	}
-
-	/**
 	 * Add a new page to the CMS.
 	 * If not parent ID and template ID are set a template allowing the user to set these values is displayed.
 	 *
@@ -422,15 +358,10 @@ class Sledge_Controller_Cms_Page extends Sledge_Controller
 	 */
 	public function action_revisions()
 	{
-		if (Kohana::$environment == Kohana::PRODUCTION OR Kohana::$environment == Kohana::STAGING)
-		{
-			HTTP::check_cache($this->request, $this->response, $this->page->active_vid . "-" . $this->page->published_vid);
-		}
-
 		// Get all revisions back to the last published version, then all published versions before that.
 		$revisions = DB::select('page_versions.id')
 			->from('page_versions')
-			->where('rid', '=', $this->page->id)
+			->where('page_id', '=', $this->page->page_id)
 			->and_where_open()
 			->where('id', '>=', $this->page->published_vid)
 			->or_where('published', '=', TRUE)
