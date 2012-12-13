f8ed529268de216aaf1153c3d9756c9974fadc25
Rob Taylor <robt@hoopassociates.co.uk>
2012-12-13 14:13:20 +0000

    Reordered route definitions in init.php

--- init.php
+++ init.php
@@ -9,7 +9,7 @@ if (Kohana::$environment === Kohana::DEVELOPMENT)
 }
 
 /**
-* Route for displaying / saving assets
+* Route for displaying assets
 *
 */
 Route::set('asset', 'asset/<action>/<id>(/<width>(/<height>(/<quality>(/<crop>))))')
@@ -17,75 +17,6 @@ Route::set('asset', 'asset/<action>/<id>(/<width>(/<height>(/<quality>(/<crop>))
 		'controller' => 'asset',
 		'action'	 => 'view'
 	));
-
-/**
-* Defines the route for /cms page settings pages..
-*
-*/
-Route::set('page_settings', 'cms/page/settings/<action>/<id>' )
-	->defaults(array(
-		'controller' => 'cms_page_settings',
-	));
-
-/**
-* Defines the route for /cms page links pages..
-*
-*/
-Route::set('page_links', 'cms/page/link/<action>/<id>' )
-	->defaults(array(
-		'controller' => 'cms_page_link',
-	));
-
-/**
-* Defines the route for /cms pages.
-*
-*/
-Route::set('cms', '<directory>/<controller>(/<action>(/<id>))',
-	array(
-		'directory'	=> 'cms'
-	))
-	->defaults(array(
-		'action'     => 'index',
-	));
-
-/**
-* Defines the route for plugin controllers.
-*/
-Route::set('plugin', '<directory>/<controller>(/<action>)',
-	array(
-		'directory'	=> 'plugin'
-	))
-	->defaults(array(
-		'controller' => 'default',
-		'action'     => 'index',
-	));
-
-Route::set('child_page_plugin', 'plugin/page/children.<action>')
-	->defaults(array(
-		'controller'	=>	'plugin_page_children'
-	));
-
-Route::set('chunks', 'cms/chunk/<controller>/<action>/<page>')
-	->defaults(array(
-		'directory'	=>	'cms_chunk'
-	));
-
-/**
- * Route for vanity URIs. Vanity URIs are the page ID base-36 encoded and prefixed with an underscore.
- * Vanity URIs redirect to the page's primary URI
- */
-Route::set('vanity', '_<link>', array(
-		'link'	=>	'[a-zA-Z0-9]',
-	))
-	->filter(function(Route $route, $params, Request $request)
-		{
-			// Turn the vanity URI into a page ID.
-			$page_id = base_convert($params['link'], 36, 10);
-
-			HTTP::redirect(ORM::factory('Page', $page_id)->link(), 302);
-			return FALSE;
-		}
-	);
 
 /**
 * Any URIs not caught by a previous route will be caught by this.
@@ -128,3 +59,81 @@ Route::set('sledge', '<location>(.<action>)', array(
 			return FALSE;
 		}
 	);
+
+/**
+ * Route for vanity URIs. Vanity URIs are the page ID base-36 encoded and prefixed with an underscore.
+ * Vanity URIs redirect to the page's primary URI
+ */
+Route::set('vanity', '_<link>', array(
+		'link'	=>	'[a-zA-Z0-9]',
+	))
+	->filter(function(Route $route, $params, Request $request)
+		{
+			// Turn the vanity URI into a page ID.
+			$page_id = base_convert($params['link'], 36, 10);
+
+			HTTP::redirect(ORM::factory('Page', $page_id)->link(), 302);
+			return FALSE;
+		}
+	);
+
+
+
+/**
+* Defines the route for /cms page settings pages..
+*
+*/
+Route::set('page_settings', 'cms/page/settings/<action>/<id>' )
+	->defaults(array(
+		'controller' => 'cms_page_settings',
+	));
+
+/**
+* Defines the route for /cms page links pages..
+*
+*/
+Route::set('page_links', 'cms/page/link/<action>/<id>' )
+	->defaults(array(
+		'controller' => 'cms_page_link',
+	));
+
+/**
+* Defines the route for plugin controllers.
+*/
+Route::set('plugin', '<directory>/<controller>(/<action>)',
+	array(
+		'directory'	=> 'plugin'
+	))
+	->defaults(array(
+		'controller' => 'default',
+		'action'     => 'index',
+	));
+
+
+/**********************************
+ *
+ * Routes for logged in users only below
+ *
+ **********************************
+ */
+/**
+* Defines the route for /cms pages.
+*
+*/
+Route::set('cms', '<directory>/<controller>(/<action>(/<id>))',
+	array(
+		'directory'	=> 'cms'
+	))
+	->defaults(array(
+		'action'     => 'index',
+	));
+
+Route::set('child_page_plugin', 'plugin/page/children.<action>')
+	->defaults(array(
+		'controller'	=>	'plugin_page_children'
+	));
+
+Route::set('chunks', 'cms/chunk/<controller>/<action>/<page>')
+	->defaults(array(
+		'directory'	=>	'cms_chunk'
+	));
