cea1b1e8b68532a9cbc44345ee154c35779e431f
Rob Taylor <robt@hoopassociates.co.uk>
2012-12-21 10:04:32 +0000

    Removed active people check

---
+++ migrations/boom/20121221095800_remove_active_people_check.php
@@ -0,0 +1,24 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Migration_Boom_20121221095800 extends Minion_Migration_Base
+{
+
+	/**
+	 * Run queries needed to apply this migration
+	 *
+	 * @param Kohana_Database Database connection
+	 */
+	public function up(Kohana_Database $db)
+	{
+		$db->query(NULL, "drop table people_pages");
+	}
+
+	/**
+	 * Run queries needed to remove this migration
+	 *
+	 * @param Kohana_Database Database connection
+	 */
+	public function down(Kohana_Database $db)
+	{
+	}
+}
