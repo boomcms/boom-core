b436b34f3d2db9805d72cde34b27906e464d76b1
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-02 13:43:49 +0000

    Bugfix: wrong class name for extends

--- classes/Controller/Cms/Page/Version.php
+++ classes/Controller/Cms/Page/Version.php
@@ -1,3 +1,3 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
-class Controller_Cms_Page_Version extends Boom_Controller_Cms_Page_Options {}
+class Controller_Cms_Page_Version extends Boom_Controller_Cms_Page_Version {}
