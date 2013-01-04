6f64c08cfc6014d32ca0aed52a38c0f778c71cd3
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-04 13:54:16 +0000

    Added data-icons to text and slideshow toolbars

--- media/boom/toolbars/slideshow.php
+++ media/boom/toolbars/slideshow.php
@@ -1,7 +1,7 @@
 <div class="toolbar">
-	<button class="save" type="button">Save</button>
-	<button class="cancel" type="button">Cancel</button>
-	<button class="insert" type="button">New slide</button>
-	<button class="delete" type="button">Delete</button>
-	<button class="sort" type="button">Sort</button>
+	<button class="save" type="button" data-icon="boom-accept">Save</button>
+	<button class="cancel" type="button"data-icon="boom-cancel">Cancel</button>
+	<button class="insert" type="button" data-icon="boom-add">New slide</button>
+	<button class="delete" type="button" data-icon="boom-delete">Delete</button>
+	<button class="sort" type="button" data-icon="boom-sort">Sort</button>
 </div>
--- media/boom/toolbars/text.php
+++ media/boom/toolbars/text.php
@@ -3,35 +3,35 @@
 ?>
 <div id="wysihtml5-toolbar" class="toolbar">
 	<div class="commands">
-		<button id="b-editor-accept" data-wysihtml5-action class="action">Save</button>
-		<button id="b-editor-cancel" data-wysihtml5-action class="action">Cancel</button>
+		<button id="b-editor-accept" data-wysihtml5-action class="action" data-icon="boom-accept">Save</button>
+		<button id="b-editor-cancel" data-wysihtml5-action class="action" data-icon="boom-cancel">Cancel</button>
 <?
 	switch ( $mode ) {
 		case 'text' :
 		break;
 		case 'inline' :
 ?>
-		<button data-wysihtml5-command="bold" title="Make text bold (CTRL + B)" class="command">bold</button>
-		<button data-wysihtml5-command="italic" title="Make text italic (CTRL + I)" class="command">italic</button>
-		<button data-wysihtml5-command="createLink" title="Insert a link" class="command">link</button>
-		<button data-wysihtml5-command="insertSpeech" title="Insert speech" class="command">speech</button>
+		<button data-wysihtml5-command="bold" title="Make text bold (CTRL + B)" class="command" data-icon="boom-bold">bold</button>
+		<button data-wysihtml5-command="italic" title="Make text italic (CTRL + I)" class="command" data-icon="boom-italic">italic</button>
+		<button data-wysihtml5-command="createLink" title="Insert a link" class="command" data-icon="boom-link">link</button>
+		<button data-wysihtml5-command="insertSpeech" title="Insert speech" class="command" data-icon="boom-speech">speech</button>
 		<button data-wysihtml5-action="change_view" title="Show HTML" class="action">HTML</button>
-<?		
+<?
 		break;
 		default :
 ?>
-		<button data-wysihtml5-command="bold" title="Make text bold (CTRL + B)" class="command">bold</button>
-		<button data-wysihtml5-command="italic" title="Make text italic (CTRL + I)" class="command">italic</button>
-		<button data-wysihtml5-command="insertUnorderedList" title="Insert an unordered list" class="command">UL</button>
-		<button data-wysihtml5-command="insertOrderedList" title="Insert an ordered list" class="command">OL</button>
-		<button data-wysihtml5-command="insertHorizontalRule" title="Insert horizontal rule" class="command">HR</button>
-		<button data-wysihtml5-command="createLink" title="Insert a link" class="command">link</button>
-		<button data-wysihtml5-command="insertImage" title="Insert an image" class="command">asset</button>
-		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="p" title="Insert paragraph" class="command">P</button>
-		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2" title="Insert headline 2" class="command">H2</button>
-		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h3" title="Insert headline 3" class="command">H3</button>
-		<button data-wysihtml5-command="indent" title="Insert blockquote" class="command">blockquote</button>
-		<button data-wysihtml5-command="insertSpeech" title="Insert speech" class="command">speech</button>
+		<button data-wysihtml5-command="bold" title="Make text bold (CTRL + B)" class="command" data-icon="boom-bold">bold</button>
+		<button data-wysihtml5-command="italic" title="Make text italic (CTRL + I)" class="command" data-icon="boom-italic">italic</button>
+		<button data-wysihtml5-command="insertUnorderedList" title="Insert an unordered list" class="command" data-icon="boom-list">UL</button>
+		<button data-wysihtml5-command="insertOrderedList" title="Insert an ordered list" class="command" data-icon="boom-ol">OL</button>
+		<button data-wysihtml5-command="insertHorizontalRule" title="Insert horizontal rule" class="command" data-icon="boom-hr">HR</button>
+		<button data-wysihtml5-command="createLink" title="Insert a link" class="command" data-icon="boom-link">link</button>
+		<button data-wysihtml5-command="insertImage" title="Insert an image" class="command" data-icon="boom-asset">asset</button>
+		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="p" title="Insert paragraph" class="command" data-icon="boom-paragraph">P</button>
+		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2" title="Insert headline 2" class="command" data-icon="boom-h2">H2</button>
+		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h3" title="Insert headline 3" class="command" data-icon="boom-h3">H3</button>
+		<button data-wysihtml5-command="indent" title="Insert blockquote" class="command" data-icon="boom-blockquote">blockquote</button>
+		<button data-wysihtml5-command="insertSpeech" title="Insert speech" class="command" data-icon="boom-speech">speech</button>
 		<button data-wysihtml5-action="change_view" title="Show HTML" class="action">HTML</button>
 <?
 	}
