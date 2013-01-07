a788822f49777dec419462dffd2358ba560e7c9a
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-07 16:12:31 +0000

    Added comments

--- classes/Boom/Controller/Cms/Chunk/Linkset.php
+++ classes/Boom/Controller/Cms/Chunk/Linkset.php
@@ -19,8 +19,11 @@ class Boom_Controller_Cms_Chunk_Linkset extends Boom_Controller_Cms_Chunk
 
 	public function action_preview()
 	{
+		// Instantiate a linkset model
 		$model = new Model_Chunk_Linkset;
 
+		// Get the linkset data from the query string.
+		// TODO: POST would be better for linksets, slideshows already use POST.
 		$query = $this->request->query();
 
 		if (isset($query['data']['links']))
@@ -31,12 +34,15 @@ class Boom_Controller_Cms_Chunk_Linkset extends Boom_Controller_Cms_Chunk
 				$link['url'] = urldecode($link['url']);
 			}
 
+			// Add the links to the linkset model.
 			$model->links($query['data']['links']);
 		}
 
+		// Create a chunk with the linkset model.
 		$chunk = new Chunk_Linkset($this->page, $model, $query['slotname'], TRUE);
 		$chunk->template($query['template']);
 
+		// Display the chunk.
 		$this->response->body($chunk->execute());
 	}
 }
