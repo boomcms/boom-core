69243ff194e221c2dbcb2a848e838730cc8c1d84
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-09 16:41:15 +0000

    Fixed asset chunks

--- classes/Boom/Controller/Cms/Chunk/Asset.php
+++ classes/Boom/Controller/Cms/Chunk/Asset.php
@@ -8,27 +8,19 @@
  * @author	Rob Taylor
  * @copyright	Hoop Associates
  */
-class Boom_Controller_Cms_Chunk_Asset extends Boom_Controller
+class Boom_Controller_Cms_Chunk_Asset extends Boom_Controller_Cms_Chunk
 {
 	public function action_preview()
 	{
-		$chunk = ORM::factory('Chunk_Asset')
+		$model = ORM::factory('Chunk_Asset')
 			->values(array(
 				'asset_id'	=>	$this->request->query('asset_id'),
 				'link'		=>	$this->request->query('url'),
 			));
 
-		$chunk = new Chunk_Asset($this->request->query('slotname'), $this->page, $chunk);
+		$chunk = new Chunk_Asset($this->page, $model, $this->request->query('slotname'), TRUE);
 		$chunk->template($this->request->query('template'));
 
 		$this->response->body($chunk->execute());
-	}
-
-	/**
-	 * Save one or more asset slots to a page.
-	 */
-	public function action_save()
-	{
-		
 	}
 }
