3cd4323744dfb68f9519a375b2eb83c383a6cb89
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-10 11:16:23 +0000

    fixed bug in migration script

--- migrations/boom/20130109175700_page_links_back_to_urls.php
+++ migrations/boom/20130109175700_page_links_back_to_urls.php
@@ -16,7 +16,7 @@ class Migration_Boom_20130109175700 extends Minion_Migration_Base
 		$db->query(NULL, "alter table page_links rename to page_urls");
 		$db->query(NULL, "delete from roles where name like '%links%'");
 		$db->query(NULL, "insert into roles (name) values ('edit_page_urls')");
-		$db->query(NULL, "alter table page_versions change children_link_prefix children_url_prefix varchar(2048)");
+		$db->query(NULL, "alter table pages change children_link_prefix children_url_prefix varchar(2048)");
 	}
 
 	/**
