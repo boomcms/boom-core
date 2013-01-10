683bcd4617a4bd43e57915211f9e760e9d55ee55
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-10 15:15:57 +0000

    Updated preview toolbar HTML

--- views/boom/editor/toolbar_preview.php
+++ views/boom/editor/toolbar_preview.php
@@ -1,32 +1,7 @@
 <?= View::factory('boom/header', array('title' => $page->version()->title)) ?>
 
-<div id="boom-topbar" class="ui-helper-clearfix ui-tabs ui-widget ui-widget-content ui-corner-all">
-
-	<?= Menu::factory('boom') ?>
-
-	<div class="ui-helper-clearfix ui-tabs-panel ui-widget-content ui-corner-bottom">
-
-		<div id="b-page-actions">
-
-			<div class="boom-buttonset">
-				<button id="b-page-preview-published" class="boom-button ui-button-text-icon ui-button">
-					<?=__('Published changes')?>
-				</button>
-				<button id="b-page-preview-all" class="boom-button ui-button-text-icon ui-button">
-					<?=__('All changes')?>
-				</button>
-			</div>
-
-		</div>
-
-
-		<div class="ui-helper-right">
-			<button id="b-page-editbutton" class="boom-button b-button-preview ui-button-text-icon ui-button" data-preview="edit">
-				<?=__('Edit')?> <?=__('page')?>
-			</button>
-		</div>
-	</div>
-
-</div>
+<button id="b-page-editbutton" class="boom-button b-button-preview ui-button-text-icon ui-button" data-preview="edit">
+	<?=__('Edit')?> <?=__('page')?>
+</button>
 
 <?= View::factory('boom/editor/footer', array('register_page' => FALSE)) ?>
