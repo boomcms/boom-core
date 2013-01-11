1bdd14da0e20fc0d47f97a3ccef9e476c28aa184
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-11 10:50:44 +0000

    remove layout table from page admin settings

--- views/boom/editor/page/settings/admin.php
+++ views/boom/editor/page/settings/admin.php
@@ -1,12 +1,7 @@
 <form name="pagesettings-adminsettings" id="boom-form-pagesettings-adminsettings">
 	<div id="b-pagesettings" class="boom-tabs s-pagesettings">
-		<table width="100%">
-			<tr>
-				<td><?=__('Internal name')?></td>
-				<td>
-					<?= Form::input('internal_name', $page->internal_name, array('class' => 'boom-input', 'id' => 'internal_name')); ?>
-				</td>
-			</tr>
-		</table>
+		<?=__('Internal name')?>
+		
+		<?= Form::input('internal_name', $page->internal_name, array('class' => 'boom-input', 'id' => 'internal_name')); ?>
 	</div>
 </form>
