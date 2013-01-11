5d9d812bb32a7d054c85f0e382b9575ca3147851
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-11 11:05:24 +0000

    remove layout table from page template form

--- views/boom/editor/page/version/template.php
+++ views/boom/editor/page/version/template.php
@@ -1,13 +1,10 @@
 <form id="b-form-pageversion-template" name="pageversion-template">
 
 	<div class="b-template">
-		<table width="100%">
-			<tr>
-				<td width="180">Template:</td>
-				<td>
-					<?= Form::select('template_id', $templates, $template_id) ?>
-				</td>
-			</tr>
-		</table>
+		
+		Template:
+		
+		<?= Form::select('template_id', $templates, $template_id) ?>
+		
 	</div>
 </form>
