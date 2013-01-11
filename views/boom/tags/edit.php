891883e2aa69b26d279ae8fc0c5ab868d0350033
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-11 11:13:05 +0000

    remove layout table from tags form

--- views/boom/tags/edit.php
+++ views/boom/tags/edit.php
@@ -6,33 +6,28 @@
 			<li><a href="#edit-folder-basic">Basic</a></li>
 		</ul>
 		<div id="edit-folder-basic">
-			<table width="100%">
-				<tr>
-					<td>Name</td>
-					<td><input type="text" id="boom-tagmanager-tag-edit-name" class="boom-input boom-input-medium" name="name" value="<?=$tag->name?>" /></td>
-				</tr>
-				<tr>
-					<td>Parent tag</td>
-					<td>
-						<select id="boom-tagmanager-tag-edit-parent" name="parent_id">
-						<?
-							echo "<option value=''>None</option>";
+			Name
+			
+			<input type="text" id="boom-tagmanager-tag-edit-name" class="boom-input boom-input-medium" name="name" value="<?=$tag->name?>" />
+			
+			Parent tag
+			
+			<select id="boom-tagmanager-tag-edit-parent" name="parent_id">
+			<?
+				echo "<option value=''>None</option>";
 
-							foreach(ORM::factory('Tag')->where('type', '=', $type)->find_all() as $t):
-								echo "<option";
+				foreach(ORM::factory('Tag')->where('type', '=', $type)->find_all() as $t):
+					echo "<option";
 
-								if ($tag->parent_id AND $tag->parent_id == $t->parent_id)
-								{
-									echo " selected='selected'";
-								}
+					if ($tag->parent_id AND $tag->parent_id == $t->parent_id)
+					{
+						echo " selected='selected'";
+					}
 
-								echo " value='", $t->id, "'>", $t->name, "</option>";
-							endforeach
-						?>
-						</select>
-					</td>
-				</tr>
-			</table>
+					echo " value='", $t->id, "'>", $t->name, "</option>";
+				endforeach
+			?>
+			</select>
 		</div>
 	</div>
 </form>
