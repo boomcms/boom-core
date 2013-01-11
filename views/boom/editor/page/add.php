71589d4f008501e70a0125950ec80e6e6d5fd625
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-11 10:48:04 +0000

    remove layout table and inline styles from add page form

--- views/boom/editor/page/add.php
+++ views/boom/editor/page/add.php
@@ -1,34 +1,24 @@
 <form id="b-page-add-form">
-	<table>
-		<tr>
-			<td>
-				<label for="parent-page">
-					<?=__('Parent page')?>
-				</label>
-			</td>
-			<td>
-				<select name="parent_id" style="width:24em">
-					<option value="0"><?=__('No parent')?></option>
-					<?
-						foreach($page->mptt->fulltree() as $node):
-							echo "<option value='", $node->id, "'";
 
-							if ($node->id == $page->id)
-								echo " selected='selected'";
+	<label for="parent-page">
+		<?=__('Parent page')?>
+	</label>
 
-							echo ">", $node->page->version()->title, "</option>";
-						endforeach;
-					?>
-					</select>
-				</select>
-			</td>
-		</tr>
+	<select name="parent_id">
+		<option value="0"><?=__('No parent')?></option>
+		<?
+		foreach($page->mptt->fulltree() as $node):
+			echo "<option value='", $node->id, "'";
 
-		<tr>
-			<td><?=__('Template')?></td>
-			<td>
-				<?= Form::select('template_id', $templates, $default_template, array('style' => 'width: 24em')); ?>
-			</td>
-		</tr>
-	</table>
+			if ($node->id == $page->id)
+				echo " selected='selected'";
+
+			echo ">", $node->page->version()->title, "</option>";
+		endforeach;
+		?>
+		</option>
+	</select>
+<?=__('Template')?>
+<?= Form::select('template_id', $templates, $default_template, array('style' => 'width: 24em')); ?>
+
 </form>
