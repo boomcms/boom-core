ba82684c535e2fa377cc12691cf9c77a1389c4d2
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-14 14:31:53 +0000

    log out and user profile menu

--- media/boom/js/boom.page.js
+++ media/boom/js/boom.page.js
@@ -203,6 +203,22 @@ $.extend($.boom, {
 				);
 			});
 			
+			var user_menu = {
+				"Profile" : function(){
+				},
+				"Logout" : function(){
+					top.location = '/cms/logout';
+				}
+			};
+			
+			$('#boom-page-user-menu')
+				.splitbutton({
+					items: user_menu,
+					width: 'auto',
+					menuPosition: 'left',
+					split: false
+				});
+			
 			if ( $.boom.cookie.contains( 'navmenu' ) ) {
 				$( '#boom-nav' ).hide();
 			}
