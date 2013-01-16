71292dd3fb43145d701b4e3063a7f5d3d96778ea
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-16 16:52:12 +0000

    implicit input labelling

--- views/boom/editor/page/settings/visibility.php
+++ views/boom/editor/page/settings/visibility.php
@@ -1,20 +1,21 @@
 <form id="boom-form-pagesettings-visibility" name="pagesettings-visibility">
 	<div class="b-pagesettings">
-		<label for="page-visible"><?=__('Visible')?></label>
+		<label for="page-visible"><?=__('Visible')?>
 		<select id="page-visible" name="visible" class="boom-input boom_select">
 			<option value='1' <? if ($page->visible) echo "selected='selected'"; ?>>Yes</option>
 			<option value='0' <? if ( ! $page->visible) echo "selected='selected'"; ?>>No</option>
 		</select>
+		</label>
 		
-		<label for="page-visible-from"><?=__('Visible from')?></label>
+		<label for="page-visible-from"><?=__('Visible from')?>
 		
 		<input id="page-visible-from" name="visible_from" class="boom-input boom-datepicker" value="<?=date("d F Y", $page->visible_from);?>" />
 		<select>
 			<option>12:00</option>
 		</select>
+		</label>
 		
-		<label for="page-visible-to"><?=__('Visible until')?></label>
-		<input id="b-page-toggle-visible" type="checkbox" value="1" name='toggle_visible_to' class="ui-helper-right ui-helper-reset"<?=($page->visible_to) ? ' checked="checked"' : ''; ?> />
+		<label for="page-visible-to"><?=__('Visible until')?>
 		
 		<input id="page-visible-to"
 			name="visible_to"
@@ -25,5 +26,7 @@
 		<select id="page-visible-to-time" disabled="diabled">
 			<option>12:00</option>
 		</select>
+		</label>
+		<input id="b-page-toggle-visible" type="checkbox" value="1" name='toggle_visible_to' class="ui-helper-right ui-helper-reset"<?=($page->visible_to) ? ' checked="checked"' : ''; ?>
 	</div>
 </form>
