535c35769bb4aae895779137aa88a757da52bc38
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-16 13:28:07 +0000

    Bugfix: when saving a page the new version should be a draft

--- classes/Boom/Controller/Cms/Page/Version.php
+++ classes/Boom/Controller/Cms/Page/Version.php
@@ -40,6 +40,14 @@ class Boom_Controller_Cms_Page_Version extends Controller_Cms_Page_Settings
 		{
 			// Create a new version of the page.
 			$this->new_version = $this->_page->create_version();
+
+			// If the embargo time of the new version is in the past, set the embargo time to NULL
+			// This means that if the old version was published, the new version will be a draft.
+			// If the embargo time is in the future don't change it.
+			if ($this->new_version->embargoed_until <= $_SERVER['REQUEST_TIME'])
+			{
+				$this->new_version->embargoed_until = NULL;
+			}
 		}
 	}
 
