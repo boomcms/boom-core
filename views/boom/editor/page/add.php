23685beb2229558c368ceb1c406a5b11e7b57323
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-16 17:28:59 +0000

    add page form

--- views/boom/editor/page/add.php
+++ views/boom/editor/page/add.php
@@ -1,8 +1,8 @@
 <form id="b-page-add-form">
 
-	<label for="parent-page">
+	<label for="parent_id">
 		<?=__('Parent page')?>
-	</label>
+	
 
 	<select name="parent_id">
 		<option value="0"><?=__('No parent')?></option>
@@ -18,7 +18,9 @@
 		?>
 		</option>
 	</select>
-<?=__('Template')?>
+	</label>
+	
+<label for="template_id"><?=__('Template')?>
 <?= Form::select('template_id', $templates, $default_template, array('style' => 'width: 24em')); ?>
-
+</label>
 </form>
