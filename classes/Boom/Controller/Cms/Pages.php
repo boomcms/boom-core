5b747aed0129ce060989c7403fc5615fcad24d12
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-23 14:33:08 +0000

    Moved pages tab to new repo

---
+++ classes/Boom/Controller/Cms/Pages.php
@@ -0,0 +1,51 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+/**
+ * Controller for the page manager.
+ * Allows viewing a list of the pages in the CMS and, in the future, doing stuff with that list.
+ *
+ * Not to be confused with [Controller_Cms_Page] which is used for editing a single page.
+ *
+ * @package	BoomCMS
+ * @category	Controllers
+ * @author	Rob Tayor
+ *
+ */
+class Boom_Controller_Cms_Pages extends Boom_Controller
+{
+	/**
+	 * Check that they can manage templates.
+	 */
+	public function before()
+	{
+		parent::before();
+
+		// Permissions check
+		$this->authorization('manage_pages');
+	}
+
+	/**
+	 * Display a list of all the pages in the CMS.
+	 */
+	public function action_index()
+	{
+		$pages = DB::select('*')
+			->from('page_mptt')
+			->order_by('lft', 'asc')
+			->execute()
+			->as_array();
+
+		array_walk($pages, function( & $page)
+			{
+				$page = array(
+					'mptt' => $page,
+					'page' => ORM::factory('Page', $page['id'])
+				);
+			}
+		);
+
+		$this->template = View::factory('boom/pages/index', array(
+			'pages'	=>	$pages,
+		));
+	}
+}
---
+++ config/menu.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'boom'	=>	array(
+		'view_filename'		=>	'menu/boom',
+		'items'			=>	array(
+			'pages'	=>	array(
+				'title'		=>	'Pages',
+				'url'		=>	'/cms/pages',
+				'priority'	=>	2,
+			),
+		),
+	),
+);
