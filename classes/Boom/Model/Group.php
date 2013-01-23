56926e6186ca6cb8095341b52ca172d00aae2d25
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-23 17:42:16 +0000

    Fixed editing general permissions

--- classes/Boom/Model/Group.php
+++ classes/Boom/Model/Group.php
@@ -56,10 +56,14 @@ class Boom_Model_Group extends ORM
 				->values(array($this->id, $role_id, $allowed, $page_id))
 				->execute($this->_db);
 
+			// If the page ID is isn't set the set it to a string with '0' as the contents
+			// otherwise it won't be included in the DB::select()
+			$page_id = ($page_id)? DB::expr($page_id) : DB::expr("'0'");
+
 			// Add records in the people_roles table for the members of this group.
 			DB::insert('people_roles', array('person_id', 'group_id', 'role_id', 'allowed', 'page_id'))
 				->select(
-					DB::select('person_id', DB::expr($this->id), DB::expr($role_id), DB::expr($allowed), DB::expr($page_id))
+					DB::select('person_id', DB::expr($this->id), DB::expr($role_id), DB::expr($allowed), $page_id)
 						->from('people_groups')
 						->where('group_id', '=', $this->id)
 				)
