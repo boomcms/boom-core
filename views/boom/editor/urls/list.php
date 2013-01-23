301f43238d8b74726f07e573f8c23e6848c8ccb7
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-23 17:20:00 +0000

    indicate primary URL using ui-state-active

--- views/boom/editor/urls/list.php
+++ views/boom/editor/urls/list.php
@@ -9,7 +9,7 @@
 		<h1>All URLs</h1>
 		<ul>
 			<? foreach($page->urls->order_by('location', 'asc')->find_all() as $url): ?>
-				<li data-url="<?= $url->location ?>" data-id="<?= $url->id ?>">
+				<li data-url="<?= $url->location ?>" data-id="<?= $url->id ?>" <? if ( (bool) $url->is_primary ): echo 'class="ui-state-active"'; endif;?>>
 					<?= $url->location ?>
 					<?= Form::radio('is_primary', $url->location, (bool) $url->is_primary, array('class' => 'b-urls-primary')) ?>
 					<?= Form::checkbox("redirect_" . $url->id, 1, (bool) $url->redirect, array('class' => 'b-urls-redirect')) ?>
