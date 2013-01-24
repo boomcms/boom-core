a1bbf2c9695cc7915c1186ba0a7b0ccc2a647e76
Rob Taylor <robt@hoopassociates.co.uk>
2013-01-24 13:08:18 +0000

    Added permissions checks to page settings menu

--- views/boom/editor/page/settings/index.php
+++ views/boom/editor/page/settings/index.php
@@ -1,37 +1,49 @@
-<button id="boom-page-navigation" class="boom-button">
-	Navigation
-</button>
+<? if ($auth->logged_in('edit_page_navigation_basic', $page)): ?>
+	<button id="boom-page-navigation" class="boom-button">
+		Navigation
+	</button>
+<? endif; ?>
 
-<button id="boom-page-urls" class="boom-button">
-	URLs
-</button>
+<? if ($auth->logged_in('edit_page_urls', $page)): ?>
+	<button id="boom-page-urls" class="boom-button">
+		URLs
+	</button>
+<? endif; ?>
 
-<button id="boom-page-search" class="boom-button">
-	Search
-</button>
+<? if ($auth->logged_in('edit_page_search_basic', $page)): ?>
+	<button id="boom-page-search" class="boom-button">
+		Search
+	</button>
+<? endif; ?>
 
 <button id="boom-page-tags" class="boom-button">
 	Tags
 </button>
 
-<button id="boom-page-security" class="boom-button">
-	Security
-</button>
+<? if ($auth->logged_in('edit_page_children_basic', $page)): ?>
+	<button id="boom-page-childsettings" class="boom-button">
+		Child page settings
+	</button>
+<? endif; ?>
 
-<button id="boom-page-childsettings" class="boom-button">
-	Child page settings
-</button>
+<? if ($auth->logged_in('edit_page_admin_basic', $page)): ?>
+	<button id="boom-page-adminsettings" class="boom-button">
+		Admin settings
+	</button>
+<? endif; ?>
 
-<button id="boom-page-adminsettings" class="boom-button">
-	Admin settings
-</button>
+<? if ($auth->logged_in('edit_page_template', $page)): ?>
+	<button id="boom-page-template" class="boom-button">
+		<?= __('Template') ?>
+	</button>
+<? endif; ?>
 
-<button id="boom-page-template" class="boom-button">
-	<?= __('Template') ?>
-</button>
 <button id="boom-page-embargo" class="boom-button">
 	<?= __('Embargo') ?>
 </button>
-<button id="boom-page-featureimage" class="boom-button">
-	<?= __('Feature image') ?>
-</button>
+
+<? if ($auth->logged_in('edit_page_feature_image', $page)): ?>
+	<button id="boom-page-featureimage" class="boom-button">
+		<?= __('Feature image') ?>
+	</button>
+<? endif; ?>
