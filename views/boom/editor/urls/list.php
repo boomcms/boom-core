42f8c91ab6e2927a0362ff8c46faa1e7f0efb0cd
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-25 09:58:23 +0000

    updates to URL form for primary URLs.

--- views/boom/editor/urls/list.php
+++ views/boom/editor/urls/list.php
@@ -7,15 +7,24 @@
 	</div>
 	<div>
 		<h1>All URLs</h1>
-		<ul>
+		<ul class="boom-tree">
 			<? foreach($page->urls->order_by('location', 'asc')->find_all() as $url): ?>
 				<li data-url="<?= $url->location ?>" data-id="<?= $url->id ?>" <? if ( (bool) $url->is_primary ): echo 'class="ui-state-active"'; endif;?>>
 					<?= $url->location ?>
 					<?= Form::radio('is_primary', $url->location, (bool) $url->is_primary, array('id' => 'is_primary_' . $url->id, 'class' => 'b-urls-primary')) ?>
 					<label for="is_primary_<?= $url->id ?>">✔</label>
-					<?= Form::checkbox("redirect_" . $url->id, 1, (bool) $url->redirect, array('id' => 'redirect_' . $url->id,'class' => 'b-urls-redirect')) ?>
-					<label for="redirect_<?= $url->id ?>"><span class="on">R</span> <span class="off">V</span></label>
-					<button class="boom-button b-urls-remove">X</button>
+					<?
+					if ( !$url->is_primary ):
+					?>
+						<?= Form::checkbox("redirect_" . $url->id, 1, (bool) $url->redirect, array('id' => 'redirect_' . $url->id,'class' => 'b-urls-redirect')) ?>
+						<label for="redirect_<?= $url->id ?>">
+							<span class="on">R</span> 
+							<span class="off">V</span>
+						</label>
+						<button class="boom-button b-urls-remove">X</button>
+					<?
+					endif;
+					?>
 				</li>
 			<? endforeach; ?>
 		</ul>
