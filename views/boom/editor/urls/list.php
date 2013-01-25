b4ab3c28e82e8bff6c3b57c5cbe799225b337ceb
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-25 15:19:42 +0000

    URL form layout

--- views/boom/editor/urls/list.php
+++ views/boom/editor/urls/list.php
@@ -13,7 +13,7 @@
 								<span class="on">R</span> 
 								<span class="off">V</span>
 							</label>
-							<button class="boom-button b-urls-remove">X</button>
+							<button class="b-urls-remove">X</button>
 						
 					</span>
 					
