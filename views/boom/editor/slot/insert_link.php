2c7cb2fadbc7ae605e7930b65bdc08462d85e9da
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-01-29 15:03:50 +0000

    add generic page picker to link editor

--- views/boom/editor/slot/insert_link.php
+++ views/boom/editor/slot/insert_link.php
@@ -5,7 +5,7 @@
 	</ul>
 	<div id="boom-chunk-linkset-addlink-internal">
 
-			<input type="hidden" name="parent_id" value="<?=$page->mptt->parent_id?>">
+			<input type="hidden" name="parent_id" value="">
 			<div class="boom-chunk-linkset-internal-links boom-tree">
 				<ul>
 					<li><a id="page_5" href="/" rel="5">Home</a></li>
