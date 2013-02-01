d8ad9058bff54ee38758818206ac4424fdf38a8a
Rob Taylor <robt@hoopassociates.co.uk>
2013-02-01 15:33:43 +0000

    Bugfix: only show links in linksets if the editor is enabled, the link is external, or the link target is visible

--- classes/Boom/Chunk/Linkset.php
+++ classes/Boom/Chunk/Linkset.php
@@ -14,9 +14,28 @@ class Boom_Chunk_Linkset extends Chunk
 
 	protected function _show()
 	{
+		if ( ! Editor::instance()->state_is(Editor::DISABLED))
+		{
+			// Editor is enabled, show all the links.
+			$links = $this->_chunks->links();
+		}
+		else
+		{
+			// Editor is disabled - only show links where the target page is visible
+			$links = array();
+
+			foreach ($this->_chunk->links() as $link)
+			{
+				if ($link->is_external OR $link->target->is_visible())
+				{
+					$links[] = $link;
+				}
+			}
+		}
+
 		return View::factory("site/slots/linkset/$this->_template", array(
 			'title'		=>	$this->_chunk->title,
-			'links'	=>	$this->_chunk->links(),
+			'links'	=>	$links,
 		));
 	}
 
