462f02e8c24e3f48fc4b0c955a7d592e55af5e55
Rob Taylor <robt@hoopassociates.co.uk>
2013-02-07 12:48:09 +0000

    Removed redundant tag views

--- views/boom/tags/children.php
+++
@@ -1,9 +0,0 @@
-<div class="boom-box ui-widget ui-corner-all">
-	<ul class="ui-helper-clearfix boom-tree b-tags-tree boom-tree-noborder">
-		<? foreach ($tags as $tag): ?>
-			<li id='t<?= $tag->id ?>'>
-				<a rel='<?= $tag->id ?>' id='tag_<?= $tag->id ?>' href='#tag/<?= $tag->id ?>'><?= $tag->name ?></a>	
-			</li>
-		<? endforeach; ?>
-	</ul>
-</div>
--- views/boom/tags/edit.php
+++
@@ -1,9 +0,0 @@
-<form onsubmit='return false;'>
-	<label for="b-tag-name">Name
-
-	<input type="text" id="b-tag-name" class="boom-input boom-input-medium" name="name" value="<?=$tag->name?>" /></label>
-
-	<label for="b-tag-parent">Parent tag
-
-	<?= Form::select('parent_id', $all_tags, $tag->parent_id, array('id' => 'b-tag-parent')) ?></label>
-</form>
