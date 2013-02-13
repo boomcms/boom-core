e4b385323f52e49f5e08192e19969266906dda01
Jim O'Donnell <jim@eatyourgreens.org.uk>
2013-02-13 13:08:01 +0000

    bugfix for tag input text

--- media/boom/js/boom.tags.js
+++ media/boom/js/boom.tags.js
@@ -170,7 +170,7 @@ $.extend($.boom, {
 					},
 					select: function( event, ui ){
 						event.preventDefault();
-						add_input.val( ui.item.label );
+						add_input.val( '' );
 						complete.notify( ui.item );
 					}
 				})
