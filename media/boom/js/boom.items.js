78985be0355108ddc36d4ae863346c518acbe405
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-02-20 16:36:23 +0000

    delete old tag edit/save/remove code

--- media/boom/js/boom.items.js
+++ media/boom/js/boom.items.js
@@ -87,98 +87,22 @@ $.extend($.boom.items.tag,  {
 				$.boom.tagmanager.base.item.prototype.get.apply( self );
 
 				$.boom.loader.hide();
+				
+				var tabsConfig = $.extend({}, $.boom.config.tabs, {
 
-				self.bind();
+					show: function(event, ui){
+					}
+				});
+
+				self.tagmanager.elements.rightpane.ui({
+					tabs: tabsConfig
+				});
+				
+				$.boom.events.register('tag.clickAfter', 'tagmanager');
 				
 				$('.b-items-thumbs .thumb').captions($.boom.config.captions);
 				
 				$.boom.log('Tag items get');
 			});
-	},
-
-	/** @function */
-	bind : function(){
-		$.boom.log( 'items.tag.bind()' );
-
-		var self = this;
-
-		var tabsConfig = $.extend({}, $.boom.config.tabs, {
-
-			show: function(event, ui){
-			}
-		});
-
-		this.tagmanager.elements.rightpane.ui({
-			tabs: tabsConfig
-		});
-
-		$.boom.events.register('tag.clickAfter', 'tagmanager');
-
-	},
-
-	/** @function */
-	edit : function(event){
-		event.preventDefault();
-
-		var item = $( event.target ).closest( 'li' ), rid = item.find('a')[0].rel;
-		var self = this;
-
-		$.boom.dialog.open({
-			url: '/cms/tags/edit/' + rid + '?type=1',
-			title: 'Edit tag',
-			buttons: {
-				Cancel: function(){
-					$.boom.dialog.destroy(this);
-				},
-				Save: function(){
-					var dialog = this;
-					var data = $( dialog ).find('form').serialize();
-
-					item.find('> a').text( $( '#b-tag-name' ).val() );
-
-					self.save( rid, data)
-					.done( function(){
-
-						$.boom.dialog.destroy(dialog);
-
-						$.boom.growl.show('Tag successfully saved.');
-					});
-				}
-			}
-		});
-	},
-
-	/** @function */
-	save: function( tag_id, data ){
-		$.boom.loader.show();
-
-		return $.post('/cms/tags/save/' + tag_id, data)
-		.done( function(response){
-
-			$.boom.loader.hide();
-
-		});
-	},
-
-	/** @function */
-	remove : function(event){
-
-		event.preventDefault();
-
-		var item = $( event.target ).closest( 'li' ), rid = item.find('a')[0].rel;
-
-		$.boom.dialog.confirm('Please confirm', 'Are you sure you want to remove this tag? <br /><br /> This will delete the tag from the database and cannot be undone!', function(){
-
-			$.boom.loader.show();
-
-			$.post('/cms/tags/delete/' + rid)
-			.done( function(){
-
-				$.boom.loader.hide();
-
-				$.boom.growl.show('Tag successfully removed.');
-				item.remove();
-			});
-		});
 	}
 });
