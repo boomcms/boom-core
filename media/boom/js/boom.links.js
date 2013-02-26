a55f386a9d9f2e504f0369f0d97d2a821b874fc2
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-02-26 13:11:13 +0000

    allow the default link to be overridden

--- media/boom/js/boom.links.js
+++ media/boom/js/boom.links.js
@@ -54,16 +54,17 @@ $.extend( $.boom, {
 						
 						return false;
 					}
+				},
+				link : {
+					title: '',
+					rid: -1,
+					url: ''
 				}
 			};
 			
 			opts = $.extend( default_opts, opts );
 			
-			var link = {
-				title: '',
-				rid: -1,
-				url: ''
-			};
+			var link = opts.link;
 			var complete = new $.Deferred();
 			
 			$.boom.dialog.open( opts );
