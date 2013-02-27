b677fc1f102041b4b357212200ef0e83139a7e46
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-02-27 16:50:25 +0000

    New upload file text

--- views/boom/assets/upload.php
+++ views/boom/assets/upload.php
@@ -5,13 +5,13 @@
 				<div class="ui-corner-all">
 					<p>
 						<span class="ui-icon ui-icon-info"></span>
-						<span class="message">You may upload up to 5 files at a time.  <?=__('Allowed file types')?>: <?= implode(', ', Boom_Asset::$allowed_extensions) ?></span>
+						<span class="message">Drag and drop files here, or use the 'Add files' button, to start uploading.  <?=__('Allowed file types')?>: <?= implode(', ', Boom_Asset::$allowed_extensions) ?></span>
 					</p>
 				</div>
 			</div>
 			<div id="b-upload-progress"></div>
 				<input type="file" name="b-assets-upload-files[]" id="b-assets-upload-file" multiple min="1" max="5" />
-				<button class="boom-button" type="button" id="b-upload-add"> Add files </button>
+				<button class="boom-button" type="button" id="b-upload-add">Add files</button>
 				<button type="button" id="b-assets-upload-cancel" class="boom-button" data-icon="ui-icon-boom-cancel">Cancel</button>
 		</div>
 	</form>
