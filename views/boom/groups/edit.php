a1a56671217ffc65952abd89ac14eadeab443a26
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-02-27 16:42:51 +0000

    CSS fix

--- views/boom/groups/edit.php
+++ views/boom/groups/edit.php
@@ -1,4 +1,4 @@
-<form rel='<?= $group->id ?>' onsubmit='return false;'>
+<form class="ui-widget-content" rel='<?= $group->id ?>' onsubmit='return false;'>
 	<label for="b-people-group-name"><?= __('Name') ?></label>
 	<input type="text" id="b-people-group-name" class="boom-input boom-input-medium" name="name" value="<?=$group->name?>" />
 
