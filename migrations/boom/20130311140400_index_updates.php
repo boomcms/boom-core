119b9d4366b025eeec711b9bef070c294dd378c5
Rob Taylor <robt@hoopassociates.co.uk>
2013-03-11 15:13:09 +0000

    Fixed typo in db migration

--- migrations/boom/20130311140400_index_updates.php
+++ migrations/boom/20130311140400_index_updates.php
@@ -10,7 +10,7 @@ class Migration_Boom_20130311140400 extends Minion_Migration_Base
 	public function up(Kohana_Database $db)
 	{
 		$db->query(NULL, "create index page_versions_page_id_published_embargoed_until on page_versions(page_id, published, embargoed_until desc);");
-		$db->query(NULL, "alter table page_verisons drop index page_v_published_rid;");
+		$db->query(NULL, "alter table page_versions drop index page_v_published_rid;");
 	}
 
 	public function down(Kohana_Database $db)
