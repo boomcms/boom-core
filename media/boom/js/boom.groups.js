33ac856520429e96562e4cc7f71ff3f60bb8171c
Jim O'Donnell <jim@hoopassociates.co.uk>
2013-05-02 14:37:24 +0100

    jquery UI widget for editing group permissions.

--- media/boom/js/boom.groups.js
+++ media/boom/js/boom.groups.js
@@ -25,8 +25,6 @@ $.widget( 'boom.group_editor', {
 		$.boom.log( 'init group editor' );
 		
 		this._bind();
-		
-		this.permissions.editor = this;
 	},
 	
 	_bind: function() {
@@ -143,93 +141,152 @@ $.widget( 'boom.group_editor', {
 		.sload( self.options.base_url + 'edit/' + self.options.id, function(){
 
 			$.boom.loader.hide();
-
-			self.permissions._bind();
-		} );
-	},
-	
-	/**
-	@property
-	*/
-	permissions: {
-		
-		editor: null,
-		
-		_check_inputs: function( radio_buttons, value ) {
-
-			radio_buttons
-				.filter( ':checked' )
-				.prop( 'checked', false )
-				.removeAttr( 'checked' )
-				.end()
-				.filter( '[value=' + value + ']' )
-				.prop( 'checked', true )
-				.attr( 'checked', 'checked' );
-
-		},
-		
-		_change: function( role_id, allowed, page_id ) {
-
-			var self = this;
-
-			return $.post(
-				self.editor.options.base_url + 'remove_role/' + self.editor.options.id,
-				{
-					role_id : role_id,
-					page_id : page_id
-				}
-			)
-			.pipe( function( response ){
-				if ( allowed == -1 ){
-					console.log( this );
-					return this;
-				}
-				return $.post(
-					self.editor.options. base_url + 'add_role/' + self.editor.options.id,
-					{
-						role_id : role_id,
-						allowed : allowed,
-						page_id: page_id
-					}
-				);
-			})
-			.done( function( response ){
+			
+			self.options.browser.main_panel.group_permissions({ 
+				base_url: self.options.base_url,
+				id: self.options.id
 			});
 
-		},
+		} );
+	}
+});
+
+/**
+* User interface for managing group permissions.
+* @class
+* @name $.boom.group_permissions
+*/
+$.widget( 'boom.group_permissions', {
+	
+	/**
+	default config
+	@property
+	*/
+	options: {
+		/** URL prefix for ajax calls */
+		base_url : '/cms/groups/',
+		/** ID of the group being edited */
+		id : null
+	},
+	
+	_init: function() {
+		$.boom.log( 'init group permissions editor' );
 		
-		_bind: function() {
+		this._bind();
+	},
+	
+	_check_inputs: function( radio_buttons, value ) {
 
-			var self = this;
-			var selected_page = null;
-			var options = this.editor.options;
+		radio_buttons
+			.filter( ':checked' )
+			.prop( 'checked', false )
+			.removeAttr( 'checked' )
+			.end()
+			.filter( '[value=' + value + ']' )
+			.prop( 'checked', true )
+			.attr( 'checked', 'checked' );
 
-			$.boom.loader.hide();
+	},
+	
+	_change: function( role_id, allowed, page_id ) {
+
+		var self = this;
+
+		return $.post(
+			self.options.base_url + 'remove_role/' + self.options.id,
+			{
+				role_id : role_id,
+				page_id : page_id
+			}
+		)
+		.pipe( function( response ){
+			if ( allowed == -1 ){
+				console.log( this );
+				return this;
+			}
+			return $.post(
+				self.options. base_url + 'add_role/' + self.options.id,
+				{
+					role_id : role_id,
+					allowed : allowed,
+					page_id: page_id
+				}
+			);
+		})
+		.done( function( response ){
+		});
+
+	},
+	
+	_bind: function() {
+
+		var self = this;
+		var selected_page = null;
+		var options = this.options;
+
+		$.boom.loader.hide();
 
 
-			options.browser.main_panel
-				.ui()
-				.on( 'change', '#b-group-roles-general input[type=radio]', function( event ){
+		this.element
+			.ui()
+			.on( 'change', '#b-group-roles-general input[type=radio]', function( event ){
 
-					var role_id = this.name;
-					var allowed = parseInt( this.value, 10 );
+				var role_id = this.name;
+				var allowed = parseInt( this.value, 10 );
 
-					self._change( role_id, allowed, 0 );
+				self._change( role_id, allowed, 0 );
 
-				})
-				.on( 'change', '#b-group-roles-pages input[type=radio]', function( event ){
+			})
+			.on( 'change', '#b-group-roles-pages input[type=radio]', function( event ){
 
-					var role_id = this.name;
-					var allowed = parseInt( this.value, 10 );
-					var page_id = selected_page;
+				var role_id = this.name;
+				var allowed = parseInt( this.value, 10 );
+				var page_id = selected_page;
 
-					self._change( role_id, allowed, page_id );
+				self._change( role_id, allowed, page_id );
 
-				});
+			});
 
-				self._check_inputs( $( '#b-group-roles-general input[type=radio]'), -1 );
+			self._check_inputs( $( '#b-group-roles-general input[type=radio]'), -1 );
 
-				$.get( options.base_url + 'list_roles/' + options.id + '?page_id=0' )
+			$.get( options.base_url + 'list_roles/' + options.id + '?page_id=0' )
+			.done( function( data ){
+				for ( role in data ) {
+
+					self._check_inputs( $( 'input[name=' + role + ']' ), data[ role ] );
+				}
+			});
+
+		/**
+		 * Clicking on a page in the tree.
+		 * Should make a GET call to /cms/groups/list_roles/<group ID>?page_id=<page ID>
+		 *
+		 * This will return a json encoded array of role ID => <value>
+		 * Possible values are 1 if the role is allowed and 0 if the role is disallowed.
+		 * If nothing is set for a role then that role ID won't be in the returned array.
+		 *
+		 * The role checkboxes should then be updated if the correct values.
+		 */
+
+		var page_tree = this.element.find( '#b-group-roles-pages .boom-tree' );
+
+		$.boom.util.page_tree(  page_tree )
+			.progress( function( page ) {
+
+				selected_page = page.page_id;
+
+				self._check_inputs( $( '#b-group-roles-pages input[type=radio]'), -1 );
+
+				page_tree
+					.find( 'a[rel=' + page.page_id + ']' )
+					.parents( '.boom-tree' )
+					.find( 'a.ui-state-active' )
+					.removeClass( 'ui-state-active' )
+					.end()
+					.end()
+					.addClass( 'ui-state-active' );
+
+				$.get( options.base_url + 'list_roles/' + options.id + '?page_id=' + selected_page )
 				.done( function( data ){
 					for ( role in data ) {
 
@@ -237,45 +294,7 @@ $.widget( 'boom.group_editor', {
 					}
 				});
 
-			/**
-			 * Clicking on a page in the tree.
-			 * Should make a GET call to /cms/groups/list_roles/<group ID>?page_id=<page ID>
-			 *
-			 * This will return a json encoded array of role ID => <value>
-			 * Possible values are 1 if the role is allowed and 0 if the role is disallowed.
-			 * If nothing is set for a role then that role ID won't be in the returned array.
-			 *
-			 * The role checkboxes should then be updated if the correct values.
-			 */
+			});
 
-			var page_tree = options.browser.main_panel.find( '#b-group-roles-pages .boom-tree' );
-
-			$.boom.util.page_tree(  page_tree )
-				.progress( function( page ) {
-
-					selected_page = page.page_id;
-
-					self._check_inputs( $( '#b-group-roles-pages input[type=radio]'), -1 );
-
-					page_tree
-						.find( 'a[rel=' + page.page_id + ']' )
-						.parents( '.boom-tree' )
-						.find( 'a.ui-state-active' )
-						.removeClass( 'ui-state-active' )
-						.end()
-						.end()
-						.addClass( 'ui-state-active' );
-
-					$.get( options.base_url + 'list_roles/' + options.id + '?page_id=' + selected_page )
-					.done( function( data ){
-						for ( role in data ) {
-
-							self._check_inputs( $( 'input[name=' + role + ']' ), data[ role ] );
-						}
-					});
-
-				});
-
-		}
 	}
-});
+} );
