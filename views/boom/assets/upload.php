33d56f07a0e12bd27e368d402dea65ba8b0e3edf
Rob Taylor <robt@hoopassociates.co.uk>
2013-05-31 10:50:09 +0100

    Turned add files button into + icon

--- views/boom/assets/upload.php
+++ views/boom/assets/upload.php
@@ -5,13 +5,15 @@
 				<div class="ui-corner-all">
 					<p>
 						<span class="ui-icon ui-icon-info"></span>
-						<span class="message">Drag and drop files here, or use the 'Add files' button, to start uploading.  <?=__('Allowed file types')?>: <?= implode(', ', Boom_Asset::$allowed_extensions) ?></span>
+						<span class="message">Drag and drop files here, or use the + button, to start uploading.  <?=__('Allowed file types')?>: <?= implode(', ', Boom_Asset::$allowed_extensions) ?></span>
 					</p>
 				</div>
 			</div>
 			<div id="b-upload-progress"></div>
 				<input type="file" name="b-assets-upload-files[]" id="b-assets-upload-file" multiple min="1" max="5" />
-				<button class="boom-button" type="button" id="b-upload-add">Add files</button>
+				<button id="b-assets-upload" class="boom-button ui-button-text-icon" data-icon="ui-icon-boom-add">
+					<?=__('Add files')?>
+				</button>
 				<button type="button" id="b-assets-upload-cancel" class="boom-button" data-icon="ui-icon-boom-cancel">Cancel</button>
 		</div>
 	</form>
