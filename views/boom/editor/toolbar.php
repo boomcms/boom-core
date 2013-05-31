08f368724fb919f4624f07dd04149b7402a7464a
Rob Taylor <robt@hoopassociates.co.uk>
2013-05-31 11:19:26 +0100

    Added margins to the toolbar buttons

--- views/boom/editor/toolbar.php
+++ views/boom/editor/toolbar.php
@@ -20,8 +20,8 @@
 		$constants = $class->getConstants();
 		$constants = array_flip($constants);
 		$environment = $constants[Kohana::$environment];
-		$branchname = ''; 
-		
+		$branchname = '';
+
 		if ( Kohana::$environment == Kohana::DEVELOPMENT ):
 			$dir = DOCROOT;
 			exec( "cd '$dir'; git branch", $lines );
@@ -39,8 +39,8 @@
 		</div>
 	<? endif; ?>
 
-	<div id="b-page-actions">
-		<? if ($page->was_created_by($person) OR $auth->logged_in('edit_page_content', $page)): ?>
+	<? if ($page->was_created_by($person) OR $auth->logged_in('edit_page_content', $page)): ?>
+		<div id="b-page-actions" class="b-page-container">
 			<span id="boom-page-save-menu">
 				<button id="b-page-save" class="ui-button boom-button" disabled="disabled" title="You have no unsaved changes" data-icon="ui-icon-boom-accept">
 					<?=__('Accept')?>
@@ -49,7 +49,9 @@
 			<button id="b-page-cancel" class="ui-button boom-button" disabled="disabled" data-icon="ui-icon-boom-cancel">
 					<?=__('Cancel')?>
 			</button>
+		</div>
 
+		<div class="b-page-container">
 			<button id="b-page-version-status" class="ui-button boom-button">
 				<?= __($page->status()) ?>
 			</button>
@@ -60,46 +62,51 @@
 
 			<span id="boom-page-template-menu">
 				<button id="boom-page-template-settings" class="ui-button boom-button" data-icon="ui-icon-boom-options">
-					<?= __('Template settings') ?>
+					<?= __('Version settings') ?>
 				</button>
 			</span>
-		<? endif; ?>
-	</div>
-
-	<? if ($auth->logged_in('edit_page', $page)): ?>
-		<button id="boom-page-visibility" class="ui-button boom-button" data-icon="ui-icon-boom-<? echo ($page->is_visible())? 'visible' : 'invisible' ?>">
-			<?= __('Visibility') ?>
-		</button>
-		<span id="boom-page-settings-menu">
-			<button id="boom-page-settings" class="ui-button boom-button" data-icon="ui-icon-boom-settings">
-				<?= __('Settings') ?>
-			</button>
-		</span>
-		<button id="boom-page-history" class="ui-button boom-button" data-icon="ui-icon-boom-history">
-			<?= __('History') ?>
-		</button>
-	<? endif; ?>
-
-	<? if (($page->was_created_by($person) OR $auth->logged_in('delete_page', $page)) AND ! $page->mptt->is_root()): ?>
-		<button class="ui-button boom-button" id="b-page-delete" data-icon="ui-icon-boom-delete">
-			<?= __('Delete') ?>
-		</button>
+		</div>
 	<? endif; ?>
 
 	<? if ($auth->logged_in('add_page', $page)): ?>
-		<button id="b-page-addpage" class="ui-button boom-button" data-icon="ui-icon-boom-add">
-			<?=__('Add')?>
-		</button>
+		<div class="b-page-container">
+			<button id="b-page-addpage" class="ui-button boom-button" data-icon="ui-icon-boom-add">
+				<?=__('Add')?>
+			</button>
+		</div>
 	<? endif; ?>
 
 	<? if ($auth->logged_in('edit_page', $page)): ?>
-		<button id="boom-page-editlive" class="ui-button boom-button" data-icon="ui-icon-boom-edit-live">
-			<?=__('Edit live')?>
-		</button>
+		<div class="b-page-container">
+			<button id="boom-page-visibility" class="ui-button boom-button" data-icon="ui-icon-boom-<? echo ($page->is_visible())? 'visible' : 'invisible' ?>">
+				<?= __('Visibility') ?>
+			</button>
+			<span id="boom-page-settings-menu">
+				<button id="boom-page-settings" class="ui-button boom-button" data-icon="ui-icon-boom-settings">
+					<?= __('Settings') ?>
+				</button>
+			</span>
+			<button id="boom-page-history" class="ui-button boom-button" data-icon="ui-icon-boom-history">
+				<?= __('History') ?>
+			</button>
+			<? if (($page->was_created_by($person) OR $auth->logged_in('delete_page', $page)) AND ! $page->mptt->is_root()): ?>
+				<button class="ui-button boom-button" id="b-page-delete" data-icon="ui-icon-boom-delete">
+					<?= __('Delete') ?>
+				</button>
+			<? endif; ?>
+		</div>
+	<? endif; ?>
 
-		<button id="boom-page-viewlive" class="boom-button b-button-preview" data-icon="ui-icon-boom-view-live" data-preview="disabled">
-			<?=__('View live')?>
-		</button>
+	<? if ($auth->logged_in('edit_page', $page)): ?>
+		<div class="b-page-container">
+			<button id="boom-page-editlive" class="ui-button boom-button" data-icon="ui-icon-boom-edit-live">
+				<?=__('Edit live')?>
+			</button>
+
+			<button id="boom-page-viewlive" class="boom-button b-button-preview" data-icon="ui-icon-boom-view-live" data-preview="disabled">
+				<?=__('View live')?>
+			</button>
+		</div>
 	<? endif; ?>
 
 	<div id="boom-topbar-pagesettings" class="ui-helper-clearfix">
