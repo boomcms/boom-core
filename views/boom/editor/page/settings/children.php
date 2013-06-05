fd7121b949a3189cee8aab1b2212026729a8320c
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-05 12:40:33 +0100

    Whitespace

--- views/boom/editor/page/settings/children.php
+++ views/boom/editor/page/settings/children.php
@@ -13,62 +13,58 @@
 
 		<div id="child-settings-basic">
 			<label for="children_template_id"><?=__('Default child template')?>
-
-			<?= Form::select('children_template_id', $templates, $default_child_template) ?>
+				<?= Form::select('children_template_id', $templates, $default_child_template) ?>
 			</label>
 
 			<label for="child_template_cascade">Update existing child pages
-			<?= Form::checkbox('cascade[]', 'template_id', false, array('id' => 'child_template_cascade')); ?>
+				<?= Form::checkbox('cascade[]', 'template_id', false, array('id' => 'child_template_cascade')); ?>
 			</label>
 
 			<label for="children_ordering_policy"><?=__('Child ordering policy')?>
-			<?= Form::select('children_ordering_policy', array(
-					'sequence'		=>	'Manual',
-					'visible_from'	=>	'Date',
-					'title'			=>	'Alphabetic'
-				), $child_order_column);
-			?>
-			<?= Form::select('children_ordering_direction', array(
-					'asc'		=>	'Ascending',
-					'desc'	=>	'Descending'
-				), $child_order_direction);
-			?>
+				<?= Form::select('children_ordering_policy', array(
+						'sequence'		=>	'Manual',
+						'visible_from'	=>	'Date',
+						'title'			=>	'Alphabetic'
+					), $child_order_column);
+				?>
+				<?= Form::select('children_ordering_direction', array(
+						'asc'		=>	'Ascending',
+						'desc'	=>	'Descending'
+					), $child_order_direction);
+				?>
 			</label>
 		</div>
 		<? if ($allow_advanced): ?>
 			<div id="child-settings-advanced">
 
 				<label for="children_visible_in_nav"><?=__('Children visible in nav')?>?
-				<?= Form::select('children_visible_in_nav', array(
-							1	=>	'Yes',
-							0	=>	'No',
-						), $page->children_visible_in_nav); ?>
+					<?= Form::select('children_visible_in_nav', array(
+								1	=>	'Yes',
+								0	=>	'No',
+							), $page->children_visible_in_nav); ?>
 				</label>
 
 				<label for="visible_in_nav_cascade">Update existing child pages
-				<?= Form::checkbox('cascade[]', 'visible_in_nav', false, array('id' => 'visible_in_nav_cascade')); ?>
+					<?= Form::checkbox('cascade[]', 'visible_in_nav', false, array('id' => 'visible_in_nav_cascade')); ?>
 				</label>
 
 				<label for="children_visible_in_nav_cms"><?=__('Children visible in CMS nav')?>?
-
-				<?= Form::select('children_visible_in_nav_cms', array(
-							1	=>	'Yes',
-							0	=>	'No',
-						), $page->children_visible_in_nav_cms); ?>
+					<?= Form::select('children_visible_in_nav_cms', array(
+								1	=>	'Yes',
+								0	=>	'No',
+							), $page->children_visible_in_nav_cms); ?>
 				</label>
 
 				<label for="visible_in_nav_cms_cascade">Update existing child pages
-				<?= Form::checkbox('cascade', 'visible_in_nav_cms', false, array('id' => 'visible_in_nav_cms_cascade')); ?>
+					<?= Form::checkbox('cascade', 'visible_in_nav_cms', false, array('id' => 'visible_in_nav_cms_cascade')); ?>
 				</label>
 
 				<label for="children_url_prefix"><?=__('Default child URI prefix')?>
-
-				<?= Form::input('children_url_prefix', $page->children_url_prefix); ?>
+					<?= Form::input('children_url_prefix', $page->children_url_prefix); ?>
 				</label>
 
 				<label for="grandchild_template_id"><?=__('Default grandchild template')?>
-
-				<?= Form::select('grandchild_template_id', $templates, $default_grandchild_template) ?>
+					<?= Form::select('grandchild_template_id', $templates, $default_grandchild_template) ?>
 				</label>
 			</div>
 		<? endif; ?>
