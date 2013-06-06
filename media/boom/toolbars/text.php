73dff726f8a71f15c45b11fbbe680a63340ce9e3
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-06 10:41:27 +0100

    Changed title of insert image button to insert asset

--- media/boom/toolbars/text.php
+++ media/boom/toolbars/text.php
@@ -26,7 +26,7 @@
 		<button data-wysihtml5-command="insertOrderedList" title="Insert an ordered list" class="command"><span class="b-ol"></span></button>
 		<button data-wysihtml5-command="insertHorizontalRule" title="Insert horizontal rule" class="command"><span class="b-hr"></span></button>
 		<button data-wysihtml5-command="createLink" title="Insert a link" class="command"><span class="b-link"></span></button>
-		<button data-wysihtml5-command="insertImage" title="Insert an image" class="command"><span class="b-asset"></span></button>
+		<button data-wysihtml5-command="insertImage" title="Insert an asset" class="command"><span class="b-asset"></span></button>
 		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="p" title="Insert paragraph" class="command"><span class="b-paragraph"></span></button>
 		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2" title="Insert headline 2" class="command"><span class="b-h2"></span></button>
 		<button data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h3" title="Insert headline 3" class="command"><span class="b-h3"></span></button>
