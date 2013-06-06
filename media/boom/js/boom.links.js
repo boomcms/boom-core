40fe9bfb798412970ee2c4abc205175c8c451dc4
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-06 17:04:59 +0100

    Fixed bug with link picker not populating existing links from text chunks

--- media/boom/js/boom.links.js
+++ media/boom/js/boom.links.js
@@ -9,14 +9,14 @@ $.extend( $.boom,
 	@namespace
 	*/
 	links : {
-		
-		/** 
-		@function 
+
+		/**
+		@function
 		@example
 		var link = {
 			url : existing_link.href,
 			rid : existing_link.rel,
-			title : ( existing_link.textContent || existinglink.innerText ) 
+			title : ( existing_link.textContent || existinglink.innerText )
 		};
 		opts.link = link;
 		$.boom.links
@@ -27,45 +27,45 @@ $.extend( $.boom,
 			});
 		*/
 		picker: function( opts ) {
-			
+
 			opts = ( opts ) ? opts : {};
-			
+
 			var complete = new $.Deferred();
-			
+
 			var link_manager_url =
 				( opts.page_rid && typeof opts.page_rid != 'undefined' ) ?
-				'/cms/chunk/insert_url/' + opts.page_rid : 
+				'/cms/chunk/insert_url/' + opts.page_rid :
 				'/cms/chunk/insert_url';
-			
-			var default_opts = 
+
+			var default_opts =
 			/** @ignore */ {
 				title: 'Edit link',
 				url: link_manager_url,
 				deferred: complete,
 				onLoad: function(){
 					var self = this;
-					
-					if ( opts.link.rid == -1 ) {
+
+					if ( opts.link.rid == -1 || opts.link.rid == "" ) {
 						$( '#boom-chunk-linkset-addlink-external-url' ).val( opts.link.url );
 					}
-					
+
 					$.boom.util.page_tree( this.find( '.boom-tree' ) )
 						.progress( function( page ) {
 							link = page;
-							
+
 							complete.resolve( page );
 
 							$.boom.dialog.destroy( self );
 						});
 				},
 				callback: function(){
-					
+
 					if ( link.rid == -1 ) {
 						var link_text = $( '#boom-chunk-linkset-addlink-external-url' ).val();
 						link.url = link_text;
 						link.title = link_text;
 					}
-					
+
 					complete.resolve( link );
 				},
 				link : {
@@ -74,13 +74,13 @@ $.extend( $.boom,
 					url: ''
 				}
 			};
-			
+
 			opts = $.extend( default_opts, opts );
-			
+
 			var link = opts.link;
-			
+
 			$.boom.dialog.open( opts );
-			
+
 			return complete;
 		}
 	}
