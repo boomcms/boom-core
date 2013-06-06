ea63bfb9d63d1084eb660a5108dc23a06cb18d66
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-06 17:53:30 +0100

    Removed debugging

--- media/boom/js/boom.wysihtml5.js
+++ media/boom/js/boom.wysihtml5.js
@@ -380,9 +380,6 @@ $.widget('wysihtml5.editor', $.boom.editor,
 			};
 
 			opts.link = link;
-
-			console.log(opts);
-			console.log(opts.link);
 		}
 
 
@@ -390,8 +387,6 @@ $.widget('wysihtml5.editor', $.boom.editor,
 			.picker( opts )
 			.fail( function(){
 				var link = top.$( ed.element ).find( '[rel=new-link]' );
-				console.log(opts);
-				console.log( link );
 
 				link
 					.after( link.text() )
