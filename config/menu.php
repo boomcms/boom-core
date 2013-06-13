bbcd635d936c30bd52607009ba5e0a08efa42d6f
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-13 13:37:05 +0100

    Added menu config

---
+++ config/menu.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') or die('No direct access allowed.');
+
+return array(
+	'boom'	=>	array(
+		'items'			=>	array(
+			'people'	=>	array(
+				'title'		=>	'People',
+				'url'		=>	'/cms/people',
+				'role'		=>	'manage_people',
+				'priority'	=>	4,
+			),
+		),
+	),
+);
