30808acc325a9c7885ef58032c9633a597c1ce9f
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-13 12:44:33 +0100

    Added migration and views

---
+++ migrations/boom/20130613124200_create_template_table.php
@@ -0,0 +1,45 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * Initial boom core structure.
+ */
+class Migration_Boom_20121008111800 extends Minion_Migration_Base
+{
+
+	/**
+	 * Run queries needed to apply this migration
+	 *
+	 * @param Kohana_Database Database connection
+	 */
+	public function up(Kohana_Database $db)
+	{
+		$db->query(NULL, "
+			CREATE TABLE IF NOT EXISTS`templates` (
+				`id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+				`name` varchar(100) NOT NULL,
+				`description` text,
+				`filename` varchar(25) NOT NULL,
+				PRIMARY KEY (`id`)
+			  ) ENGINE=MyISAM AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
+		");
+
+		$db->query(NULL, "
+			CREATE TABLE IF NOT EXISTS `assets_tags` (
+			  `asset_id` int(10) unsigned NOT NULL DEFAULT '0',
+			  `tag_id` int(10) unsigned NOT NULL DEFAULT '0',
+			  PRIMARY KEY (`asset_id`,`tag_id`),
+			  KEY `assets_tags_tag_id` (`tag_id`),
+			  KEY `assets_tags_asset_id` (`asset_id`)
+			) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+		");
+	}
+
+	/**
+	 * Run queries needed to remove this migration
+	 *
+	 * @param Kohana_Database Database connection
+	 */
+	public function down(Kohana_Database $db)
+	{
+	}
+}
