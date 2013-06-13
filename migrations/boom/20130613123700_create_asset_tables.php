f8b5e16bb78a61b17710d79248566ac0843f903b
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-13 15:50:19 +0100

    Bugfix

--- migrations/boom/20130613123700_create_asset_tables.php
+++ migrations/boom/20130613123700_create_asset_tables.php
@@ -3,7 +3,7 @@
 /**
  * Initial boom core structure.
  */
-class Migration_Boom_20121008111800 extends Minion_Migration_Base
+class Migration_Boom_20130613123700 extends Minion_Migration_Base
 {
 
 	/**
