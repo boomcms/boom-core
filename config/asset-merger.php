fa3862192c5a422a26777f4cd80a1b625be8e3ff
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-16 16:10:54 +0100

    Added some CSS from the boom module

--- config/asset-merger.php
+++ config/asset-merger.php
@@ -3,5 +3,6 @@
 return array(
 	'load_paths' => array(
 		Assets::JAVASCRIPT => array(MODPATH.'boom-people/media/boom-people/js/'),
+		Assets::STYLESHEET => array(MODPATH.'boom-people/media/boom-people/css/'),
 	),
 );
