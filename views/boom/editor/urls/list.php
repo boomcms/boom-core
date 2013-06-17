257ba8b75918037fd1330ec3af05a78ed9275caa
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-17 12:30:05 +0100

    Page URLs bugfix

--- views/boom/editor/urls/list.php
+++ views/boom/editor/urls/list.php
@@ -5,21 +5,21 @@
 			<? foreach($urls as $url): ?>
 				<li data-url="<?= $url->location ?>" data-id="<?= $url->id ?>" <? if ( (bool) $url->is_primary ): echo 'class="ui-state-active"'; endif;?>>
 					/<?= $url->location ?>
-					
+
 					<span class="tools">
-						
-							<?= Form::checkbox("redirect_" . $url->id, 1, (bool) $url->redirect, array('id' => 'redirect_' . $url->id,'class' => 'b-urls-redirect')) ?>
+
+							<?= Form::checkbox("redirect_" . $url->id, 1, (bool) $url->redirect, array('id' => 'redirect_' . $url->id,'class' => 'ui-helper-hidden b-urls-redirect')) ?>
 							<label for="redirect_<?= $url->id ?>">
-								<span class="on">R</span> 
+								<span class="on">R</span>
 								<span class="off">V</span>
 							</label>
 							<button class="b-urls-remove">✕</button>
-						
+
 					</span>
-					
-					<?= Form::radio('is_primary', $url->location, (bool) $url->is_primary, array('id' => 'is_primary_' . $url->id, 'class' => 'b-urls-primary')) ?>
+
+					<?= Form::radio('is_primary', $url->location, (bool) $url->is_primary, array('id' => 'is_primary_' . $url->id, 'class' => 'ui-helper-hidden b-urls-primary')) ?>
 					<label class="primary" for="is_primary_<?= $url->id ?>">✔</label>
-					
+
 				</li>
 			<? endforeach; ?>
 		</ul>
