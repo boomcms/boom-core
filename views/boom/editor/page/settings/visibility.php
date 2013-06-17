098ae2db897acf8cc2d63ead87e0f1ea64d18026
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-17 15:52:18 +0100

    Whitespace

--- views/boom/editor/page/settings/visibility.php
+++ views/boom/editor/page/settings/visibility.php
@@ -1,25 +1,23 @@
 <form id="boom-form-pagesettings-visibility" name="pagesettings-visibility">
 	<div class="b-pagesettings">
 		<label for="page-visible"><?=__('Visible')?>
-		<select id="page-visible" name="visible" class="boom-input boom_select">
-			<option value='1' <? if ($page->visible) echo "selected='selected'"; ?>>Yes</option>
-			<option value='0' <? if ( ! $page->visible) echo "selected='selected'"; ?>>No</option>
-		</select>
+			<select id="page-visible" name="visible" class="boom-input boom_select">
+				<option value='1' <? if ($page->visible) echo "selected='selected'"; ?>>Yes</option>
+				<option value='0' <? if ( ! $page->visible) echo "selected='selected'"; ?>>No</option>
+			</select>
 		</label>
-		
+
 		<label for="page-visible-from"><?=__('Visible from')?>
-		
-		<input id="page-visible-from" name="visible_from" class="boom-input boom-datepicker" value="<?=date("d F Y h:m", $page->visible_from);?>" />
+			<input id="page-visible-from" name="visible_from" class="boom-input boom-datepicker" value="<?=date("d F Y h:m", $page->visible_from);?>" />
 		</label>
-		
+
 		<label for="page-visible-to"><?=__('Visible until')?>
-		
-		<input id="page-visible-to"
-			name="visible_to"
-			class="boom-input boom-datepicker"
-			value="<?=($page->visible_to) ?	date('d F Y h:m',$page->visible_to) : 'forever'; ?>"
-			<?=(!$page->visible_to) ? ' disabled="disabled"' : ''; ?>
-		/>
+			<input id="page-visible-to"
+				name="visible_to"
+				class="boom-input boom-datepicker"
+				value="<?=($page->visible_to) ?	date('d F Y h:m',$page->visible_to) : 'forever'; ?>"
+				<?=(!$page->visible_to) ? ' disabled="disabled"' : ''; ?>
+			/>
 		</label>
 		<input id="b-page-toggle-visible" type="checkbox" value="1" name='toggle_visible_to' class="ui-helper-right ui-helper-reset"<?=($page->visible_to) ? ' checked="checked"' : ''; ?>
 	</div>
