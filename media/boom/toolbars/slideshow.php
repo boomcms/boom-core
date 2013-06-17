e62bca0671e43853247084bebdf50938ebab7011
Rob Taylor <robt@hoopassociates.co.uk>
2013-06-17 10:57:04 +0100

    Slideshow toolbar layout fix

--- media/boom/toolbars/slideshow.php
+++ media/boom/toolbars/slideshow.php
@@ -1,14 +1,8 @@
 <div id="wysihtml5-toolbar" class="toolbar toolbar-slideshow">
-	<div class="b-button-group">
-		<button class="save" type="button" title="Accept changes"><span class="b-accept"></span></button>
-		<button class="cancel" type="button" title="Discard changes"><span class="b-cancel"></span></button>
-	</div>
-	<div class="b-button-group">
-		<button class="link" type="button" title="Edit slide link"><span class="b-link"></span></button>
-	</div>
-	<div class="b-button-group">
-		<button class="insert" type="button" title="Insert slide"><span class="b-add"></span></button>
-		<button class="delete" type="button" title="Delete current slide"><span class="b-delete"></span></button>
-		<button class="sort" type="button" title="Sort slides"><span class="b-sort"></span></button>
-	</div>
+	<button class="save" type="button" title="Accept changes"><span class="b-accept"></span></button>
+	<button class="cancel" type="button" title="Discard changes"><span class="b-cancel"></span></button>
+	<button class="link" type="button" title="Edit slide link"><span class="b-link"></span></button>
+	<button class="insert" type="button" title="Insert slide"><span class="b-add"></span></button>
+	<button class="delete" type="button" title="Delete current slide"><span class="b-delete"></span></button>
+	<button class="sort" type="button" title="Sort slides"><span class="b-sort"></span></button>
 </div>
