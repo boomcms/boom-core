554b34291acd1baeba3ee6eb70cc1d409c0523fb
Rob Taylor <robt@hoopassociates.co.uk>
2013-08-06 16:47:50 +0100

    Removed redundant boom-dialog HTML from iframe substemplate

--- views/boom/editor/iframe.php
+++ views/boom/editor/iframe.php
@@ -1,11 +1,3 @@
 <?= $body_tag ?>
 
 <iframe frameBorder="0" style="width: 100%; overflow: hidden; z-index: 10000; height: 0; background: transparent; position: fixed;<? if (Editor::instance()->state() !== Editor::EDIT): ?>border: none; width: 100px; right: 0; <? endif; ?>" id='b-page-topbar' scrolling="no" src='/cms/editor/toolbar/<?= $page_id ?>'></iframe>
-
-<div id="boom-dialogs">
-	<div id="boom-dialog-alerts">
-		<p>&nbsp;</p>
-	</div>
-</div>
-
-<div id="boom-loader-dialog-overlay" class="ui-widget-overlay"></div>
