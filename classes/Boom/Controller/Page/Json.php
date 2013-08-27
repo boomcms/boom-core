04a4aab591caf5fb9b110aa0ccb4044449583e64
Rob Taylor <robt@hoopassociates.co.uk>
2013-08-27 11:37:43 +0100

    Refactored JSON controller

--- classes/Boom/Controller/Page/Json.php
+++ classes/Boom/Controller/Page/Json.php
@@ -7,19 +7,26 @@
  */
 class Boom_Controller_Page_Json extends Controller_Page
 {
+	protected $_data = array();
+
 	public function action_show()
+	{
+		$this->_data = array(
+			'id'			=>	$this->page->id,
+			'title'			=>	$this->page->version()->title,
+			'visible'		=>	$this->page->visible,
+			'visible_to'	=>	$this->page->visible_to,
+			'visible_from'	=>	$this->page->visible_from,
+			'parent'		=>	$this->page->mptt->parent_id,
+			'bodycopy'	=>	Chunk::factory('text', 'bodycopy', $this->page)->text(),
+			'standfirst'		=>	Chunk::factory('text', 'standfirst', $this->page)->text(),
+		);
+	}
+
+	public function after()
 	{
 		$this->response
 			->headers('Content-Type', 'application/json')
-			->body(json_encode(array(
-				'id'			=>	$this->page->id,
-				'title'			=>	$this->page->version()->title,
-				'visible'		=>	$this->page->visible,
-				'visible_to'	=>	$this->page->visible_to,
-				'visible_from'	=>	$this->page->visible_from,
-				'parent'		=>	$this->page->mptt->parent_id,
-				'bodycopy'	=>	Chunk::factory('text', 'bodycopy', $this->page)->text(),
-				'standfirst'		=>	Chunk::factory('text', 'standfirst', $this->page)->text(),
-			)));
+			->body(json_encode($this->_data));
 	}
 }
