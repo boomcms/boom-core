3881d835117299de359973d21e6e41e0ddfaae0b
Rob Taylor <robt@hoopassociates.co.uk>
2013-09-11 11:53:00 +0100

    Removed gravatar code

--- classes/Boom/URL.php
+++ classes/Boom/URL.php
@@ -1,20 +1,11 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
+
 /**
-* @package Boom
-* @category Helpers
-*/
+ * @package Boom
+ * @category Helpers
+ */
 class Boom_URL extends Kohana_URL
 {
-	/**
-	 * Default gravatar options for [URL::gravatar()]
-	 *
-	 * @var array
-	 */
-	public static $gravatar_options = array(
-		's'	=>	80,
-		'd'	=>	'wavatar',
-	);
-
 	/**
 	 * Generate a unique URI
 	 *
@@ -49,38 +40,7 @@ class Boom_URL extends Kohana_URL
 			$append++;
 		}
 		while ( ! $page_url->location_available($uri));
-		
+
 		return $uri;
-	}
-
-	/**
-	 * Generate a gravatar URL.
-	 *
-	 * @param	string	$email	Emailaddress of the gravater.
-	 * @param	array	$options	Options to include in the request
-	 * @param	boolean	$secure	Whether the gravatar request should use HTTPS
-	 * @return	string
-	 */
-	public static function gravatar($email, array $options = NULL, $secure = FALSE)
-	{
-		$url = ($secure)? "https://secure.gravatar.com/avatar/" : "http://www.gravatar.com/avatar/";
-
-		// Add the MD5 email to the URL.
-		$url .= md5($email);
-
-		// Merge the given options with the default options.
-		$options = array_merge(URL::$gravatar_options, (array) $options);
-
-		// Are there any options?
-		if ($options !== NULL AND ! empty($options))
-		{
-			// Turn the options array into a http query string.
-			$query = http_build_query($options);
-
-			// Add the query string to the URL.
-			$url .= "?" . $query;
-		}
-
-		return  $url;
 	}
 }
