0030af46adf8d2807d9bc0d6fcf0054a8e77a6f9
Rob Taylor <robt@hoopassociates.co.uk>
2013-09-12 13:29:40 +0100

    More style fixes for filters

--- views/boom/assets/manager.php
+++ views/boom/assets/manager.php
@@ -49,6 +49,7 @@
 		<?= Form::select('types', array_merge(array('' => 'Filter by type'), ORM::factory('Asset')->types()), NULL, array('id' => 'b-assets-types')) ?>
 
 		<div id='b-tags-search'>
+			<span class="ui-icon ui-icon-boom-tag"></span>
 			<input type='text' class="b-filter-input" placeholder="Type a tag name" />
 			<ul class="b-tags-list">
 			</ul>
