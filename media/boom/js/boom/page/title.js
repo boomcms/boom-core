19aee5587272e606c65d41e6f4484089c84c8044
Rob Taylor <robt@hoopassociates.co.uk>
2013-09-17 12:35:43 +0100

    Refactoring page title editor

--- media/boom/js/boom/page/title.js
+++ media/boom/js/boom/page/title.js
@@ -1,12 +1,13 @@
-$.widget('boom.pageTitle', $.ui.chunkText, {
+$.widget('boom.pageTitle', $.ui.chunk, {
 
 	$el : null,
 
 	_bind : function() {
+		var self = this;
+
 		this.$el
 			.on('click', function(event) {
-				$.boom.page.slots.edit(event, this, {type : 'text'}, {});
-				return false;
+				self.edit();
 			});
 	},
 
@@ -14,6 +15,33 @@ $.widget('boom.pageTitle', $.ui.chunkText, {
 		this.$el = $(this.element);
 
 		this._bind();
+	},
+
+	edit : function() {
+		var self = this;
+		var old_html = this.$el.html();
+
+		self._bring_forward();
+		self._unbind();
+
+		$('body').editor('edit', self.$el)
+			.fail(function() {
+				self.element.html(old_html).show();
+				self.destroy();
+			})
+			.done(function(html) {
+				if (html != old_html) {
+					self.insert(html);
+				}
+			})
+			.always(function() {
+				if (self.$el.text() == '' ) {
+					self.$el.html(old_html);
+				}
+
+				self._send_back();
+				self._bind();
+			});
 	},
 
 	insert : function(html) {
@@ -31,5 +59,9 @@ $.widget('boom.pageTitle', $.ui.chunkText, {
 		.always(function() {
 			$.boom.loader.hide();
 		});
+	},
+
+	_unbind : function() {
+		this.$el.unbind('click');
 	}
 });
