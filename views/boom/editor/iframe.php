7bc8a5e5cb6232b80525e91a784ec83c90125059
Rob Taylor <robt@hoopassociates.co.uk>
2013-09-23 15:20:34 +0100

    Fixed position of editor iframe

--- views/boom/editor/iframe.php
+++ views/boom/editor/iframe.php
@@ -14,4 +14,4 @@
 	}
 </style>
 
-<iframe frameBorder="0" style="padding-bottom: 4px; width: 100%; overflow: hidden; z-index: 10000; height: 0; background: transparent; position: fixed;<? if (Editor::instance()->state() !== Editor::EDIT): ?>border: none; width: 100px; right: 0; <? endif; ?>" id='b-page-topbar' scrolling="no" src='/cms/editor/toolbar/<?= $page_id ?>'></iframe>
+<iframe frameBorder="0" style="left: 0; padding-bottom: 4px; width: 100%; overflow: hidden; z-index: 10000; height: 0; background: transparent; position: fixed;<? if (Editor::instance()->state() !== Editor::EDIT): ?>border: none; width: 100px; right: 0; <? endif; ?>" id='b-page-topbar' scrolling="no" src='/cms/editor/toolbar/<?= $page_id ?>'></iframe>
