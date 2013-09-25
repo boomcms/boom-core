50d405e5649ce82574e7c49dbcc3afd81a9169a5
Rob Taylor <robt@hoopassociates.co.uk>
2013-09-25 11:29:33 +0100

    Bugfix: auto_increment had been removed from chunk_slideshows.id

--- migrations/boom/20130925110400_add_foreign_key_constraints_to_chunks.php
+++ migrations/boom/20130925110400_add_foreign_key_constraints_to_chunks.php
@@ -8,7 +8,7 @@ class Migration_Boom_20130925110400 extends Minion_Migration_Base
 		$db->query(NULL, "SET FOREIGN_KEY_CHECKS=0;");
 
 		$db->query(NULL, "alter table chunk_slideshow_slides change asset_id asset_id int(10) unsigned");
-		$db->query(NULL, "alter table chunk_slideshows change id id int(8) unsigned");
+		$db->query(NULL, "alter table chunk_slideshows change id id int(8) unsigned auto_increment");
 		$db->query(NULL, "alter table chunk_slideshow_slides change chunk_id chunk_id int(8) unsigned");
 		$db->query(NULL, "alter table chunk_slideshow_slides add foreign key (asset_id) references assets(id)");
 		$db->query(NULL, "alter table chunk_slideshow_slides add foreign key (chunk_id) references chunk_slideshows(id)");
