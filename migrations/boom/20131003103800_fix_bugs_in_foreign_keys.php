abd8651f54b3e1fe34313b3b6ecf0f83a2aa381b
Rob Taylor <robt@hoopassociates.co.uk>
2013-10-03 10:43:39 +0100

    Fixed foreign key constraints not cascading

--- migrations/boom/20131003103800_fix_bugs_in_foreign_keys.php
+++ migrations/boom/20131003103800_fix_bugs_in_foreign_keys.php
@@ -1,19 +1,18 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Migration_Boom_20130925110400 extends Minion_Migration_Base
+class Migration_Boom_20131003103800 extends Minion_Migration_Base
 {
 
 	public function up(Kohana_Database $db)
 	{
 		$db->query(NULL, "SET FOREIGN_KEY_CHECKS=0;");
 
-		$db->query(NULL, "alter table chunk_slideshow_slides change asset_id asset_id int(10) unsigned");
-		$db->query(NULL, "alter table chunk_slideshows change id id int(8) unsigned auto_increment");
-		$db->query(NULL, "alter table chunk_slideshow_slides change chunk_id chunk_id int(8) unsigned");
+		$db->query(NULL, "alter table chunk_slideshow_slides drop foreign key chunk_slideshow_slides_ibfk_1");
+		$db->query(NULL, "alter table chunk_slideshow_slides drop foreign key chunk_slideshow_slides_ibfk_2");
+		$db->query(NULL, "alter table chunk_assets drop foreign key chunk_assets_ibfk_2");
+
 		$db->query(NULL, "alter table chunk_slideshow_slides add foreign key (asset_id) references assets(id) on delete cascade on update cascade");
 		$db->query(NULL, "alter table chunk_slideshow_slides add foreign key (chunk_id) references chunk_slideshows(id) on delete cascade on update cascade");
-
-		$db->query(NULL, "alter table chunk_assets change asset_id asset_id int(10) unsigned");
 		$db->query(NULL, "alter table chunk_assets add foreign key (asset_id) references assets(id) on delete cascade on update cascade");
 
 		$db->query(NULL, "SET FOREIGN_KEY_CHECKS=1;");
