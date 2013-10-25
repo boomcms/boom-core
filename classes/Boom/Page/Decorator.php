a52dcb823597ce673b6d31787636b87ea2f2ec11
Rob Taylor <robt@hoopassociates.co.uk>
2013-10-25 14:22:07 +0100

    Added page decorator class

---
+++ classes/Boom/Page/Decorator.php
@@ -0,0 +1,46 @@
+<?php
+
+class Boom_Page_Decorator
+{
+	/**
+	 *
+	 * @var Model_Page
+	 */
+	protected $_page;
+
+	/**
+	 *
+	 * @var array
+	 */
+	protected $_text_chunks_to_properties = array();
+
+	public function __construct(Model_Page $page)
+	{
+		$this->_page = $page;
+	}
+
+	public function __call($name, $arguments)
+	{
+		return call_user_func_array(array($this->_page, $name), $arguments);
+	}
+
+	public function __get($name)
+	{
+		return $this->_page->$name;
+	}
+
+	protected function _get_chunk_data()
+	{
+		$chunks = ORM::factory('Chunk_Text')
+			->where('page_vid', '=', $this->_page->version()->id)
+			->where('slotname', 'in', $this->_text_chunks_to_properties)
+			->find_all();
+
+		foreach ($chunks as $chunk)
+		{
+			$property = "_{$chunk->slotname}";
+
+			$this->$property = $chunk->text;
+		}
+	}
+}
