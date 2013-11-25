a9b6b8d5f46b13858debb3b84e97da7f05da06ec
Rob Taylor <robt@hoopassociates.co.uk>
2013-11-25 11:03:51 +0000

    Fixed styling of 'edit page' button when in preview mode

--- views/boom/editor/toolbar_preview.php
+++ views/boom/editor/toolbar_preview.php
@@ -1,7 +1,9 @@
 <?= View::factory('boom/header', array('title' => $page->version()->title)) ?>
 
-<button id="b-page-editbutton" class="boom-button b-button-preview ui-button-text-icon ui-button" data-preview="edit">
-	<?=__('Edit')?> <?=__('page')?>
-</button>
+<div id="b-topbar">
+	<button id="b-page-editbutton" class="boom-button b-button-preview ui-button-text-icon ui-button" data-preview="edit">
+		<?=__('Edit')?> <?=__('page')?>
+	</button>
+</div>
 
 <?= View::factory('boom/editor/footer', array('register_page' => FALSE)) ?>
