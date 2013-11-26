d192db476f254e803f06367e43890b23bcdaffaf
Rob Taylor <robt@hoopassociates.co.uk>
2013-11-26 15:41:37 +0000

    Don't have margin on page body when editor is in preview / disabled

--- views/boom/editor/iframe.php
+++ views/boom/editor/iframe.php
@@ -6,10 +6,12 @@
 	</div>
 <? endif; ?>
 
-<script type="text/javascript">
-	$(document).ready(function() {
-		$('body').css('margin-left', '60px');
-	});
-</script>
+<? if (Editor::instance()->state_is(Editor::EDIT)): ?>
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('body').css('margin-left', '60px');
+		});
+	</script>
+<? endif ?>
 
 <iframe frameBorder="0" style="position: fixed; left: 0; top: 0; bottom: 0; width: 60px; height: 100%; overflow: hidden; z-index: 10000; background: transparent; <? if (Editor::instance()->state() !== Editor::EDIT): ?>border: none; width: 100px; right: 0; <? endif; ?>" id='b-page-topbar' scrolling="no" src='/cms/editor/toolbar/<?= $page_id ?>'></iframe>
