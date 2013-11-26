27166c5ced095b8cdb13dcd47af85d70ba7e756d
Rob Taylor <robt@hoopassociates.co.uk>
2013-11-26 13:03:17 +0000

    Added a left margin to the main page so that it doesn't clash with the toolbar

--- views/boom/editor/iframe.php
+++ views/boom/editor/iframe.php
@@ -6,4 +6,10 @@
 	</div>
 <? endif; ?>
 
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('body').css('margin-left', '60px');
+	});
+</script>
+
 <iframe frameBorder="0" style="position: fixed; left: 0; top: 0; bottom: 0; width: 60px; height: 100%; overflow: hidden; z-index: 10000; background: transparent; <? if (Editor::instance()->state() !== Editor::EDIT): ?>border: none; width: 100px; right: 0; <? endif; ?>" id='b-page-topbar' scrolling="no" src='/cms/editor/toolbar/<?= $page_id ?>'></iframe>
