fd5ae951b7c3b8fc90caaba4c3d3e8d8ddd7624b
Rob Taylor <robt@hoopassociates.co.uk>
2013-11-26 09:55:38 +0000

    Bind loader to globa ajaxStart and ajaxComplete events

--- media/boom/js/boom/loader.js
+++ media/boom/js/boom/loader.js
@@ -6,6 +6,19 @@ $.widget('boom.boomLoader', {
 		img.src = '/media/boom/img/ajax_load.gif';
 
 		this.loader = $('<div id="b-loader"></div>').appendTo($(top.document).find('body'));
+		this._bind_loader_to_global_ajax_events();
+	},
+
+	_bind_loader_to_global_ajax_events : function() {
+		var loader = this;
+
+		$(this.document)
+			.bind("ajaxSend", function(){
+				loader.show();
+			 })
+			.bind("ajaxComplete", function(){
+				loader.hide();
+			 });
 	},
 
 	show : function() {
