fcab0bc6251d21f075282b385a8fa6daa24e86a8
Rob Taylor <robt@hoopassociates.co.uk>
2013-11-26 17:21:18 +0000

    Fixed styling of preview toolbar

--- views/boom/editor/toolbar_preview.php
+++ views/boom/editor/toolbar_preview.php
@@ -1,6 +1,6 @@
 <?= View::factory('boom/header', array('title' => $page->version()->title)) ?>
 
-<div id="b-topbar">
+<div id="b-topbar" class="b-page-toolbar-preview">
 	<button id="b-page-editbutton" class="boom-button b-button-preview ui-button-text-icon ui-button" data-preview="edit">
 		<?=__('Edit')?> <?=__('page')?>
 	</button>
