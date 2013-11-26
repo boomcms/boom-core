869459a472666dac868af24c9d592ffc321bf1bd
Rob Taylor <robt@hoopassociates.co.uk>
2013-11-26 09:58:34 +0000

    Removed counter of loaders from $.boom.loader

--- media/boom/js/boom/loader.js
+++ media/boom/js/boom/loader.js
@@ -1,6 +1,4 @@
 $.widget('boom.boomLoader', {
-	loaders : 0,
-
 	_create : function(){
 		var img = new Image();
 		img.src = '/media/boom/img/ajax_load.gif';
@@ -22,24 +20,10 @@ $.widget('boom.boomLoader', {
 	},
 
 	show : function() {
-		this.loaders ++;
-
 		this.loader.show();
-
-		return this.loaders;
 	},
 
-	hide : function(force) {
-		force = (force == undefined) ? false : true;
-
-		if (force) this.loaders = 0;
-
-		if (this.loaders > 0) this.loaders --;
-
-		if (this.loaders === 0) {
-			this.loader.hide();
-		}
-
-		return this;
+	hide : function() {
+		this.loader.hide();
 	}
 });
