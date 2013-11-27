7a5613119c4556735a4b64f088657a6d73c2b2ab
Rob Taylor <robt@hoopassociates.co.uk>
2013-11-27 14:28:39 +0000

    Added details of last edit to pending approvals list

--- views/boom/approvals/index.php
+++ views/boom/approvals/index.php
@@ -9,9 +9,19 @@
 
 		<? if (count($pages)): ?>
 			<table id="b-items-view-list">
+				<tr>
+					<th>Page title</th>
+					<th>Last edited by</th>
+					<th>Time of last edit</th>
+					<th></th>
+					<th></th>
+					<th></th>
+				</tr>
 				<? foreach ($pages as $page): ?>
 					<tr class="<?= Text::alternate('odd', 'even') ?>" data-page-id="<?= $page->id ?>">
 						<td><a href="<?= $page->url() ?>"><?= $page->version()->title ?></a></td>
+						<td><?= $page->version()->person->name ?> (<?= $page->version()->person->email ?>)</td>
+						<td><?= date('d F Y H:i', $page->version()->edited_time) ?></td>
 						<td><a href="#" class="b-approvals-publish">Publish</a></td>
 						<td><a href="#" class="b-approvals-reject">Reject</a></td>
 						<td><a href="<?= $page->url() ?>">View page</a></td>
