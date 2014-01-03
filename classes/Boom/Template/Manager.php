9f5fd9a06b115f0ce8bdc93f20ae67cf92a3cfd2
Rob Taylor <robt@hoopassociates.co.uk>
2014-01-03 14:27:50 +0000

    Added new method Boom_Template_Manager::get_valid_templates() to return templates which exist on the filesystem

--- classes/Boom/Template/Manager.php
+++ classes/Boom/Template/Manager.php
@@ -45,6 +45,22 @@ class Boom_Template_Manager
 		return $this->_template_filenames;
 	}
 
+	public function get_valid_templates()
+	{
+		$valid = array();
+		$templates = ORM::factory('Template')->order_by('name', 'asc')->find_all();
+
+		foreach ($templates as $template)
+		{
+			if ($template->file_exists())
+			{
+				$valid[] = $template;
+			}
+		}
+
+		return $valid;
+	}
+
 	public function template_exists_with_filename($filename)
 	{
 		$template = new Model_Template(array('filename' => $filename));
