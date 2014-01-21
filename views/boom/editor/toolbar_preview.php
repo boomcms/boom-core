d74edcc7971241049279079c79e4db4d710f0e63
Rob Taylor <robt@hoopassociates.co.uk>
2014-01-21 12:23:07 +0000

    Fixed the appearance of the edit page button while in preview mode

--- views/boom/editor/toolbar_preview.php
+++ views/boom/editor/toolbar_preview.php
@@ -1,7 +1,7 @@
 <?= View::factory('boom/header', array('title' => $page->version()->title)) ?>
 
 <div id="b-topbar" class="b-page-toolbar b-page-toolbar-preview">
-	<button id="b-page-editbutton" class="boom-button b-button-preview ui-button-text-icon ui-button" data-preview="edit">
+	<button id="b-page-editbutton" class="b-button b-button-preview" data-preview="edit">
 		<?=__('Edit')?> <?=__('page')?>
 	</button>
 </div>
