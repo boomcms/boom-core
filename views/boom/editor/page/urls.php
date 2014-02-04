53be6d0e5d29bf72845b0d6fa51c4c99c076233a
Rob Taylor <robt@hoopassociates.co.uk>
2014-02-04 17:34:57 +0000

    URL editor refinements

--- views/boom/editor/page/urls.php
+++ views/boom/editor/page/urls.php
@@ -4,17 +4,18 @@
 		<p>
 			Below is a list of all URLs for the page.
 		</p>
-		<p>
-			The highlighted URL indicates the page's primary URL.
-			You may only have one primary URL for the page which cannot be deleted.
-			All non-primary URLs will redirect to the primary URL.
-			Click on a URL to make it the primary URL for the page.
-		</p>
-		<ul>
+		<ul class='b-page-urls-help'>
+			<li>The highlighted URL indicates the page's primary URL.</li>
+			<li>You may only have one primary URL for the page which cannot be deleted.</li>
+			<li>All non-primary URLs will redirect to the primary URL.</li>
+			<li>Click on a URL to make it the primary URL for the page.</li>
+		</ul>
+		<ul id='b-page-urls-list'>
 			<? foreach($urls as $url): ?>
 				<li data-url="<?= $url->location ?>" data-id="<?= $url->id ?>" <? if ( (bool) $url->is_primary ): echo 'class="b-page-urls-primary"'; endif;?>>
 					<label class="primary" for="is_primary_<?= $url->id ?>">/<?= $url->location ?></label>
 
+					<span class='b-page-urls-primary-indicator'>primary</span>
 					<span title="Remove URL" class="ui-icon ui-icon-remove-small b-urls-remove"></span>
 					<?= Form::radio('is_primary', $url->location, (bool) $url->is_primary, array('id' => 'is_primary_' . $url->id, 'class' => 'ui-helper-hidden b-urls-primary')) ?>
 				</li>
