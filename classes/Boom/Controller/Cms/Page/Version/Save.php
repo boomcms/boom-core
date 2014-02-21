eb334afa390f18f1694bb6108fb30cc5f59a3e70
Rob Taylor <robt@hoopassociates.co.uk>
2014-02-21 13:11:36 +0000

    Bugfix: Remove forward slashes from page title before generating a url from a title

--- classes/Boom/Controller/Cms/Page/Version/Save.php
+++ classes/Boom/Controller/Cms/Page/Version/Save.php
@@ -104,7 +104,7 @@ class Boom_Controller_Cms_Page_Version_Save extends Controller_Cms_Page_Version
 			// Create a new primary link for the page.
 			$link = ORM::factory('Page_URL')
 				->values(array(
-					'location'	=>	Page_URL::from_title($this->page->parent()->url()->location, $this->request->post('title')),
+					'location'	=>	Page_URL::from_title($this->page->parent()->url()->location, URL::title($this->request->post('title'))),
 					'page_id'	=>	$this->page->id,
 					'is_primary'	=>	TRUE,
 				))
