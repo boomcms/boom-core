b5877670dd58a7436d913bfc7f97c29905888c56
Rob Taylor <robt@hoopassociates.co.uk>
2014-03-03 17:24:16 +0000

    Added migration to increase size of asset titles

---
+++ migrations/boom/20140302172300_increase_size_of_asset_titles.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Migration_Boom_20140302172300 extends Minion_Migration_Base
+{
+	public function up(Kohana_Database $db)
+	{
+		$db->query(NULL, "alter table assets change title title varchar(150)");
+	}
+
+	public function down(Kohana_Database $db)
+	{
+	}
+}
