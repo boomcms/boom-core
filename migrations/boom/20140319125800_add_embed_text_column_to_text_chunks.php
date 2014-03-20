243c137125006da75218e394336b0ebbd501f862
Rob Taylor <robt@hoopassociates.co.uk>
2014-03-20 17:19:14 +0000

    Done the last commit properly

--- migrations/boom/20140319125800_add_embed_text_column_to_text_chunks.php
+++ migrations/boom/20140319125800_add_embed_text_column_to_text_chunks.php
@@ -5,10 +5,11 @@ class Migration_Boom_20140319125800 extends Minion_Migration_Base
 
 	public function up(Kohana_Database $db)
 	{
-		$db->query(NULL, 'SET autocommit=0');
-		$db->begin();
 		$db->query(NULL, "alter table chunk_texts add site_text text");
 		$db->query(NULL, "update chunk_texts set site_text = text");
+
+		$db->query(NULL, 'SET autocommit=0');
+		$db->begin();
 
 		foreach (ORM::factory('Chunk_Text')->find_all() as $chunk)
 		{
