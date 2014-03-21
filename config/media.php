dda94ef6848c7a01f19d4f8a2e3c712ac11512ff
Rob Taylor <robt@hoopassociates.co.uk>
2014-03-21 15:11:03 +0000

    Added a custom class command to wysihtml5

--- config/media.php
+++ config/media.php
@@ -57,5 +57,6 @@ return array(
 		'wysihtml5/parser_rules/inline.js',
 		'wysihtml5/commands/insertBoomAsset.js',
 		'wysihtml5/commands/createBoomLink.js',
+		'wysihtml5/commands/customClass.js',
 	),
 );
