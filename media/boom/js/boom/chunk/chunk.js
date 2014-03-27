7ea08093a01331d8dd4b2152795e4b3325635977
Rob Taylor <robt@hoopassociates.co.uk>
2014-03-27 09:51:15 +0000

    Refactored updating HTML after chunks are saved

--- media/boom/js/boom/chunk/chunk.js
+++ media/boom/js/boom/chunk/chunk.js
@@ -61,17 +61,8 @@ $.widget('ui.chunk',
 	*/
 	_update_html : function(html) {
 		var $html = $(html);
-		this.element.html($html.html());
-
-		// Remove existing attributes
-		for (var i = 0, attrs = this.element[0].attributes, l = attrs.length; i < l; i++) {
-			this.element[0].setAttribute(attrs.item(i).nodeName, null);
-		}
-
-		// Copy all the attributes from the new html to the current element
-		for (var i = 0, attrs = $html[0].attributes, l = attrs.length; i < l; i++) {
-			this.element[0].setAttribute(attrs.item(i).nodeName, attrs.item(i).nodeValue);
-		}
+		this.element.replaceWith($html);
+		this.element = $($html[0]);
 
 		this.bind();
 	},
