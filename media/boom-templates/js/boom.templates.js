dce6fd72936e067576e4a70056e279f6962e6f50
Rob Taylor <robt@hoopassociates.co.uk>
2014-04-04 11:43:39 +0100

    Updated boom confirmations

--- media/boom-templates/js/boom.templates.js
+++ media/boom-templates/js/boom.templates.js
@@ -27,16 +27,15 @@ $.extend($.boom, {
 			var self = this;
 
 			$('.b-templates-delete').click(function(event){
-				var item = $(this).closest( "tr" );
-				$.boom.dialog.confirm(
-					"Please confirm",
-					"Are you sure you want to delete this template?"
-				)
-				.done( function(){
-					item.fadeOut(600, function(){
-						item.remove();
+				var item = $(this).closest( "tr" ),
+					confirmation = new boomConfirmation("Please confirm", "Are you sure you want to delete this template?");
+
+				confirmation
+					.done( function(){
+						item.fadeOut(600, function(){
+							item.remove();
+						});
 					});
-				});
 			});
 
 			$('#b-templates-save').click(function(){
