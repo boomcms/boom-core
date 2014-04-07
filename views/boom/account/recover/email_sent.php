41641cf65936cbc36f9a6207ad1e0c29640ca406
Rob Taylor <robt@hoopassociates.co.uk>
2014-04-07 17:46:55 +0100

    Restyled login / password reset forms

---
+++ views/boom/account/recover/email_sent.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en-gb">
+	<head>
+		<title>BoomCMS | <?= __('Password reset') ?></title>
+		<meta name="robots" content="noindex, nofollow" />
+		<?= Boom::include_css() ?>
+	</head>
+	<body id='b-login'>
+		<div>
+			<p>
+				Please follow the link in the email which has been sent to you to create a new password.
+			</p>
+		</div>
+	</body>
+</html>
