a72439f565ab46ddefdd74d06d6b608471083b36
Rob Taylor <robt@hoopassociates.co.uk>
2014-04-15 14:23:09 +0100

    Moving away from Kohana coding standards towards PSR-1: replaced NULL with null

--- migrations/boom/20130613124200_create_template_table.php
+++ migrations/boom/20130613124200_create_template_table.php
@@ -13,17 +13,17 @@ class Migration_Boom_20130613124200 extends Minion_Migration_Base
 	 */
 	public function up(Kohana_Database $db)
 	{
-		$db->query(NULL, "
+		$db->query(null, "
 			CREATE TABLE IF NOT EXISTS`templates` (
-				`id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
-				`name` varchar(100) NOT NULL,
+				`id` tinyint(3) unsigned NOT null AUTO_INCREMENT,
+				`name` varchar(100) NOT null,
 				`description` text,
-				`filename` varchar(25) NOT NULL,
+				`filename` varchar(25) NOT null,
 				PRIMARY KEY (`id`)
 			  ) ENGINE=MyISAM AUTO_INCREMENT=83 DEFAULT CHARSET=utf8;
 		");
 
-		$db->query(NULL, "insert ignore into roles (name, description) values ('manage_templates', 'View the template manager')");
+		$db->query(null, "insert ignore into roles (name, description) values ('manage_templates', 'View the template manager')");
 	}
 
 	/**
@@ -33,7 +33,7 @@ class Migration_Boom_20130613124200 extends Minion_Migration_Base
 	 */
 	public function down(Kohana_Database $db)
 	{
-		$db->query(NULL, "drop table templates");
-		$db->query(NULL, "delete from roles where name = 'manage_templates'");
+		$db->query(null, "drop table templates");
+		$db->query(null, "delete from roles where name = 'manage_templates'");
 	}
 }
