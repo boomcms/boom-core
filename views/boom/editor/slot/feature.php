0da1243b0defd497f606cddc16c9178e124b0708
Rob Taylor <robt@hoopassociates.co.uk>
2014-04-24 14:51:36 +0100

    Tidied markup of page feature editor

--- views/boom/editor/slot/feature.php
+++ views/boom/editor/slot/feature.php
@@ -1,28 +1,7 @@
-<?php
-/**
-* Shows a tree of pages on the site to select a page to feature.
-*
-* Rendered by Controller_Cms_Chunk::action_feature()
-*
-*********************** Variables **********************
-*	$page			****	Instance of Model_Page	****	The page which is being edited.
-********************************************************
-*
-*/
-?>
-<div style="margin-bottom: .6em">
-	<div class="ui-widget">
-		<div class="ui-state-highlight ui-corner-all"> 
-			<p style="margin: .5em;">
-				<span style="float: left; margin-right: 0.3em; margin-top:-.2em" class="ui-icon ui-icon-info"></span>
-				Select a page to feature below.
-			</p> 
-		</div>
-	</div>
-	<br />
+<p>Select a page to feature below.</p>
 
-	<input type="hidden" name="parent_id" value="<?=$page->id?>">
-		<ul class="boom-tree">
-			<li><a id="page_5" href="/" rel="5">Home</a></li>
-		</ul>
-</div>
+<input type="hidden" name="parent_id" value="<?=$page->id?>">
+
+<ul class="boom-tree">
+	<li><a id="page_5" href="/" rel="5">Home</a></li>
+</ul>
