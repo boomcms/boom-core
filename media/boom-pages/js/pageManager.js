a736799f060171bd83a870718b258b55ea90cd8c
Rob Taylor <robt@hoopassociates.co.uk>
2014-04-25 16:33:48 +0100

    Added ability to edit page visibility from page manager

--- media/boom-pages/js/pageManager.js
+++ media/boom-pages/js/pageManager.js
@@ -1,12 +1,21 @@
 $.widget( 'boom.pageManager', {
-	addActionButtons : function($elements) {
+	addActionButtons : function($elements, children) {
+		var pageManager = this,
+			elementsById = {};
+
 		$elements.each(function() {
 			var $el = $(this);
 
 			$el.append("<div><a href='#' class='b-pages-delete'>Delete page</a></div>");
+			elementsById[$el.data('page-id')] = $el;
 		});
 
-		$elements.sortable();
+		$(children).each(function(i, child) {
+			var $visibilityButton = $("<a class='b-pages-visibility' href='#'>Visibility</a>");
+
+			child.visible? $visibilityButton.addClass('visible') : $visibilityButton.addClass('invisible');
+			elementsById[child.id].find('div').append($visibilityButton);
+		});
 	},
 
 	_create : function() {
@@ -15,23 +24,52 @@ $.widget( 'boom.pageManager', {
 		this.element
 			.pageTree({
 				load : function(e, data) {
-					pageManager.addActionButtons(data.elements);
+					pageManager.addActionButtons(data.elements, data.children);
 				}
 			});
 
-		this.addActionButtons(this.element.find('li'));
+		this.addActionButtons(this.element.find('li'), {});
+	},
+
+	deletePage : function($el) {
+		var page = new boomPage($el.data('page-id'));
+
+		page.remove()
+			.done(function() {
+				$el.remove();
+			});
+	},
+
+	editVisibility : function($el) {
+		console.log($el);
+		console.log($el.data());
+		var page = new boomPage($el.data('page-id'));
+
+		page.visibility()
+			.done(function(visible) {
+				var $visibilityButton = $el.find('.b-page-visibility');
+
+				if (visible) {
+					$visibilityButton.removeClass('invisible').addClass('visible');
+				} else {
+					$visibilityButton.removeClass('visible').addClass('invisible');
+				}
+			});
 	},
 
 	_init : function() {
-		this.element.on('click', '.b-pages-delete', function() {
-			var $this = $(this),
-				$li = $this.closest('li'),
-				page = new boomPage($li.data('page-id'));
+		var pageManager = this;
 
-			page.remove()
-				.done(function() {
-					$li.remove();
-				});
-		});
+		this.element
+			.on('click', '.b-pages-delete', function(e) {
+				e.preventDefault();
+
+				pageManager.deletePage($(this).closest('li'));
+			})
+			.on('click', '.b-pages-visibility', function(e) {
+				e.preventDefault();
+
+				pageManager.editVisibility($(this).closest('li'));
+			});
 	}
 });
