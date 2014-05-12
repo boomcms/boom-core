11d2b49255c1275e768551c715269d14363ba91f
Rob Taylor <robt@hoopassociates.co.uk>
2014-05-12 17:14:21 +0100

    Added NextPageBySequence and PreviousPageBySequence filters for the page finder

---
+++ classes/Boom/Finder/Page/Filter/NextPageBySequence.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Boom\Finder\Page\Filter;
+
+use \Boom\Finder as Finder;
+
+class NextPageBySequence extends Finder\Filter
+{
+	/**
+	 *
+	 * @var \Boom\Page
+	 */
+	protected $_currentPage;
+
+	public function __construct(\Boom\Page $currentPage)
+	{
+		$this->_currentPage = $currentPage;
+	}
+
+	public function execute(\ORM $query)
+	{
+		$query
+			->where('sequence', '>', $this->currentPage->sequence)
+			->order_by('sequence', 'asc');
+	}
+}
