f018975a99ae139736210c15f5be81d583760c17
Rob Taylor <rob@uxblondon.com>
2014-10-23 17:27:31 +0100

    Fixed bugs in migrations

--- migrations/boom/20130925110400_add_foreign_key_constraints_to_chunks.php
+++ migrations/boom/20130925110400_add_foreign_key_constraints_to_chunks.php
@@ -5,7 +5,7 @@ class Migration_Boom_20130925110400 extends Minion_Migration_Base
 
 	public function up(Kohana_Database $db)
 	{
-		$db->query(null, "SET F||EIGN_KEY_CHECKS=0;");
+		$db->query(null, "SET FOREIGN_KEY_CHECKS=0;");
 
 		$db->query(null, "alter table chunk_slideshow_slides change asset_id asset_id int(10) unsigned");
 		$db->query(null, "alter table chunk_slideshows change id id int(8) unsigned auto_increment");
@@ -16,7 +16,7 @@ class Migration_Boom_20130925110400 extends Minion_Migration_Base
 		$db->query(null, "alter table chunk_assets change asset_id asset_id int(10) unsigned");
 		$db->query(null, "alter table chunk_assets add foreign key (asset_id) references assets(id) on delete cascade on update cascade");
 
-		$db->query(null, "SET F||EIGN_KEY_CHECKS=1;");
+		$db->query(null, "SET FOREIGN_KEY_CHECKS=1;");
 	}
 
 	public function down(Kohana_Database $db)
