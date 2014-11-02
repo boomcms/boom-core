1834610a7938b890797727c2b18389f997c0f027
Rob Taylor <rob@uxblondon.com>
2014-11-02 17:01:45 +0000

    Added method Tag\Factory::byId()

--- classes/Boom/Tag/Factory.php
+++ classes/Boom/Tag/Factory.php
@@ -6,6 +6,11 @@ use \Model_Tag as Model_Tag;
 
 abstract class Factory
 {
+    public static function byId($id)
+    {
+        return new Tag(new Model_Tag($id));
+    }
+
     public static function byName($name)
     {
         return new Tag(new Model_Tag(array('name' => $name)));
