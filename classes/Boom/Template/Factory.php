f54e5742a4b04424e8ebf9c77876d18a6aeb30e0
Rob Taylor <rob@uxblondon.com>
2014-11-02 18:00:20 +0000

    Added method Template\Factory::byId()

--- classes/Boom/Template/Factory.php
+++ classes/Boom/Template/Factory.php
@@ -2,10 +2,17 @@
 
 namespace Boom\Template;
 
+use Model_Template;
+
 abstract class Factory
 {
+    public static function byId($id)
+    {
+        return new Template(new Model_Template($id));
+    }
+
     public static function byFilename($filename)
     {
-        return new Template(new \Model_Template(array('filename' => $filename)));
+        return new Template(new Model_Template(array('filename' => $filename)));
     }
 }
