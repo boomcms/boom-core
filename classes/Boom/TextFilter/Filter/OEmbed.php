01db65931e1b78b9c30bf7a2a836bde359a019da
Rob Taylor <rob@uxblondon.com>
2014-11-03 08:43:31 +0000

    Fixed OEmbed text filter

--- classes/Boom/TextFilter/Filter/OEmbed.php
+++ classes/Boom/TextFilter/Filter/OEmbed.php
@@ -6,19 +6,9 @@ use \Embera\Embera as Embera;
 
 class OEmbed implements \Boom\TextFilter\Filter
 {
-    protected function _includeDependencies()
-    {
-        require \Kohana::find_file('vendor', 'embera/Lib/Embera/Autoload');
-    }
-
     public function filterText($text)
     {
-        if ( ! class_exists('Embera')) {
-            $this->_includeDependencies();
-        }
-
         $embera = new Embera();
-
         return $embera->autoEmbed($text);
     }
 }
