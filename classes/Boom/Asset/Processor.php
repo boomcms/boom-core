d9af97d7177edf17ac2759fc8614a12ed3c953fa
Rob Taylor <rob@uxblondon.com>
2014-11-04 21:33:36 +0000

    Added base Asset\Processor class as initial stage in refactoring asset view/download/embed

---
+++ classes/Boom/Asset/Processor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Boom\Asset;
+
+abstract class Processor
+{
+	public function download()
+	{
+
+	}
+
+    public function embed()
+    {
+
+    }
+
+    public function view()
+    {
+
+    }
+
+    public function thumbnail()
+    {
+
+    }
+}
