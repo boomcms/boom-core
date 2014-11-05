2a2a18b05fdda129b3ab32262f3b2ff1e2e48679
Rob Taylor <rob@uxblondon.com>
2014-11-05 13:21:09 +0000

    Fixed page visibility layout

--- views/boom/editor/page/settings/visibility.php
+++ views/boom/editor/page/settings/visibility.php
@@ -1,19 +1,17 @@
 <form class="b-form-settings">
-	<?= Form::hidden('csrf', Security::token()) ?>
+        <label>
+            <?=__('Visible') ?>
+            <?= Form::select('visible', array(1 => 'Yes', 0 => 'No'), (int) $page->isVisibleAtAnyTime(), array('id' => 'b-page-visible')) ?>
+        </label>
 
-	<div>
-		<p>
-			<label for="b-page-visible"><?=__('Visible')?></label>
-			<?= Form::select('visible', array(1 => 'Yes', 0 => 'No'), (int) $page->isVisibleAtAnyTime(), array('id' => 'b-page-visible')) ?>
-		</p>
-		<p>
-			<label for="visible-from"><?=__('Visible from')?></label>
-			<?= Form::input('visible_from', $page->getVisibleFrom()->format('d F Y H:i'), array('id' => 'visible-from', 'class' => 'boom-datepicker')) ?>
-		</p>
-		<p>
-			<label for="toggle-visible"><?=__('Visible until')?></label>
-			<?= Form::checkbox('toggle_visible_to', 1, $page->getVisibleTo()->getTimestamp() != 0, array('id' => 'toggle-visible')) ?>
-			<?= Form::input('visible_to', ($page->getVisibleTo()->getTimestamp())? $page->getVisibleTo()->format('d F Y H:i') : date("d F Y H:i", time()), array('id' => 'visible-to', 'class' => 'boom-datepicker')) ?>
-		</p>
-	</div>
+        <label>
+            <?=__('Visible from')?>
+            <?= Form::input('visible_from', $page->getVisibleFrom()->format('d F Y H:i'), array('id' => 'visible-from', 'class' => 'boom-datepicker')) ?>
+        </label>
+
+        <label>
+            <?=__('Visible until')?>
+            <?= Form::checkbox('toggle_visible_to', 1, $page->getVisibleTo()->getTimestamp() != 0, array('id' => 'toggle-visible')) ?>
+            <?= Form::input('visible_to', ($page->getVisibleTo()->getTimestamp())? $page->getVisibleTo()->format('d F Y H:i') : date("d F Y H:i", time()), array('id' => 'visible-to', 'class' => 'boom-datepicker')) ?>
+        </label>
 </form>
