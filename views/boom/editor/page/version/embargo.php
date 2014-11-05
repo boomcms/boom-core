9e55321c02b07f4ce27ed6f798f68b47e3406f44
Rob Taylor <rob@uxblondon.com>
2014-11-05 13:04:06 +0000

    Refactored page embargo form

--- views/boom/editor/page/version/embargo.php
+++ views/boom/editor/page/version/embargo.php
@@ -1,8 +1,8 @@
-<form class="b-form-settings narrow">
+<form class="b-form-settings">
 	<?= Form::hidden('csrf', Security::token()) ?>
 
-	<p>
-		<label for="page-embargo"><?=__('Embargo until')?></label>
-		<?= Form::input('embargoed_until', ($version->embargoed_until)? date("d F Y h:i", $version->embargoed_until) : date("d F Y h:i", $_SERVER['REQUEST_TIME']), array('class' => 'boom-datepicker', 'id' => 'page-embargo')) ?>
-	</p>
+        <label>
+            <?=__('Embargo until')?>
+            <?= Form::input('embargoed_until', ($version->embargoed_until)? date("d F Y h:i", $version->embargoed_until) : date("d F Y h:i", $_SERVER['REQUEST_TIME']), array('class' => 'boom-datepicker', 'id' => 'page-embargo')) ?>
+        </label>
 </form>
