8b9cda2ade5133320c32292a2547731e8460a539
Rob Taylor <rob@uxblondon.com>
2014-11-06 17:31:52 +0000

    Removed redundant PHP

--- classes/Boom/TextFilter/Filter/PurifyHTML.php
+++ classes/Boom/TextFilter/Filter/PurifyHTML.php
@@ -6,20 +6,11 @@ class PurifyHTML implements \Boom\TextFilter\Filter
 {
     public function filterText($text)
     {
-        if ( ! class_exists('HTMLPurifier')) {
-            $this->_includeDependencies();
-        }
-
         $config = \HTMLPurifier_Config::createDefault();
         $config->loadArray(\Kohana::$config->load('htmlpurifier'));
 
         $purifier = new \HTMLPurifier($config);
 
         return $purifier->purify($text);
-    }
-
-    protected function _includeDependencies()
-    {
-        require \Kohana::find_file('vendor', 'htmlpurifier/library/HTMLPurifier.auto');
     }
 }
