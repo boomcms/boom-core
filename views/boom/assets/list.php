b3f02ef1678ca0aa50827450bed55b788edfb054
Rob Taylor <rob@uxblondon.com>
2014-11-06 13:30:25 +0000

    Show asset manager pagination even if there's only one page

--- views/boom/assets/list.php
+++ views/boom/assets/list.php
@@ -1,12 +1,11 @@
 <div id="b-assets-content">
 	<?= new View('boom/assets/thumbs', get_defined_vars()) ?>
 </div>
+
 <div id="b-assets-pagination" class="pagination">
-	<?php if ($pages > 1): ?>
-		<a href="#" class="first" data-action="first">&laquo;</a>
-		<a href="#" class="previous" data-action="previous">&lsaquo;</a>
-		<input type="text" readonly="readonly" data-max-page="<?= $pages ?>" data-current-page="<?= $page ?>" />
-		<a href="#" class="next" data-action="next">&rsaquo;</a>
-		<a href="#" class="last" data-action="last">&raquo;</a>
-	<?php endif ?>
+    <a href="#" class="first" data-action="first">&laquo;</a>
+    <a href="#" class="previous" data-action="previous">&lsaquo;</a>
+    <input type="text" readonly="readonly" data-max-page="<?= $pages ?>" data-current-page="<?= $page ?>" />
+    <a href="#" class="next" data-action="next">&rsaquo;</a>
+    <a href="#" class="last" data-action="last">&raquo;</a>
 </div>
