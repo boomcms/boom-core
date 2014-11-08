ad41c2655e1be26dcecc79292bcdf1a510d1b7ad
Rob Taylor <rob@uxblondon.com>
2014-11-08 16:37:03 +0000

    Added text instructions to link picker

--- views/boom/editor/slot/insert_link.php
+++ views/boom/editor/slot/insert_link.php
@@ -1,18 +1,26 @@
 <div class="boom-tabs b-linkpicker">
 	<ul>
-		<li><a href="#b-linkpicker-add-internal">Internal link</a></li>
-		<li><a href="#b-linkpicker-add-external">External link</a></li>
+		<li><a href="#b-linkpicker-add-internal">Select page</a></li>
+		<li><a href="#b-linkpicker-add-external">Enter URL</a></li>
 	</ul>
 
 	<div id="b-linkpicker-add-internal">
 		<input type="hidden" name="parent_id" value="">
 
-                <ul class="boom-tree">
-                    <li><a id="page_5" href="/" rel="5">Home</a></li>
-                </ul>
+        <p>
+            Select a page from the CMS page tree.
+        </p>
+
+        <ul class="boom-tree">
+            <li><a id="page_5" href="/" rel="5">Home</a></li>
+        </ul>
 	</div>
 
 	<div id="b-linkpicker-add-external">
+        <p>
+            Manually enter an internal or external URL.
+        </p>
+
 		<form action="#">
 			<select>
 				<option value="http" selected="selected">Website</option>
