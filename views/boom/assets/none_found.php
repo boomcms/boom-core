f0bafe3e0856df6c08b7f9f7a28e01a040f652bc
Rob Taylor <rob@uxblondon.com>
2014-11-11 13:08:49 +0000

    Fixed asset manager crashing when there's no assets. Closes #3

--- views/boom/assets/none_found.php
+++ views/boom/assets/none_found.php
@@ -1,5 +1,13 @@
-<? /* Displayed by /cms/assets/lists when no assets were found for the specified tags / filters */ ?>
+<div id="b-assets-content">
+	<div id="b-assets-none">
+            <?= __('No assets to show') ?>.
+        </div>
+</div>
 
-<div id="b-assets-none">
-	<?= __('No assets to show') ?>.
+<div id="b-assets-pagination" class="pagination">
+    <a href="#" class="first" data-action="first">&laquo;</a>
+    <a href="#" class="previous" data-action="previous">&lsaquo;</a>
+    <input type="text" readonly="readonly" data-max-page="0" data-current-page="0" />
+    <a href="#" class="next" data-action="next">&rsaquo;</a>
+    <a href="#" class="last" data-action="last">&raquo;</a>
 </div>
