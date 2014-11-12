87fc972ec90c49386a1a1f9b16fdd6941ada8bba
Rob Taylor <rob@uxblondon.com>
2014-11-12 15:44:15 +0000

    Use DOMDocument (and a lot of faff) to only embed URLs which appear in a paragraph by themselves. Fixes #6

--- classes/Boom/TextFilter/Filter/OEmbed.php
+++ classes/Boom/TextFilter/Filter/OEmbed.php
@@ -2,14 +2,52 @@
 
 namespace Boom\TextFilter\Filter;
 
-use \Embera\Embera as Embera;
+use \Embera\Embera;
+use DOMDocument;
 
 class OEmbed implements \Boom\TextFilter\Filter
 {
+    /**
+     *
+     * @var Embera 
+     */
+    private $embera;
+
+    /**
+     *
+     * @var DOMDocument
+     */
+    private $dom;
+
+    public function __construct()
+    {
+        $this->embera = new Embera();
+        $this->dom = new DOMDocument;
+    }
+
+
     public function filterText($text)
     {
-        $embera = new Embera();
+        $this->dom->loadHTML(mb_convert_encoding($text, 'HTML-ENTITIES', 'UTF-8'));
 
-        return $embera->autoEmbed($text);
+        foreach ($this->dom->getElementsByTagName('p') as $p) {
+            $textContent = preg_replace('|\s|', '', $p->textContent);
+
+            if ($p->getElementsByTagName('a')->length == 0 && filter_var($textContent, FILTER_VALIDATE_URL) !== false) {
+                $embed = $this->embera->autoEmbed($p->textContent);
+
+                if ($embed !== $p->textContent) {
+                    $html = new DOMDocument();
+                    $html->loadHTML($embed);
+
+                    foreach ($html->getElementsByTagName('iframe') as $el) {
+                        $el = $this->dom->importNode($el);
+                        $p->parentNode->replaceChild($el, $p);
+                    }
+                }
+            }
+        }
+
+        return $this->dom->saveHtml();
     }
 }
