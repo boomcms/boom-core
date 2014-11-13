4bafc0c7ba79ba6b678616b9e58d9ee1edd26ea9
Rob Taylor <rob@uxblondon.com>
2014-11-13 10:37:17 +0000

    Fixed boomAlert.done() not working

--- media/boom/js/boom/alert.js
+++ media/boom/js/boom/alert.js
@@ -1,16 +1,13 @@
 function boomAlert(message) {
-	this.deferred = new $.Deferred();
 	this.message = message;
 
 	boomAlert.prototype.open = function() {
 		var alert = this;
 
-		new boomDialog({
+		return new boomDialog({
 			msg : alert.message,
 			cancelButton : false
 		});
-
-		return this.deferred;
 	};
 
 	return this.open();
