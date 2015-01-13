732bdd4df1c4c04e7a1fcd28015e319c886b3a5f
Rob Taylor <rob@uxblondon.com>
2015-01-13 12:43:54 +0000

    Deleted commented code in asset finder

--- classes/Boom/Asset/Finder.php
+++ classes/Boom/Asset/Finder.php
@@ -32,28 +32,6 @@ class Finder extends Boom\Finder\Finder
         return new Finder\Result($assets);
     }
 
-    /**
-	 *
-	 * @return array
-	 */
-//	public function get_count_and_total_size()
-//	{
-//		$query = clone $this->_query;
-//		$query->reset();
-//
-//		$this->_applyFilters($query);
-//
-//		$result = $query
-//			->select(array(DB::expr('sum(filesize)'), 'filesize'))
-//			->select(array(DB::expr('count(*)'), 'count'))
-//			->find();
-//
-//		return array(
-//			'count' => $result->get('count'),
-//			'filesize' => $result->get('filesize')
-//		);
-//	}
-
     public function setOrderBy($field, $direction = null)
     {
         in_array($field, $this->_allowedOrderByColumns) || $field = 'title';
