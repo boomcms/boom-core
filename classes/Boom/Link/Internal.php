02b3b987645ad36bc462f053be0e38a8fe20a3e7
Rob Taylor <rob@uxblondon.com>
2015-02-05 14:07:53 +0000

    Bugfix: added ability to link to internal pages with a query string

--- classes/Boom/Link/Internal.php
+++ classes/Boom/Link/Internal.php
@@ -7,10 +7,22 @@ use Boom\Page;
 class Internal extends Link
 {
     /**
-	 *
-	 * @var Page\Page
-	 */
+     *
+     * @var Page\Page
+     */
     protected $page;
+
+    /**
+     * Holds an optional query string for the link.
+     *
+     * If the URL text entry of the URL picker is used then a query string can be provided (e.g. for linking to a search results page)
+     *
+     * When this is done the query string needs to be ignored when finding the page with the provided URL.
+     * And then appended to the page URL when the link is used.
+     *
+     * @var string
+     */
+    protected $queryString;
 
     public function __construct($link)
     {
@@ -18,7 +30,12 @@ class Internal extends Link
             $this->page = Page\Factory::byId($link);
         } else {
             $location = ($link === '/') ? '' : substr($link, 1);
-            $this->page = Page\Factory::byUri($location);
+            
+            // Extract the query string, if there is one.
+            $this->queryString = parse_url($link, PHP_URL_QUERY);
+
+            // Only get the path of the URL to ensure that if there's a query string it's ignored.
+            $this->page = Page\Factory::byUri(parse_url($location, PHP_URL_PATH));
         }
     }
 
@@ -34,6 +51,7 @@ class Internal extends Link
 
     public function url()
     {
-        return $this->page->url();
+        // Return the URL of the page and append the query string if one was provided.
+        return $this->page->url() . (($this->queryString)? '?' . $this->queryString : '');
     }
 }
