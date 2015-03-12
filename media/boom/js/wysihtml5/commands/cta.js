7380c1314d68711c518ff8f780122364c96a2747
Rob Taylor <rob@uxblondon.com>
2015-03-12 12:31:38 +0000

    Updated the CTA command

--- media/boom/js/wysihtml5/commands/cta.js
+++ media/boom/js/wysihtml5/commands/cta.js
@@ -3,21 +3,19 @@
  *
  */
 (function(wysihtml5) {
-  var CLASS_NAME  = "cta",
-      REG_EXP     = /cta/g;
+	var nodeOptions = {
+		className: "cta",
+		classRegExp: /cta/g,
+		toggle: true
+	};
 
 	wysihtml5.commands.cta = {
 		exec: function(composer, command) {
-			if (wysihtml5.commands.cta.state(composer, command)) {
-				wysihtml5.commands.formatBlock.exec(composer, "formatBlock", 'p', CLASS_NAME, REG_EXP);
-				return wysihtml5.commands.formatBlock.exec(composer, "formatBlock", 'p');
-			} else {
-				return wysihtml5.commands.formatBlock.exec(composer, "formatBlock", 'p', CLASS_NAME, REG_EXP);
-			}
+			return wysihtml5.commands.formatBlock.exec(composer, "formatBlock", nodeOptions);
 		},
 
 		state: function(composer, command) {
-			return wysihtml5.commands.formatBlock.state(composer, "formatBlock", 'p', CLASS_NAME, REG_EXP);
+			return wysihtml5.commands.formatBlock.state(composer, "formatBlock", nodeOptions);
 		}
 	};
 })(wysihtml5);
