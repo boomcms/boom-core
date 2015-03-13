e7f358967a5fdade4e951b2eba65bb80b8e1233c
Rob Taylor <rob@uxblondon.com>
2015-03-13 11:11:26 +0000

    Updated page provider to use eloquent models

--- src/classes/Boom/Page/Factory.php
+++ src/classes/Boom/Page/Factory.php
@@ -2,21 +2,24 @@
 
 namespace Boom\Page;
 
+use Boom\Model\Page as Model;
+use Boom\Page\Finder;
+
 class Provider
 {
     public function findById($id)
     {
-        return new Page(new \Model_Page(['id' => $id, 'deleted' => false]));
+        return new Page(Model::find($id));
     }
 
     public function findByInternalName($name)
     {
-        return new Page(new \Model_Page(['internal_name' => $name, 'deleted' => false]));
+        return new Page(Model::where('internal_name', '=', $name)->get());
     }
 
     public function findByPrimaryUri($uri)
     {
-        return new Page(new \Model_Page(['primary_uri' => $uri, 'deleted' => false]));
+        return new Page(Model::where('primary_uri', '=', $uri)->get());
     }
 
     public function findByUri($uri)
