c75e69c8e842b8d41a473e4629f9a57d433c8ec3
Rob Taylor <rob@uxblondon.com>
2015-04-23 17:19:42 +0100

    Deleted asset-merger config files

--- config/asset-merger.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'load_paths' => array(
-        Assets::JAVASCRIPT => array(__DIR__ . '/../media/boom/js/', __DIR__ . '/../bower_components/'),
-        Assets::STYLESHEET => array(__DIR__.'/../media/boom/css/', __DIR__ . '/../bower_components/'),
-    ),
-    'dev' => Boom\Boom::instance()->getEnvironment()->isDevelopment(),
-);
