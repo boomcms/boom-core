b985e346bcd2a5b09a70ca24ddeacae43a2361a1
Rob Taylor <rob@uxblondon.com>
2015-04-23 15:11:13 +0100

    Manage JS dependencies with bower

--- config/asset-merger.php
+++ config/asset-merger.php
@@ -2,8 +2,8 @@
 
 return array(
     'load_paths' => array(
-        Assets::JAVASCRIPT => array(__DIR__ . '/../media/boom/js/'),
-        Assets::STYLESHEET => array(__DIR__.'/../media/boom/css/'),
+        Assets::JAVASCRIPT => array(__DIR__ . '/../media/boom/js/', __DIR__ . '/../bower_components/'),
+        Assets::STYLESHEET => array(__DIR__.'/../media/boom/css/', __DIR__ . '/../bower_components/'),
     ),
     'dev' => Boom\Boom::instance()->getEnvironment()->isDevelopment(),
 );
