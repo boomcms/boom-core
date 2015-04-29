92fd2b39a4bed7354afcbe045c1d4c092474f712
Rob Taylor <rob@uxblondon.com>
2015-04-29 16:43:49 +0100

    ProcessSiteURL middleware

---
+++ src/BoomCMS/Core/Http/Middleware/ProcessSiteURL.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace BoomCMS\Core\Http\Middleware;
+
+use Closure;
+use BoomCMS\Core\Page\Provider;
+use BoomCMS\Core\Editor\Editor;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Illuminate\Http\Request;
+
+class ProcessSiteURL
+{
+    /**
+     *
+     * @var Provider
+     */
+    protected $pageProvider;
+
+    public function __construct(Provider $pageProvider, Editor $editor)
+    {
+        $this->pageProvider = $pageProvider;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        // TODO: 2015/04/29: findByUri won't find a deleted page. Need to make it an option.
+        $page = $this->pageProvider->findByUri($request->path());
+
+         if ( ! $page->loaded()) {
+             throw new NotFoundHttpException;
+         }
+
+        if ($page->isDeleted()) {
+            throw new GoneHttpException;
+        }
+
+        if ($this->editor->isDisabled() && ! $page->isVisible()) {
+            throw new NotFoundHttpException;
+        }
+
+        if ($page->url()->location !== $request->path) {
+            redirect($page->url(), 301);
+        }
+
+        // TODO: 2015/04/29 Need a way of passing the page to the controller
+
+        return $next($request);
+    }
+}
