c6c311e1588b9a82dfe9e98e8610880b91ac1da8
Rob Taylor <rob@uxblondon.com>
2015-04-29 15:37:25 +0100

    Updated spacing in views/boom/header.php

--- src/views/boom/header.php
+++ src/views/boom/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en" class="boom">
-<head>
-	<title><?= $title; ?></title>
-	<meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-	<meta http-equiv="Content-Style-Type" content="text/css" />
+    <head>
+        <title><?= $title; ?></title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta http-equiv="Content-Style-Type" content="text/css" />
 
         <link rel="stylesheet" type="text/css" href="/media/boom/css/cms.css" />
-</head>
-<body>
+    </head>
+    <body>
