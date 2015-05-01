7fa9e0e74eaa59fcf4ed8505c1bc54af88e7460a
Rob Taylor <rob@uxblondon.com>
2015-05-01 21:47:18 +0100

    Updated routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'cms', 'namespace' => 'Boom\Core'], function() {
+Route::group(['prefix' => 'cms', 'namespace' => 'BoomCMS\Core'], function() {
     Route::group(array('middleware' => ['BoomCMS\Core\Http\Middleware\RedirectIfAuthenticated']), function() {
         Route::get('login', [
             'as' => 'login',
@@ -12,7 +12,7 @@ Route::group(['prefix' => 'cms', 'namespace' => 'Boom\Core'], function() {
         Route::post('login', 'Cms_Auth_LoginController');
 
         Route::get('logout', 'Cms_Auth_LogoutController@index');
-        
+
         Route::get('recover', 'Cms_Auth_RecoverController@showForm');
         Route::get('recover', 'Cms_Auth_RecoverController@createToken');
         Route::get('recover', 'Cms_Auth_RecoverController@setNewPassword');
