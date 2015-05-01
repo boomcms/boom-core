cc05c32bfa11b7aa64520f24b354055281c08c38
Rob Taylor <rob@uxblondon.com>
2015-05-01 21:46:20 +0100

    Updated the core service provider

--- src/BoomCMS/Core/CoreServiceProvider.php
+++ src/BoomCMS/Core/CoreServiceProvider.php
@@ -4,43 +4,24 @@ namespace BoomCMS\Core;
 
 use Illuminate\Support\ServiceProvider;
 
-class CoreServiceProvider extends ServiceProvider {
-
+class CoreServiceProvider extends ServiceProvider
+{
 	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Bootstrap the application events.
+	 * Bootstrap any application services.
 	 *
 	 * @return void
 	 */
 	public function boot()
 	{
-		$this->package('BoomCMS/core');
+		//
 	}
 
 	/**
-	 * Register the service provider.
 	 *
 	 * @return void
 	 */
 	public function register()
 	{
-		//
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
 	}
 
 }
