7ceef2c8e174549961815e3a8d4c1b14f80f5b1d
Rob Taylor <rob@uxblondon.com>
2015-05-05 11:45:58 +0100

    Revert "Fixed incorrect column names in chunk tag model"
    This reverts commit 8bdd912ed117e662077a6162241a1e6faf2f2d1d.

--- classes/Model/Chunk/Tag.php
+++ classes/Model/Chunk/Tag.php
@@ -5,7 +5,7 @@ class Model_Chunk_Tag extends \ORM
     protected $_table_columns = [
         'id' => '',
         'slotname'    => '',
-        'tag_id' => '',
+        'tag' => '',
         'page_vid' => '',
     ];
 
