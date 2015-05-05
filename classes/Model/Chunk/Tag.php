8bdd912ed117e662077a6162241a1e6faf2f2d1d
Rob Taylor <rob@uxblondon.com>
2015-05-05 10:50:44 +0100

    Fixed incorrect column names in chunk tag model

--- classes/Model/Chunk/Tag.php
+++ classes/Model/Chunk/Tag.php
@@ -5,7 +5,7 @@ class Model_Chunk_Tag extends \ORM
     protected $_table_columns = [
         'id' => '',
         'slotname'    => '',
-        'tag' => '',
+        'tag_id' => '',
         'page_vid' => '',
     ];
 
