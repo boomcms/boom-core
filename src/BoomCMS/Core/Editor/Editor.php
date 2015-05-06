e2f13c48ee6ad6f9eb886e7569c9b0f7b087d592
Rob Taylor <rob@uxblondon.com>
2015-05-06 11:25:43 +0100

    Added a HTTP middle to insert the CMS editor HTML

--- src/BoomCMS/Core/Editor/Editor.php
+++ src/BoomCMS/Core/Editor/Editor.php
@@ -38,31 +38,6 @@ class Editor
     }
 
     /**
-	 * Insert the editor iframe into a block of HTML
-	 *
-	 * @param	string	$html
-	 * @param	string	$page_id
-	 * @return	string
-	 */
-    public function insert($html, $page_id)
-    {
-        // Find the body tag in the HTML. We need to take into account that the body may have attributes assigned to it in the HTML.
-        preg_match("|(.*)(</head>)(.*<body[^>]*>)|imsU", $html, $matches);
-
-        if ( ! empty($matches)) {
-            $head = new \View('boom/editor/iframe', [
-                'before_closing_head' => $matches[1],
-                'body_tag'    =>    $matches[3],
-                'page_id'    =>    $page_id,
-            ]);
-
-            $html = str_replace($matches[0], $head->render(), $html);
-        }
-
-        return $html;
-    }
-
-    /**
 	 *
 	 * @return Editor
 	 */
---
+++ src/BoomCMS/Core/Http/Middleware/InsertCMSToolbar.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace BoomCMS\Core\Http\Middleware;
+
+use Closure;
+use BoomCMS\Core\Environment;
+use Illuminate\Http\RedirectResponse;
+
+class InsertCMSToolbar
+{
+    /**
+     *
+     * @var Environment
+     */
+    protected $environment;
+
+    public function __construct(Environment $environment)
+    {
+        $this->environment = $environment;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+
+        $originalHtml = $response->getOriginalContent();
+        preg_match("|(.*)(</head>)(.*<body[^>]*>)|imsU", $originalHtml, $matches);
+
+        if ( ! empty($matches)) {
+            $head = new \View('boom/editor/iframe', [
+                'before_closing_head' => $matches[1],
+                'body_tag'    =>    $matches[3],
+                'page_id'    =>    $request->route()->getParameter('boom.currentPage')->getId(),
+            ]);
+
+            $newHtml = str_replace($matches[0], $head->render(), $originalHtml);
+            $response->setContent($newHtml);
+        }
+
+        return $response;
+    }
+
+}
