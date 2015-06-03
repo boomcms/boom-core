8f6256f8c212b8efc75466713cf047eddc60720e
Rob Taylor <rob@uxblondon.com>
2015-06-03 21:21:01 +0100

    Replaced Kohana default app files with Laravel

---
+++ app/Http/Kernel.php
@@ -0,0 +1,21 @@
+<?php namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel {
+
+	/**
+	 * The application's global HTTP middleware stack.
+	 *
+	 * @var array
+	 */
+	protected $middleware = [
+		'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
+		'Illuminate\Cookie\Middleware\EncryptCookies',
+		'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
+		'Illuminate\Session\Middleware\StartSession',
+		'Illuminate\View\Middleware\ShareErrorsFromSession',
+		'App\Http\Middleware\VerifyCsrfToken',
+	];
+
+}
