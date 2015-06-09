b1980a40ca4663c7e85cae0fe525299d6449cb9d
Rob Taylor <rob@uxblondon.com>
2015-06-09 14:45:47 +0100

    Added tests for Person\Person->loaded()

--- tests/Boom/Person/Person.php
+++ tests/Boom/Person/Person.php
@@ -4,6 +4,20 @@ use BoomCMS\Core\Person\Person;
 
 class Person_PersonTest extends TestCase
 {
+    public function testLoadedIfHasId()
+    {
+        $person = new Person(['id' => 2]);
+
+        $this->assertTrue($person->loaded());
+    }
+
+    public function testNotLoadedIfNoId()
+    {
+        $person = new Person([]);
+
+        $this->assertFalse($person->loaded());
+    }
+
     public function testIsSuperuserDefaultFalse()
     {
         $person = new Person([]);
