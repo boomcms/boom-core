5bc92e2d762fddd80856d49759569ef9a1549f10
Rob Taylor <rob@uxblondon.com>
2015-06-09 16:54:04 +0100

    Updated ProcessSiteURL to throw gone expection if the URL exists in the DB but the page wasn't found (page has been deleted)

--- src/BoomCMS/Core/Http/Middleware/ProcessSiteURL.php
+++ src/BoomCMS/Core/Http/Middleware/ProcessSiteURL.php
@@ -5,8 +5,9 @@ namespace BoomCMS\Core\Http\Middleware;
 use Closure;
 use BoomCMS\Core\Page\Provider;
 use BoomCMS\Core\Editor\Editor;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use BoomCMS\Core\URL\Helpers as BoomURL;
 
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 
@@ -39,16 +40,19 @@ class ProcessSiteURL
      */
     public function handle(Request $request, Closure $next)
     {
-        // TODO: 2015/04/29: findByUri won't find a deleted page. Need to make it an option.
-        $page = $this->pageProvider->findByUri($request->route()->getParameter('location'));
+        $uri = $request->route()->getParameter('location');
+        $page = $this->pageProvider->findByUri($uri);
 
          if ( !$page->loaded()) {
+             // Check whether the URL exists in the DB.
+             // If so the page has been deleted.
+             // If not the page never existed.
+             if ( !BoomURL::isAvailable($uri)) {
+                 throw new GoneHttpException();
+             }
+             
              throw new NotFoundHttpException();
          }
-
-        if ($page->isDeleted()) {
-            throw new GoneHttpException();
-        }
 
         if ($this->editor->isDisabled() && ! $page->isVisible()) {
             throw new NotFoundHttpException();
