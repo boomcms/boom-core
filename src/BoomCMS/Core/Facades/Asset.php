b24ae17d51084ed5b0b4291ec268f03a205859ac
Rob Taylor <rob@uxblondon.com>
2015-06-11 10:22:21 +0100

    Added Asset Facade

---
+++ src/BoomCMS/Core/Facades/Asset.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BoomCMS\Core\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Asset extends Facade {
+
+    protected static function getFacadeAccessor()
+    {
+        return 'boomcms.asset.provider';
+    }
+}
