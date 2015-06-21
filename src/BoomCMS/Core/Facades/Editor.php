951e0e76959a94026b8d3bcb67263c76c6f45f59
Rob Taylor <rob@uxblondon.com>
2015-06-21 22:49:33 +0100

    Added Editor facade

---
+++ src/BoomCMS/Core/Facades/Editor.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BoomCMS\Core\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Editor extends Facade {
+
+    protected static function getFacadeAccessor()
+    {
+        return 'boomcms.editor';
+    }
+}
