de26fc5897ffdc84520906637e3ee545bd7517fb
Rob Taylor <rob@uxblondon.com>
2015-06-21 22:50:17 +0100

    Added NextTo page finder filter

---
+++ src/BoomCMS/Core/Page/Finder/NextTo.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BoomCMS\Core\Page\Finder;
+
+use BoomCMS\Core\Page\Page;
+use BoomCMS\Core\Finder\Filter;
+
+use Illuminate\Database\Eloquent\Builder;
+
+class NextTo extends Filter
+{
+    /**
+     *
+     * @var Page
+     */
+    protected $currentPage;
+
+    /**
+     *
+     * @var string
+     */
+    protected $direction;
+
+    public function __construct(Page $currentPage, $direction)
+    {
+        $this->currentPage = $currentPage;
+        $this->direction = $direction;
+    }
+
+    public function execute(Builder $query)
+    {
+        $order = $this->currentPage->getParent()->getChildOrderingPolicy();
+
+        if (
+            ($this->direction === 'before' && $order->getDirection() === 'asc') ||
+            ($this->direction === 'after' && $order->getDirection() === 'desc')
+        ) {
+            $operator = '<';
+            $direction = 'desc';
+        } else {
+            $operator = '>';
+            $direction = 'asc';
+        }
+
+        return $query
+            ->where($order->getColumn(), $operator . '=', $this->currentPage->{$order->getAccessor()}())
+            ->where('pages.id', $operator, $this->currentPage->getId())
+            ->limit(1)
+            ->orderBy($order->getColumn(), $direction);
+    }
+}
