b7407dcfec64e97b320458064a0cebd3b26a9f3f
Rob Taylor <rob@uxblondon.com>
2015-06-22 15:26:22 +0100

    Added unique index to people(email)

---
+++ src/database/migrations/2015_06_22_152000_add_unique_index_to_people_email.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddUniqueIndexToPeopleEmail extends Migration
+{
+    /**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+    public function up()
+    {
+		Schema::table('people', function($table)
+		{
+			$table->unique('email');
+		});
+    }
+
+    /**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+    public function down()
+    {            
+    }
+}
