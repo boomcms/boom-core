8c3fb132c18a1566334e9632d4057ac0336795b4
Rob Taylor <rob@uxblondon.com>
2015-06-25 11:06:30 +0100

    Removed redundant string function

--- src/js/string.js
+++ src/js/string.js
@@ -1,22 +1,4 @@
 /**
-convert 8 bit characters to their 7 bit equivalent
-@function
-*/
-String.prototype.safeEscape = function() {
-	var str = $.trim(this),
-	replacements = {
-		"\xa0": " ", "\xb7": "*", "\u2018": "'", "\u2019": "'",	"\u2026": "...", "\u2002": " ", "\u2003": " ", "\u2009": " ", "\u2012": "-", "\u2013": "-", "\u2014": "-", "\u2015": "-",
-		"\u201c": '"',	// smart quote
-		"\u201d": '"'	// smart quote
-	};
-
-	for (key in replacements)
-		str = str.replace(new RegExp(key, 'g'), replacements[key]);
-
-	return encodeURIComponent(str).replace(/%20/g, '+').toString();
-};
-
-/**
 @function
 */
 String.prototype.ucfirst = function() {
