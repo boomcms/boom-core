0ed76937338e5ac47b55d2d3ca6aefc42f4b4db4
Rob Taylor <rob@uxblondon.com>
2015-07-19 17:39:10 +0100

    Updated Http Kernel to extend Boom foundation HTTP Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -1,23 +1,7 @@
-<?php namespace App\Http;
+<?php
 
-use Illuminate\Foundation\Http\Kernel as HttpKernel;
+namespace App\Http;
 
-class Kernel extends HttpKernel
-{
-    /**
-     * The application's global HTTP middleware stack.
-     *
-     * @var array
-     */
-    protected $middleware = [
-        'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
-        'Illuminate\Cookie\Middleware\EncryptCookies',
-        'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
-        'Illuminate\Session\Middleware\StartSession',
-        'Illuminate\View\Middleware\ShareErrorsFromSession',
-        'App\Http\Middleware\VerifyCsrfToken',
-        'BoomCMS\Core\Http\Middleware\AutoLogin',
-        'BoomCMS\Core\Http\Middleware\DefineGlobalViewSharedVariables',
-    ];
+use BoomCMS\Foundation\Http\Kernel as HttpKernel;
 
-}
+class Kernel extends HttpKernel {}
