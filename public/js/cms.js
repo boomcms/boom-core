2dd3ac4e934a254dfd03b0a91c8fee7c6eacb903
Rob Taylor <rob@uxblondon.com>
2015-07-23 12:02:43 +0100

    Protocol relative URL for location chunk search
    Conflicts:
    	public/js/cms.min.js
    	public/js/cms.min.js.map

--- public/js/cms.js
+++ public/js/cms.js
@@ -36584,7 +36584,7 @@ $.widget('ui.chunkPageVisibility', {
 	boomChunkLocationEditor.prototype.bind = function() {
 		var locationEditor = this;
 
-		L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+		L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
 			subdomains: ['otile1','otile2','otile3','otile4']
 		}).addTo(this.map);
@@ -36640,7 +36640,7 @@ $.widget('ui.chunkPageVisibility', {
 	};
 
 	boomChunkLocationEditor.prototype.geocode = function(location) {
-		return $.get('http://nominatim.openstreetmap.org/search', {
+		return $.get('//nominatim.openstreetmap.org/search', {
 			q: location,
 			format: 'json'
 		});
@@ -36706,7 +36706,8 @@ $.widget('ui.chunkPageVisibility', {
 	};
 
 	return this.open();
-};;/**
+};
+;/**
  * TODO: Tell someone off for trying to blank a page title or writing an essay in the title.
  */
 $.widget('boom.pageTitle', $.ui.chunk, {
--- src/js/boom/chunk/location/editor.js
+++ src/js/boom/chunk/location/editor.js
@@ -6,7 +6,7 @@ function boomChunkLocationEditor(page_id, slotname) {
 	boomChunkLocationEditor.prototype.bind = function() {
 		var locationEditor = this;
 
-		L.tileLayer( 'http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
+		L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
 			subdomains: ['otile1','otile2','otile3','otile4']
 		}).addTo(this.map);
@@ -62,7 +62,7 @@ function boomChunkLocationEditor(page_id, slotname) {
 	};
 
 	boomChunkLocationEditor.prototype.geocode = function(location) {
-		return $.get('http://nominatim.openstreetmap.org/search', {
+		return $.get('//nominatim.openstreetmap.org/search', {
 			q: location,
 			format: 'json'
 		});
