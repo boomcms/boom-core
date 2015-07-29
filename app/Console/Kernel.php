db4147b282d54aae61daa601f3dd935dbfffd299
Rob Taylor <rob@uxblondon.com>
2015-07-29 11:58:07 +0100

    Updated file locations for available commands and service providers

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        'BoomCMS\Core\Console\Commands\InstallTemplates',
+        'BoomCMS\Console\Commands\InstallTemplates',
     ];
 
     /**
