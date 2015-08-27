63aa0c37445bc5aedf6affdf62d291eeceb0462d
Rob Taylor <rob@uxblondon.com>
2015-08-27 15:14:39 +0100

    Added migration to update asset links in text chunks to new URL format

---
+++ src/database/migrations/2015_08_27_150600_update_asset_links.php
@@ -0,0 +1,32 @@
+<?php
+
+use BoomCMS\Database\Models\Chunk\Text;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateAssetLinks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $chunks = Text::where('text', 'like', '%/asset/%')
+            ->get();
+        
+        foreach ($chunks as $chunk) {
+            $chunk->text = preg_replace('|(/asset/)([a-z]+)/([/0-9]+)|', '$1$3/$2', $chunk->text);
+            $chunk->text = preg_replace('|(/asset/)([/0-9]+)/view|', '$1$2', $chunk->text);
+            $chunk->save();
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {}
+}
