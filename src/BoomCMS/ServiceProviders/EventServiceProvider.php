90ea5f2325560d0cc09999d66364339eacc801c1
Rob Taylor <rob@uxblondon.com>
2015-10-08 10:12:32 +0100

    Bugfix: Listener for PageSearchSettingsWereUpdated event not being called

--- src/BoomCMS/ServiceProviders/EventServiceProvider.php
+++ src/BoomCMS/ServiceProviders/EventServiceProvider.php
@@ -16,7 +16,7 @@ class EventServiceProvider extends ServiceProvider
         'BoomCMS\Events\Auth\SuccessfulLogin' => [
             'BoomCMS\Listeners\ResetFailedLogins',
         ],
-        'BoomCMS\Listeners\PageSearchSettingsWereUpdated' => [
+        'BoomCMS\Events\PageSearchSettingsWereUpdated' => [
             'BoomCMS\Listeners\SaveSearchText',
         ],
         'BoomCMS\Events\PageTitleWasChanged' => [
