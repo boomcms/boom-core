d039b4efc89d5239f1f847b717971b5f28a34878
Rob Taylor <rob@uxblondon.com>
2015-10-24 13:27:36 +0100

    Fixed typo in Gruntfile.js

--- public/js/cms.js
+++ public/js/cms.js
@@ -45694,7 +45694,40 @@ function Row() {
 	};
 	
 	return this.open();
-};;// TODO: in future try to replace most inline compability checks with polyfills for code readability 
+};;$(document).ready(function() {
+	$('.b-approvals-publish').on('click', function(e) {
+		e.preventDefault();
+
+		var $this = $(this), page = boom_approvals_get_page($this);
+		page.publish();
+
+		new boomNotification('All changes to this page are now published');
+		boom_approvals_remove_row($this);
+	});
+
+	$('.b-approvals-reject').on('click', function(e) {
+		e.preventDefault();
+
+		var $this = $(this), page = boom_approvals_get_page($this);
+
+		page
+			.revertToPublished()
+			.done(function() {
+				new boomNotification("This page has been reverted to it's most recent published version.");
+				boom_approvals_remove_row($this);
+			});
+	});
+
+	function boom_approvals_get_page($element) {
+		var page_id = $element.parents('tr').attr('data-page-id');
+
+		return new boomPage(page_id);
+	}
+
+	function boom_approvals_remove_row($element) {
+		$element.parents('tr').remove();
+	}
+});;// TODO: in future try to replace most inline compability checks with polyfills for code readability 
 
 // IE8 SUPPORT BLOCK
 // You can compile wuthout all this if IE8 is not needed
