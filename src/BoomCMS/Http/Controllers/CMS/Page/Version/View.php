f6a0888ecc2d6da14b5c0bd5c64c3407a7b64111
Rob Taylor <rob@uxblondon.com>
2015-11-27 16:58:11 +0000

    Refactored page template settings using ->is() method

--- src/BoomCMS/Http/Controllers/CMS/Page/Version/View.php
+++ src/BoomCMS/Http/Controllers/CMS/Page/Version/View.php
@@ -34,7 +34,7 @@ class View extends Version
         $templates = $manager->getValidTemplates();
 
         return ViewFacade::make("$this->viewPrefix.template", [
-            'template_id' => $this->page->getCurrentVersion()->getTemplateId(),
+            'current' => $this->page->getTemplate(),
             'templates'   => $templates,
         ]);
     }
