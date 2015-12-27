f1b8e6fbdb764177db3f5d3e05816dcb7fb69e08
Rob Taylor <rob@uxblondon.com>
2015-12-27 20:59:07 +0000

    Added skeleton policy classes

---
+++ src/BoomCMS/Foundation/Policies/BasePolicy.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace BoomCMS\Foundation\Policies;
+
+abstract class BasePolicy
+{
+    public function __call($name, $arguments)
+    {
+        ;
+    }
+}
---
+++ src/BoomCMS/Policies/SitePolicy.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace BoomCMS\Policies;
+
+class SitePolicy
+{
+
+}
