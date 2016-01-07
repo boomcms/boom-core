41d59ad2e18eaff5b0672c207ce860790c57d85b
Rob Taylor <rob@uxblondon.com>
2016-01-07 14:52:49 +0000

    Fixed Notification fallback with jGrowl (required for IE)
    Also removed the growl.less file and used the CSS file from bower_components/ instead

--- public/js/cms.js
+++ public/js/cms.js
@@ -39832,16 +39832,6 @@ var Modernizr = (function( window, document, undefined ) {
 })( jQuery );;function boomNotification(message) {
 	boomNotification.prototype.$document = $(top.document);
 
-	boomNotification.prototype.options = {
-		theme : 'default',
-		speed : 240,
-		closer : false,
-		sticky : false,
-		open : function(elem, message){
-			$(this).removeClass('ui-state-highlight').addClass('ui-state-default').find('.message').prepend('<span class="ui-icon ui-icon-check ui-helper-left" />');
-		}
-	};
-
 	boomNotification.prototype.open = function(message) {
 		var notified = false,
 			waitingApproval = false,
@@ -39879,14 +39869,7 @@ var Modernizr = (function( window, document, undefined ) {
 	};
 
 	boomNotification.prototype.showFallback = function(message) {
-		if ( ! this.$document.find('#b-notification').length) {
-			$('<div id="b-notification"></div>')
-					.appendTo(this.$document.find('body'));
-		}
-
-		$.jGrowl(message, this.options);
-
-		this.$document.find('#b-notification').append($('#jGrowl'));
+		$.jGrowl(message);
 	};
 
 	this.open(message);
--- src/js/boomcms/notification.js
+++ src/js/boomcms/notification.js
@@ -1,16 +1,6 @@
 function boomNotification(message) {
 	boomNotification.prototype.$document = $(top.document);
 
-	boomNotification.prototype.options = {
-		theme : 'default',
-		speed : 240,
-		closer : false,
-		sticky : false,
-		open : function(elem, message){
-			$(this).removeClass('ui-state-highlight').addClass('ui-state-default').find('.message').prepend('<span class="ui-icon ui-icon-check ui-helper-left" />');
-		}
-	};
-
 	boomNotification.prototype.open = function(message) {
 		var notified = false,
 			waitingApproval = false,
@@ -48,14 +38,7 @@ function boomNotification(message) {
 	};
 
 	boomNotification.prototype.showFallback = function(message) {
-		if ( ! this.$document.find('#b-notification').length) {
-			$('<div id="b-notification"></div>')
-					.appendTo(this.$document.find('body'));
-		}
-
-		$.jGrowl(message, this.options);
-
-		this.$document.find('#b-notification').append($('#jGrowl'));
+		$.jGrowl(message);
 	};
 
 	this.open(message);
