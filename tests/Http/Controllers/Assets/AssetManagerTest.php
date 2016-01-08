3a2369ef6d463d96fa371a17bbf50bf90f640ff7
Rob Taylor <rob@uxblondon.com>
2016-01-08 17:56:03 +0000

    Added skeleton tests for asset manager

---
+++ tests/Http/Controllers/Assets/AssetManagerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace BoomCMS\Tests\Assets;
+
+use BoomCMS\Tests\AbstractTestCase;
+
+class AssetManagerTest extends AbstractTestCase
+{
+    public function testAddSites()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCreatedAssetIsAddedToCurrentSite()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRemoveSite()
+    {
+        $this->markTestIncomplete();
+    }
+}
