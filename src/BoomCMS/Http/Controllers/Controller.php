2a9f5ece2cac970c251d39346d19ff633688e069
Rob Taylor <rob@uxblondon.com>
2016-01-26 08:12:57 +0000

    Removed unused code from the base controller

--- src/BoomCMS/Http/Controllers/Controller.php
+++ src/BoomCMS/Http/Controllers/Controller.php
@@ -2,43 +2,25 @@
 
 namespace BoomCMS\Http\Controllers;
 
-use BoomCMS\Editor\Editor;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Session\SessionManager as Session;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests;
 
     /**
-     * @var Editor
-     */
-    public $editor;
-
-    /**
      * @var string
      */
     protected $role;
 
-    /**
-     * @var Session
-     */
-    public $session;
-
-    public function __construct(Request $request, Session $session, Editor $editor)
+    public function __construct(Request $request)
     {
-        $this->session = $session;
         $this->request = $request;
-        $this->editor = $editor;
 
         if ($this->role) {
-            if ($page = $request->route()->getParameter('page')) {
-                $this->authorize($this->role, $page);
-            } else {
-                $this->authorize($this->role, $request);
-            }
+            $this->authorize($this->role, $request);
         }
     }
 }
