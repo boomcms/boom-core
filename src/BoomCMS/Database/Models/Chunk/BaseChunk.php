c27f3ba621e8e57f1b0981d4c79e70ac65355922
Rob Taylor <rob@uxblondon.com>
2016-01-30 11:22:51 +0000

    Removed unused method from BaseChunk model

--- src/BoomCMS/Database/Models/Chunk/BaseChunk.php
+++ src/BoomCMS/Database/Models/Chunk/BaseChunk.php
@@ -21,28 +21,6 @@ class BaseChunk extends Model
             ->orderBy('page_vid', 'desc');
     }
 
-    public function scopeLatestEdit($query, PageVersion $upToVersion)
-    {
-        $query
-            ->select("{$this->table}.*")
-            ->addSelect("{$this->table}.slotname as slotname")
-            ->join('page_versions as v1', "{$this->table}.page_vid", '=', 'v1.id')
-            ->leftJoin("{$this->table} as c2", function ($query) use ($upToVersion) {
-                $query
-                    ->on("{$this->table}.id", '<', 'c2.id')
-                    ->on("{$this->table}.slotname", '=', 'c2.slotname')
-                    ->on('c2.page_vid', '<=', DB::raw("'{$upToVersion->getId()}'"));
-            })
-            ->leftJoin('page_versions as v2', function ($query) use ($upToVersion) {
-                $query
-                    ->on('c2.page_vid', '=', 'v2.id')
-                    ->on('v1.page_id', '=', 'v2.page_id');
-            })
-            ->whereNull('c2.id');
-
-        return $query;
-    }
-
     public function scopeWithRelations($query)
     {
         return $query;
