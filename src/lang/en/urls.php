b77de129f1b876f5d6d49af5d86adae5ead63664
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-02-05 16:16:02 +0000

    Added URL dialog updates
    - Added placeholder to input
    - Added english translations

---
+++ src/lang/en/urls.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'add' => [
+        'heading'     => 'Add URL',
+        'pointers'    => 'Some points to remember when creating a URL',
+        'lowercase'   => 'URLs should contain only lower-case letters, numbers, or hyphens eg /course-programe-news-august13',
+        'nospaces'    => 'No spaces or punctuation should be used',
+        'hyphens'     => 'Separate words with a hyphen eg /case-studies',
+        'nosurprises' => 'It should be clear from the URL what the content of the page is. Users who follow a URL to your page shouldn\'t be surprised by the content when they get there',
+        'keywords'    => 'Use only keywords in the URL. Remove linking words  – eg  \'and\', \'the\' –, that are not descriptive',
+        'new'         => 'New URL',
+        'placeholder' => 'Enter new URL here',
+    ],
+];
--- src/views/boomcms/editor/urls/add.php
+++ src/views/boomcms/editor/urls/add.php
@@ -1,17 +1,20 @@
-<h1>Add URL</h1>
-<h2>Some points to remember when creating a URL</h2>
+<h1><?= trans('boomcms::urls.add.heading') ?></h1>
+
+<h2><?= trans('boomcms::urls.add.pointers') ?></h2>
+
 <ul>
-	<li>URLs should contain only lower-case letters, numbers, or hyphens eg /course-programe-news-august13</li>
-	<li>No spaces or punctuation should be used.</li>
-	<li>Separate words with a hyphen eg /case-studies</li>
-	<li>It should be clear from the URL what the content of the page is. Users who follow a URL to your page shouldn't be surprised by the content when they get there.</li>
-	<li>Use only keywords in the URL. Remove linking words  – eg  'and', 'the' –, that are not descriptive.</li>
+	<li><?= trans('boomcms::urls.add.lowercase') ?></li>
+	<li><?= trans('boomcms::urls.add.nospaces') ?></li>
+	<li><?= trans('boomcms::urls.add.hyphens') ?></li>
+	<li><?= trans('boomcms::urls.add.nosurprises') ?></li>
+	<li><?= trans('boomcms::urls.add.keywords') ?></li>
 </ul>
 
 <form id="b-form-addurl" name="form-addurl">
 	<input type='hidden' name='page' value='<?= $page->getId() ?>' />
 
-	<label>New URL
-		<input type='text' name='url' id='url' />
+	<label>
+        <span><?= trans('boomcms::urls.add.new') ?></span>
+		<input type='text' name='url' id='url' placeholder='<?= trans('boomcms::urls.add.placeholder') ?>' />
 	</label>
 </form>
