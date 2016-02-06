518400ad5368b9e405d026865a508a4dc7ead856
Rob Taylor <rob@uxblondon.com>
2016-02-06 22:30:48 +0000

    Added untracked files from e6a5df9b4299a5382840b3b8337fb418c8103cee

---
+++ src/views/boomcms/footer.php
@@ -0,0 +1,13 @@
+    <?= $boomJS ?>
+    <?= view('boomcms::fontawesome') ?>
+
+	<script type="text/javascript">
+		//<![CDATA[
+		(function ($) {
+			$.boom.init();
+            $('body').ui();
+		})(jQuery);
+		//]]>
+	</script>
+</body>
+</html>
