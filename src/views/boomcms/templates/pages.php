5ba3ba4da6ae8ac8e419c2a5b5bbeddc773105fa
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-02-11 10:10:16 +0000

    Fixed layout of template manager pages list

--- src/views/boomcms/templates/pages.php
+++ src/views/boomcms/templates/pages.php
@@ -1,11 +1,12 @@
-	<?= view('boomcms::header', ['title' => 'Templates']) ?>
-	<?= $menu() ?>
+<?= view('boomcms::header', ['title' => 'Templates']) ?>
+<?= $menu() ?>
 
-	<div id="b-topbar" class="b-toolbar">
-		<?= $menuButton() ?>
-		<?= $button('download', trans('Download as CSV'), ['id' => 'b-template-pages-download', 'class' => 'b-button-withtext']) ?>
-	</div>
+<div id="b-topbar" class="b-toolbar">
+    <?= $menuButton() ?>
+    <?= $button('download', trans('Download as CSV'), ['id' => 'b-template-pages-download', 'class' => 'b-button-withtext']) ?>
+</div>
 
+<main id="b-container">
     <div id="b-templates">
         <h1><?= trans('boomcms::templates.pages') ?> <?= $template->getName() ?></h1>
 
@@ -31,15 +32,12 @@
 			</tbody>
         </table>
     </div>
+</main>
 
-	<?= $boomJS ?>
-	<script type="text/javascript">
-		//<![CDATA[
-		(function ($) {
-			$.boom.init();
-			$('body').templateManager();
-		})(jQuery);
-		//]]>
-	</script>
-</body>
-</html>
+<script type="text/javascript">
+    window.onload = function() {
+        $('body').templateManager();
+    };
+</script>
+
+<?= view('boomcms::footer') ?>
