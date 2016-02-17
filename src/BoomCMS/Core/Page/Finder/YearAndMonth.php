54ce194a266a98bf9792b50c4b2bc6d3af306228
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-02-17 15:32:57 +0000

    Fixed property names not in camelCase

--- src/BoomCMS/Core/Page/Finder/YearAndMonth.php
+++ src/BoomCMS/Core/Page/Finder/YearAndMonth.php
@@ -10,25 +10,25 @@ class YearAndMonth extends Filter
 {
     const EPOC_FIRST_YEAR = 1970;
 
-    protected $_year;
-    protected $_month;
+    protected $year;
+    protected $month;
 
     public function __construct($year, $month)
     {
-        $this->_year = $year;
-        $this->_month = $month;
+        $this->year = $year;
+        $this->month = $month;
     }
 
     public function build(Builder $query)
     {
         return $query
-            ->where(DB::raw('year(from_unixtime(visible_from))'), '=', $this->_year)
-            ->where(DB::raw('month(from_unixtime(visible_from))'), '=', $this->_month);
+            ->where(DB::raw('year(from_unixtime(visible_from))'), '=', $this->year)
+            ->where(DB::raw('month(from_unixtime(visible_from))'), '=', $this->month);
     }
 
     public function monthIsValid()
     {
-        return ctype_digit($this->_month) && $this->_month > 0 && $this->_month <= 12;
+        return ctype_digit($this->month) && $this->month > 0 && $this->month <= 12;
     }
 
     public function shouldBeApplied()
@@ -38,6 +38,6 @@ class YearAndMonth extends Filter
 
     public function yearIsValid()
     {
-        return ctype_digit($this->_year) && $this->_year >= static::EPOC_FIRST_YEAR && $this->_year <= date('Y');
+        return ctype_digit($this->year) && $this->year >= static::EPOC_FIRST_YEAR && $this->year <= date('Y');
     }
 }
