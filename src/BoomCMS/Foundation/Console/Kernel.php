05f0e104e30e6c2e358ac10ac0924e505b2fe598
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-02-18 14:39:22 +0000

    Made it easier to define console commands in core

---
+++ src/BoomCMS/Foundation/Console/Kernel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BoomCMS\Foundation\Console;
+
+use BoomCMS\Console\Commands;
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        Commands\InstallTemplates::class,
+        Commands\Publish::class,
+    ];
+
+    /**
+     * Define the application's command schedule.
+     *
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
+     *
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+    }
+}
