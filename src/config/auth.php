64f84808537ff0ce9e0385900dfac1e11ebc1f58
Rob Taylor <rob@robbytaylor.co.uk>
2016-02-23 07:13:05 -0500

    Applied fixes from StyleCI

--- src/config/auth.php
+++ src/config/auth.php
@@ -14,15 +14,15 @@ return [
     'providers' => [
         'boomcms' => [
             'driver' => 'boomcms',
-            'model' => BoomCMS\Database\Models\Person::class,
+            'model'  => BoomCMS\Database\Models\Person::class,
         ],
     ],
     'passwords' => [
         'boomcms' => [
             'provider' => 'boomcms',
-            'email' => 'boomcms::email.password',
-            'table' => 'password_resets',
-            'expire' => 60,
+            'email'    => 'boomcms::email.password',
+            'table'    => 'password_resets',
+            'expire'   => 60,
         ],
     ],
 ];
