f98b58ff4ecb1afd5b2bc1c00127d5d0b75982e5
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-03-16 10:34:51 +0000

    Removed obsolete frameborder attribute

--- src/views/boomcms/editor/iframe.php
+++ src/views/boomcms/editor/iframe.php
@@ -11,4 +11,4 @@
 	</script>
 <?php endif ?>
 
-<iframe frameBorder="0" style="max-width: none; position: fixed; left: 0; top: 0; width: <?= $editor->isEnabled() ? '60px' : '101px' ?>; height: <?= $editor->isEnabled() ? '100%' : '35px' ?>; overflow: hidden; z-index: 10000; background: transparent" id='b-page-topbar' scrolling="no" src='/boomcms/editor/toolbar?page_id=<?= $page_id ?>'></iframe>
+<iframe style="border: 0; max-width: none; position: fixed; left: 0; top: 0; width: <?= $editor->isEnabled() ? '60px' : '101px' ?>; height: <?= $editor->isEnabled() ? '100%' : '35px' ?>; overflow: hidden; z-index: 10000; background: transparent" id='b-page-topbar' scrolling="no" src='/boomcms/editor/toolbar?page_id=<?= $page_id ?>'></iframe>
