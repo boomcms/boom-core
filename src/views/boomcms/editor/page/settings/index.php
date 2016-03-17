e0195c1e50675e2dcfcbf4a5757d7d0a23bc64b6
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-03-17 12:40:03 +0000

    Fixed font issue in CMS menu and page settings menu

--- src/views/boomcms/editor/page/settings/index.php
+++ src/views/boomcms/editor/page/settings/index.php
@@ -2,7 +2,8 @@
     <ul class="b-page-settings-menu">
         <?php if (Gate::allows('editTemplate', $page)): ?>
             <li>
-                <a href="#" class="fa fa-file-text-o" data-b-page-setting="template">
+                <a href="#" data-b-page-setting="template">
+                    <span class="fa fa-file-text-o"></span>
                     <?= trans('boomcms::settings.menu.template') ?>
                 </a>
             </li>
@@ -10,7 +11,8 @@
 
         <?php if (Gate::allows('editNavBasic', $page)): ?>
             <li>
-                <a href="#" class="fa fa-sitemap" data-b-page-setting="navigation">
+                <a href="#" data-b-page-setting="navigation">
+                    <span class="fa fa-sitemap"></span>
                     <?= trans('boomcms::settings.menu.navigation') ?>
                 </a>
             </li>
@@ -18,7 +20,8 @@
 
         <?php if (Gate::allows('editUrls', $page)): ?>
             <li>
-                <a href="#" class="fa fa-link" data-b-page-setting="urls">
+                <a href="#" data-b-page-setting="urls">
+                    <span class="fa fa-link"></span>
                     <?= trans('boomcms::settings.menu.urls') ?>
                 </a>
             </li>
@@ -26,27 +29,31 @@
 
         <?php if (Gate::allows('editSearchBasic', $page)): ?>
             <li>
-                <a href="#" class="fa fa-search" data-b-page-setting="search">
+                <a href="#" data-b-page-setting="search">
+                    <span class="fa fa-search"></span>
                     <?= trans('boomcms::settings.menu.search') ?>
                 </a>
             </li>
         <?php endif ?>
 
         <li>
-            <a href="#" class="fa fa-tag" data-b-page-setting="tags">
+            <a href="#" data-b-page-setting="tags">
+                <span class="fa fa-tag"></span>
                 <?= trans('boomcms::settings.menu.tags') ?>
             </a>
         </li>
 
         <li>
-            <a href="#" class="fa fa-puzzle-piece" data-b-page-setting="relations">
+            <a href="#" data-b-page-setting="relations">
+                <span class="fa fa-puzzle-piece"></span>
                 <?= trans('boomcms::settings.menu.relations') ?>
             </a>
         </li>
 
         <?php if (Gate::allows('editChildrenBasic', $page)): ?>
             <li>
-                <a href="#" class="fa fa-child" data-b-page-setting="children">
+                <a href="#" data-b-page-setting="children">
+                    <span class="fa fa-child"></span>
                     <?= trans('boomcms::settings.menu.children') ?>
                 </a>
             </li>
@@ -54,7 +61,8 @@
 
         <?php if (Gate::allows('editAdmin', $page)): ?>
             <li>
-                <a href="#" class="fa fa-lock" data-b-page-setting="admin">
+                <a href="#" data-b-page-setting="admin">
+                    <span class="fa fa-lock"></span>
                     <?= trans('boomcms::settings.menu.admin') ?>
                 </a>
             </li>
@@ -62,7 +70,8 @@
 
         <?php if (Gate::allows('editFeature', $page)): ?>
             <li>
-                <a href="#" class="fa fa-image" data-b-page-setting="feature">
+                <a href="#" data-b-page-setting="feature">
+                    <span class="fa fa-image"></span>
                     <?= trans('boomcms::settings.menu.feature') ?>
                 </a>
             </li>
@@ -71,7 +80,8 @@
         <div class="group">
             <?php if (Gate::allows('publish', $page)): ?>
                 <li>
-                    <a href="#" class="fa fa-eye" data-b-page-setting="visibility">
+                    <a href="#" data-b-page-setting="visibility">
+                        <span class="fa fa-eye"></span>
                         <?= trans('boomcms::settings.menu.visibility') ?>
                     </a>
                 </li>
@@ -86,14 +96,16 @@
 
         <?php if (Gate::allows('delete', $page)): ?>
             <li>
-                <a href="#" class="fa fa-trash-o" data-b-page-setting="delete">
+                <a href="#" data-b-page-setting="delete">
+                    <span class="fa fa-trash-o"></span>
                     <?= trans('boomcms::settings.menu.delete') ?>
                 </a>
             </li>
         <?php endif ?>
 
         <li class="b-page-settings-close">
-            <a href="#" class="fa fa-close">
+            <a href="#">
+                <span class="fa fa-close"></span>
                 <?= trans('boomcms::settings.menu.close') ?>
             </a>
         </li>
--- src/views/boomcms/menu.php
+++ src/views/boomcms/menu.php
@@ -3,21 +3,21 @@
 
     <ul>
         <li>
-            <a target='_top' href='/' class="fa fa-home"><?= trans('boomcms::menu.home') ?></a>
+            <a target='_top' href='/'><span class="fa fa-home"></span><?= trans('boomcms::menu.home') ?></a>
         </li>
 
         <?php foreach (BoomCMS\Support\Menu::items() as $item): ?>
             <li>
-                <a target='_top' href='<?= $item['url'] ?>'<?php if (isset($item['icon'])): ?> class="fa fa-<?= $item['icon'] ?>"<?php endif ?>><?= $item['title'] ?></a>
+                <a target='_top' href='<?= $item['url'] ?>'><?php if (isset($item['icon'])): ?><span class="fa fa-<?= $item['icon'] ?>"></span><?php endif ?><?= $item['title'] ?></a>
             </li>
         <?php endforeach ?>
 
         <li>
-            <a target='_top' href='/boomcms/account' class="fa fa-user"><?= trans('boomcms::menu.account') ?></a>
+            <a target='_top' href='/boomcms/account'><span class="fa fa-user"></span><?= trans('boomcms::menu.account') ?></a>
         </li>
 
         <li>
-            <a target='_top' href='/boomcms/logout' class="fa fa-sign-out"><?= trans('boomcms::menu.logout') ?></a>
+            <a target='_top' href='/boomcms/logout'><span class="fa fa-sign-out"></span><?= trans('boomcms::menu.logout') ?></a>
         </li>
     </ul>
 
