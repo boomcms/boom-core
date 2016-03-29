c8fbf9d972316702a45b8b78da6de08deb7fa730
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-03-29 17:41:37 +0100

    Added test to ensure that migrations complete

---
+++ tests/MigrationsTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BoomCMS\Tests;
+
+use Illuminate\Support\Facades\App;
+use PDO;
+
+class MigrationsTest extends AbstractTestCase
+{
+    public function testMigrationsComplete()
+    {
+        $pdo = new PDO(env('DB_DRIVER').':host='.env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'));
+        $pdo->exec('drop database if exists '.env('DB_DATABASE'));
+        $pdo->exec('create database '.env('DB_DATABASE'));
+
+        $app = App::getFacadeRoot();
+        $app['migration.repository']->createRepository();
+        $app['migrator']->run(realpath(__DIR__.'/../src/database/migrations'));
+
+        $pdo->exec('drop database '.env('DB_DATABASE'));
+    }
+}
