2a062883d3744aa24078d28653f3fbb980f89809
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-04-29 16:48:51 +0100

    Removed jquery UI tables from people manager person view

--- src/views/boomcms/person/view.php
+++ src/views/boomcms/person/view.php
@@ -1,65 +1,64 @@
 <div class="b-person-view" data-person-id='<?= $person->getId() ?>'>
-    <div class="boom-tabs">
-        <ul>
-            <li><a href="#b-person-view-information<?= $person->getId() ?>">Information</a></li>
-            <li><a href="#b-person-view-groups<?= $person->getId() ?>">Groups</a></li>
-        </ul>
+    <section>
+        <h2>User details</h2>
 
-        <div id="b-person-view-information<?= $person->getId() ?>">
-            <form>
-                <label>
-                    Name
-                    <input type="text" name="name" value="<?= $person->getName() ?>" />
-                </label>
+        <form>
+            <label>
+                Name
+                <input type="text" name="name" value="<?= $person->getName() ?>" />
+            </label>
 
-                <label for="person-email">
-                    Email
-                    <input type="text" name="email" disabled="disabled" value="<?= $person->getEmail() ?>" />
-                </label>
+            <label for="person-email">
+                Email
+                <input type="text" name="email" disabled="disabled" value="<?= $person->getEmail() ?>" />
+            </label>
 
-                <label for='person-status'>
-                    Status
+            <label for='person-status'>
+                Status
 
-                    <select name="enabled" id="person-status">
-                        <option value=""<?php if (!$person->isEnabled()): ?> selected="selected"<?php endif ?>>Disabled</option>
-                        <option value="1"<?php if ($person->isEnabled()): ?> selected="selected"<?php endif ?>>Enabled</option>
+                <select name="enabled" id="person-status">
+                    <option value=""<?php if (!$person->isEnabled()): ?> selected="selected"<?php endif ?>>Disabled</option>
+                    <option value="1"<?php if ($person->isEnabled()): ?> selected="selected"<?php endif ?>>Enabled</option>
+                </select>
+            </label>
+
+            <?php if (Gate::allows('editSuperuser', $person)): ?>
+                <label for='person-superuser'>
+                    Superuser
+
+                    <select name="superuser" id="person-superuser">
+                        <option value=""<?php if (!$person->isSuperuser()): ?> selected="selected"<?php endif ?>>No</option>
+                        <option value="1"<?php if ($person->isSuperuser()): ?> selected="selected"<?php endif ?>>Yes</option>
                     </select>
                 </label>
-
-                <?php if (Gate::allows('editSuperuser', $person)): ?>
-                    <label for='person-superuser'>
-                        Superuser
-
-                        <select name="superuser" id="person-superuser">
-                            <option value=""<?php if (!$person->isSuperuser()): ?> selected="selected"<?php endif ?>>No</option>
-                            <option value="1"<?php if ($person->isSuperuser()): ?> selected="selected"<?php endif ?>>Yes</option>
-                        </select>
-                    </label>
-                <?php endif ?>
-
-                <div>
-                    <?= $button('save', trans('Save'), ['id' => 'b-person-save', 'class' => 'b-people-save']) ?>
-                    <?= $button('trash-o', trans('Delete'), ['id' => 'b-person-delete']) ?>
-                </div>
-            </form>
-        </div>
-
-        <div id="b-person-view-groups<?= $person->getId() ?>">
-            <?= $person->getName() ?>
-            <?php if (count($groups) == 0): ?>
-                is not a member of any groups<br />
-            <?php else: ?>
-                is a member of these groups:
-                <ul id='b-person-groups-list'>
-                     <?php foreach ($groups as $group): ?>
-                         <li data-group-id='<?= $group->getId() ?>'>
-                             <?= $group->getName() ?>&nbsp;<a title='Remove user from group' class='b-person-group-delete' href='#'>x</a>
-                         </li>
-                     <?php endforeach ?>
-                </ul>
             <?php endif ?>
 
-            <?= $button('plus', trans('Add group'), ['class' => 'b-person-addgroups', 'rel' => $person->getId()]) ?>
-        </div>
-    </div>
+            <div>
+                <?= $button('save', trans('Save'), ['id' => 'b-person-save', 'class' => 'b-people-save']) ?>
+                <?= $button('trash-o', trans('Delete'), ['id' => 'b-person-delete']) ?>
+            </div>
+        </form>
+    </section>
+
+    <section>
+        <h2>Groups</h2>
+
+        <p><?= $person->getName() ?>
+
+        <?php if (count($groups) == 0): ?>
+            is not a member of any groups.</p>
+        <?php else: ?>
+            is a member of these groups:</p>
+
+            <ul id='b-person-groups-list'>
+                 <?php foreach ($groups as $group): ?>
+                     <li data-group-id='<?= $group->getId() ?>'>
+                         <?= $group->getName() ?>&nbsp;<a title='Remove user from group' class='b-person-group-delete' href='#'>x</a>
+                     </li>
+                 <?php endforeach ?>
+            </ul>
+        <?php endif ?>
+
+        <?= $button('plus', trans('Add group'), ['class' => 'b-person-addgroups', 'rel' => $person->getId()]) ?>
+    </section>
 </div>
