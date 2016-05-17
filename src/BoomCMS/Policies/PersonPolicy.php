21a5e7f69d35f5bea60465ca3de182fb69f0ef33
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-05-17 14:13:10 +0100

    Fixes to PersonPolicy and test

--- src/BoomCMS/Policies/PersonPolicy.php
+++ src/BoomCMS/Policies/PersonPolicy.php
@@ -6,8 +6,16 @@ use BoomCMS\Contracts\Models\Person;
 
 class PersonPolicy
 {
+    /**
+     * Whether a user can edit the superuser status of another
+     *
+     * @param Person $user
+     * @param Person $editing
+     *
+     * @return bool
+     */
     public function editSuperuser(Person $user, Person $editing)
     {
-        return $user->isSuperUser() && $user->getId() != $editing->getId();
+        return $user->isSuperUser() && !$user->is($editing);
     }
 }
