d92ecc70cd2d613edcb4f4f66e717799f0dc5c75
Rob Taylor <rob@uxblondon.com>
2016-05-18 22:23:43 +0100

    Fixed animated gifs not animating

---
+++ src/BoomCMS/Http/Controllers/Asset/Gif.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BoomCMS\Http\Controllers\Asset;
+
+use Illuminate\Http\Response;
+
+class Gif extends BaseController
+{
+	/**
+	 * Gifs are not resized to ensure that animated gifs animate.
+	 *
+	 * Width and height parameters are therefore ignored.
+	 *
+	 * @param int $width
+	 * @param int $height
+	 * @return Response
+	 */
+    public function view($width = null, $height = null)
+    {
+        return $this->response
+            ->header('content-type', $this->asset->getMimetype())
+            ->setContent(file_get_contents($this->asset->getFilename()));
+    }
+
+    public function thumb($width = null, $height = null)
+    {
+        return $this->view();
+    }
+}
