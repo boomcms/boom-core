688811aaa792d3f342985fa92c42ce13df31e6bd
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-05-23 17:44:50 +0100

    Initial work on asset manager refactoring

--- src/views/boomcms/assets/index.php
+++ src/views/boomcms/assets/index.php
@@ -1,6 +1,44 @@
 <?= view('boomcms::header', ['title' => trans('boomcms::asset.manager')]) ?>
 
-<?= $manager ?>
+<div id="b-assets-manager">
+	<?= $menuButton() ?>
+	<?= $menu() ?>
+
+	<div id="b-topbar" class="b-asset-manager b-toolbar">
+		<div id="b-assets-buttons">
+			<?= $button('upload', 'upload', ['id' => 'b-assets-upload']) ?>
+			<?= $button('edit', 'view-edit', ['id' => 'b-button-multiaction-edit', 'disabled' => 'disabled']) ?>
+			<?= $button('trash-o', 'delete', ['id' => 'b-button-multiaction-delete', 'disabled' => 'disabled']) ?>
+			<?= $button('download', 'download', ['id' => 'b-button-multiaction-download', 'disabled' => 'disabled']) ?>
+			<?= $button('tags', 'add-tags', ['id' => 'b-button-multiaction-tag', 'disabled' => 'disabled']) ?>
+		
+            <a href="#" id="b-assets-select-all"><?= trans('Select all') ?></a>
+            &nbsp;:&nbsp;
+            <a href="#" id="b-assets-select-none"><?= trans('Select none') ?></a>
+        </div>
+	</div>
+
+	<div id="b-assets-filters">
+		<span>
+			<button id="b-assets-all" class="b-button">
+				<?= trans('All assets') ?>
+			</button>
+		</span>
+
+        <?= view('boomcms::assets.search.title') ?>
+        <?= view('boomcms::assets.search.type') ?>
+        <?= view('boomcms::assets.search.tag') ?>
+        <?= view('boomcms::assets.search.sort') ?>
+        <?= view('boomcms::assets.pagination') ?>
+	</div>
+
+    <div id="b-assets-content">
+        <?= view('boomcms::assets.thumbs', ['assets' => []]) ?>
+    </div>
+
+	<?= view('boomcms::assets.upload') ?>
+</div>
+
 
 <script type="text/javascript">
     window.onload = function() {
--- src/views/boomcms/assets/thumbs.php
+++ src/views/boomcms/assets/thumbs.php
@@ -1,34 +1,32 @@
 <div id="b-assets-view-thumbs">
-    <?php if (count($assets)): ?>
-        <?php foreach ($assets as $asset): ?>
-            <div style="height: 160px; width: <?= floor(160 * $asset->getAspectRatio()) ?>px" data-aspect-ratio="<?= $asset->getAspectRatio() ?>">
-                <a href="#" class="thumb" data-asset="<?= $asset->getId() ?>">
-                    <img class="loading" />
-
-                    <div class="pace loading">
-                        <div>
-                            <span><?= trans('boomcms::asset.loading') ?></span>
-                            <div class="pace-activity"></div>
-                        </div>
-                    </div>
-
-                    <div class="b-asset-details">
-                        <h2><?= $asset->getTitle() ?></h2>
-
-                        <p>
-                            <?= trans('boomcms::asset.type.'.strtolower($asset->getType())) ?><br />
-
-                            <?php if ($asset->isImage()): ?>
-                                <?= $asset->getWidth() ?> x <?= $asset->getHeight() ?>
-                            <?php else: ?>
-                                <?= Str::filesize($asset->getFilesize()) ?>
-                            <?php endif ?>
-                        </p>
-                    </div>
-                </a>
-            </div>
-        <?php endforeach ?>
-    <?php else: ?>
-        <p id="b-assets-none"><?= trans('boomcms::asset.none') ?></p>
-    <?php endif ?>
+    <p id="b-assets-none"><?= trans('boomcms::asset.none') ?></p>
 </div>
+
+<script type="text/template" id="b-asset-thumb">
+    <div style="height: 160px; width: <%= Math.floor(160 * asset.getAspectRatio()) %>px">
+        <a href="#asset/<%= asset.getId() %>" class="thumb">
+            <img class="loading" />
+
+            <div class="pace loading">
+                <div>
+                    <span><?= trans('boomcms::asset.loading') ?></span>
+                    <div class="pace-activity"></div>
+                </div>
+            </div>
+
+            <div class="b-asset-details">
+                <h2><%= asset.getTitle() %>></h2>
+
+                <p>
+                    <?= trans('boomcms::asset.type.'.strtolower($asset->getType())) ?><br />
+
+                    <?php /* if ($asset->isImage()): ?>
+                        <?= $asset->getWidth() ?> x <?= $asset->getHeight() ?>
+                    <?php else: ?>
+                        <?= Str::filesize($asset->getFilesize()) ?>
+                    <?php endif */ ?>
+                </p>
+            </div>
+        </a>
+    </div>
+</script>
