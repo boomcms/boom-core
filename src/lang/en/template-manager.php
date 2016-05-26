2d2c81c198a0c6af39b513bbd8291c69be9753f2
Rob Taylor <rob@robbytaylor.co.uk>
2016-05-26 09:33:39 -0400

    Applied fixes from StyleCI

--- src/lang/en/template-manager.php
+++ src/lang/en/template-manager.php
@@ -1,15 +1,15 @@
 <?php
 
 return [
-    'description' => 'Description',
-    'filename' => 'Filename',
-    'last-edited' => 'Last edited',
-    'name'  => 'Name',
-    'page-title' => 'Page title',
-    'pages' => 'Pages',
+    'description'   => 'Description',
+    'filename'      => 'Filename',
+    'last-edited'   => 'Last edited',
+    'name'          => 'Name',
+    'page-title'    => 'Page title',
+    'pages'         => 'Pages',
     'pages-heading' => 'Pages with template: ',
-    'theme' => 'Theme',
-    'title' => 'Manage Templates',
-    'url' => 'URL',
-    'visible' => 'Visible?',
+    'theme'         => 'Theme',
+    'title'         => 'Manage Templates',
+    'url'           => 'URL',
+    'visible'       => 'Visible?',
 ];
