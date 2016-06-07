0bea5df449dd5fb529c86d911e43dfb930274fdf
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-06-07 10:47:12 +0100

    Use ::class notation in BoomCMS\Foundation\Http\Kernel

--- src/BoomCMS/Foundation/Http/Kernel.php
+++ src/BoomCMS/Foundation/Http/Kernel.php
@@ -3,7 +3,12 @@
 namespace BoomCMS\Foundation\Http;
 
 use BoomCMS\Http\Middleware;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
+use Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
 
 class Kernel extends HttpKernel
 {
@@ -14,11 +19,11 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         Middleware\RouteSite::class,
-        'Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode',
-        'Illuminate\Cookie\Middleware\EncryptCookies',
-        'Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse',
-        'Illuminate\Session\Middleware\StartSession',
-        'Illuminate\View\Middleware\ShareErrorsFromSession',
+        CheckForMaintenanceMode::class,
+        EncryptCookies::class,
+        AddQueuedCookiesToResponse::class,
+        StartSession::class,
+        ShareErrorsFromSession::class,
         Middleware\DefineGlobalViewSharedVariables::class,
     ];
 }
