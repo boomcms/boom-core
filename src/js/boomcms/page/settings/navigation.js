37a25fa2f64324ca196ce72027bf6c3fa85aac51
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-06-17 10:44:38 +0100

    Removed duplication in page navigation settings

--- src/js/boomcms/page/settings/navigation.js
+++ src/js/boomcms/page/settings/navigation.js
@@ -1,22 +1,8 @@
 $.widget('boom.pageSettingsNavigation', $.boom.pageSettingsDefault, {
-	bind: function() {
-		var settingsEditor = this,
-			section = settingsEditor.options.section;
+	bindReparent: function() {
+		var settingsEditor = this;
 
 		this.element
-			.on('click', '.b-button-cancel', function(e) {
-				e.preventDefault();
-
-				settingsEditor.options.settings.show(section);
-			})
-			.on('click', '.b-button-save', function(e) {
-				e.preventDefault();
-
-				settingsEditor.page.saveSettings(section, settingsEditor.element.find('form').serialize())
-					.done(function() {
-						new boomNotification('Page settings saved').show();
-					});
-			})
 			.on('click', '.b-navigation-reparent', function(e) {
 				var current = settingsEditor.element.find('input[name=parent_id]').val();
 
@@ -43,6 +29,7 @@ $.widget('boom.pageSettingsNavigation', $.boom.pageSettingsDefault, {
 		var $el = this.element;
 		this.page = this.options.page;
 		this.bind();
+		this.bindReparent();
 
 		$el.find('.boom-tree').pageTree({
 			active: $el.find('input[name=parent_id]').val(),
