49be742eba6e77493721cbdb1aef6618287df3ae
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-06-17 15:05:34 +0100

    Timestamp chunk: added ability to pass a callback to modify displayed date / time
    Added getDateTime() method to timestamp chunk

--- tests/Chunk/ChunkTimestampTest.php
+++ tests/Chunk/ChunkTimestampTest.php
@@ -5,6 +5,7 @@ namespace BoomCMS\Tests\Chunk;
 use BoomCMS\Chunk\Timestamp;
 use BoomCMS\Foundation\Chunk\AcceptsHtmlString;
 use BoomCMS\Tests\AbstractTestCase;
+use DateTime;
 
 class ChunkTimestampTest extends AbstractTestCase
 {
@@ -13,5 +14,37 @@ class ChunkTimestampTest extends AbstractTestCase
         $traits = class_uses(Timestamp::class);
 
         $this->assertTrue(in_array(AcceptsHtmlString::class, $traits));
+    }
+
+    public function testClosureIsApplied()
+    {
+        $timestamp = time();
+        $attrs = ['timestamp' => $timestamp];
+        $text = 'Some test text';
+
+        $closure = function(Timestamp $chunk) use($timestamp, $text) {
+            if ($chunk->getTimestamp() !== $timestamp) {
+                $this->fail();
+            }
+
+            return $text;
+        };
+
+        $chunk = new Timestamp($this->validPage(), $attrs, 'test');
+
+        $this->assertEquals($chunk, $chunk->apply($closure));
+        $this->assertEquals('<p>'.$text.'</p>', $chunk->setHtml('<p>{time}</p>')->render());
+    }
+
+    public function testGetDateTime()
+    {
+        $time = time();
+        $attrs = ['timestamp' => $time];
+        $chunk = new Timestamp($this->validPage(), $attrs, 'test');
+
+        $datetime = $chunk->getDateTime();
+
+        $this->assertInstanceOf(DateTime::class, $datetime);
+        $this->assertEquals($time, $datetime->getTimestamp());
     }
 }
