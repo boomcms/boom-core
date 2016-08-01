ad881ba9b9d29d45f7c6a8415992f7e8fea817f6
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-08-01 10:55:44 +0100

    Updated location chunk to use WikiMedia map tiles
    Conflicts:
    	public/js/cms.min.js
    	public/js/cms.min.js.map
    	public/js/people-manager.min.js
    	public/js/template-manager.min.js

--- public/js/cms.js
+++ public/js/cms.js
@@ -44013,9 +44013,8 @@ $.widget('ui.chunkPageVisibility', {
 	boomChunkLocationEditor.prototype.bind = function() {
 		var locationEditor = this;
 
-		L.tileLayer('http://{s}.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="http://osm.org/copyright" title="OpenStreetMap" target="_blank">OpenStreetMap</a> contributors | Tiles Courtesy of <a href="http://www.mapquest.com/" title="MapQuest" target="_blank">MapQuest</a> <img src="http://developer.mapquest.com/content/osm/mq_logo.png" width="16" height="16">',
-			subdomains: ['otile1','otile2','otile3','otile4']
+		L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
+			attribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 		}).addTo(this.map);
 
 		if (this.mapElement.attr('data-lat') != 0 && this.mapElement.attr('data-lng') != 0) {
