8f80a9a39f5cd7d659b8d92fff12ba97e35380c0
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-08-15 12:04:35 +0100

    Link picker: removed code duplication

--- src/views/boomcms/editor/linkPicker.php
+++ src/views/boomcms/editor/linkPicker.php
@@ -1,18 +1,37 @@
 <div id="b-linkpicker-container">
 	<div class="boom-tabs b-linkpicker">
 		<ul>
-			<li><a href="#b-linkpicker-add-internal"><?= trans('boomcms::editor.link-picker.tabs.internal') ?></a></li>
-			<li><a href="#b-linkpicker-add-external"><?= trans('boomcms::editor.link-picker.tabs.external') ?></a></li>
-			<li><a href="#b-linkpicker-add-asset"><?= trans('boomcms::editor.link-picker.tabs.asset') ?></a></li>
-			<li><a href="#b-linkpicker-text"><?= trans('boomcms::editor.link-picker.tabs.text') ?></a></li>
+			<li class="toggle-internal">
+                <a href="#b-linkpicker-add-internal">
+                    <?= trans('boomcms::editor.link-picker.tabs.internal') ?>
+                </a>
+            </li>
+			
+            <li class="toggle-external">
+                <a href="#b-linkpicker-add-external">
+                    <?= trans('boomcms::editor.link-picker.tabs.external') ?>
+                </a>
+            </li>
+
+            <li class="toggle-asset">
+                <a href="#b-linkpicker-add-asset">
+                    <?= trans('boomcms::editor.link-picker.tabs.asset') ?>
+                </a>
+            </li>
+
+            <li class="toggle-text">
+                <a href="#b-linkpicker-text">
+                    <?= trans('boomcms::editor.link-picker.tabs.text') ?>
+                </a>
+            </li>
 		</ul>
 
-		<div id="b-linkpicker-add-internal">
+		<div id="b-linkpicker-add-internal" class="toggle-internal">
 			<p><?= trans('boomcms::editor.link-picker.internal') ?></p>
 			<ul class="boom-tree"></ul>
 		</div>
 
-		<div id="b-linkpicker-add-external">
+		<div id="b-linkpicker-add-external" class="toggle-external">
 			<p>
 				<?= trans('boomcms::editor.link-picker.external') ?>
 			</p>
@@ -28,22 +47,30 @@
 			</form>
 		</div>
 
-		<div id="b-linkpicker-add-asset">
+		<div id="b-linkpicker-add-asset" class="toggle-asset">
             <label>
                 <p><?= trans('boomcms::editor.link-picker.asset-action') ?></p>
                 
                 <select>
-                    <option value="view" selected><?= trans('boomcms::editor.link-picker.action-view') ?></option>
-                    <option value="download"><?= trans('boomcms::editor.link-picker.action-download') ?></option>
+                    <option value="view" selected>
+                        <?= trans('boomcms::editor.link-picker.action-view') ?>
+                    </option>
+
+                    <option value="download">
+                        <?= trans('boomcms::editor.link-picker.action-download') ?>
+                    </option>
                 </select>
             </label>
 
-			<?= $button('paperclip', 'select-asset', ['id' => 'b-linkpicker-asset-select', 'class' => 'b-button-withtext']) ?>
+			<?= $button('paperclip', 'select-asset', [
+                'id' => 'b-linkpicker-asset-select',
+                'class' => 'b-button-withtext'
+            ]) ?>
 		
             <img />
         </div>
 
-		<div id="b-linkpicker-text">
+		<div id="b-linkpicker-text" class="toggle-text">
 			<p>
                 <?= trans('boomcms::editor.link-picker.text') ?>
 			</p>
@@ -55,6 +82,9 @@
 	</div>
 
     <div class="buttons">
-        <?= $button('trash-o', 'remove-link', ['id' => 'b-linkpicker-remove', 'class' => 'b-button-withtext']) ?>
+        <?= $button('trash-o', 'remove-link', [
+            'id' => 'b-linkpicker-remove',
+            'class' => 'b-button-withtext toggle-remove'
+        ]) ?>
     </div>
 </div>
