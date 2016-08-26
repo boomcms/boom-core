90541c39c875902c1316c2db1c4793d82efb808c
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-08-26 12:40:43 +0100

    Prevent requests to /favicon.ico which return a 404

--- src/views/boomcms/head.php
+++ src/views/boomcms/head.php
@@ -7,6 +7,7 @@
     <meta http-equiv="Content-Style-Type" content="text/css" />
     <meta name="csrf-token" content="<?= csrf_token() ?>" />
 
+    <link rel="icon" href="data:;base64,iVBORw0KGgo=">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/vendor/boomcms/boom-core/css/cms.css" />
 
