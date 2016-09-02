fc2068fc36acd841b82decfc91c44aa9fdab2079
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-09-02 10:35:31 +0100

    Fixed missing PHP tags

--- src/views/boomcms/footer.php
+++ src/views/boomcms/footer.php
@@ -5,7 +5,7 @@
                 <?php if (auth()->check()): ?>
                     user: new BoomCMS.Person(<?= auth()->user()->toJson() ?>),
                 <?php endif ?>
-                assetTypes: json_encode(Lang::get('boomcms::asset.type'))
+                assetTypes: <?= json_encode(Lang::get('boomcms::asset.type')) ?>
             });
         })(jQuery);
         //]]>
