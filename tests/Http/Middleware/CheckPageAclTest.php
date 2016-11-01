2d8c0e25b72b1fa810aeb5403882465b27ac515c
Rob Taylor <robbytaylor@users.noreply.github.com>
2016-11-01 18:08:11 +0000

    Page ACL middleware: redirect to login page if user not logged in and improved tests

--- tests/Http/Middleware/CheckPageAclTest.php
+++ tests/Http/Middleware/CheckPageAclTest.php
@@ -2,10 +2,13 @@
 
 namespace BoomCMS\Tests\Http\Middleware;
 
+use BoomCMS\Contracts\Models\Page;
 use BoomCMS\Http\Middleware\CheckPageAcl;
 use BoomCMS\Routing\Router;
 use BoomCMS\Tests\AbstractTestCase;
 use Illuminate\Auth\Access\Gate;
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Mockery as m;
 
@@ -17,15 +20,26 @@ class CheckPageAclTest extends AbstractTestCase
     protected $gate;
 
     /**
+     * @var Guard
+     */
+    protected $guard;
+
+    /**
      * @var Router
      */
     protected $router;
+
+    /**
+     * @var Page
+     */
+    protected $page;
 
     public function setUp()
     {
         parent::setUp();
 
         $this->gate = m::mock(Gate::class);
+        $this->guard = m::mock(Guard::class);
         $this->router = m::mock(Router::class);
         $this->page = $this->validPage();
 
@@ -33,11 +47,34 @@ class CheckPageAclTest extends AbstractTestCase
             ->shouldReceive('getActivePage')
             ->andReturn($this->page);
 
-        $this->middleware = new CheckPageAcl($this->router, $this->gate);
+        $this->page->setAclEnabled(true);
+
+        $this->middleware = new CheckPageAcl($this->router, $this->guard, $this->gate);
     }
 
-    public function testPageCannotBeViewed()
+    public function testRedirectToLoginIfGuest()
     {
+        $this->guard
+            ->shouldReceive('check')
+            ->andReturn(false);
+
+        $response = $this->middleware->handle(new Request(), function () {
+        });
+
+        $this->assertInstanceOf(RedirectResponse::class, $response);
+        $this->assertEquals(302, $response->status());
+        $this->assertEquals(route('login'), $response->getTargetUrl());
+    }
+
+    /**
+     * @expectedException Symfony\Component\HttpKernel\Exception\HttpException
+     */
+    public function testLoggedInButPageCannotBeViewed()
+    {
+        $this->guard
+            ->shouldReceive('check')
+            ->andReturn(true);
+
         $this->gate
             ->shouldReceive('denies')
             ->with('view', $this->page)
@@ -51,10 +88,37 @@ class CheckPageAclTest extends AbstractTestCase
     {
         $nextCalled = false;
 
+        $this->guard
+            ->shouldReceive('check')
+            ->andReturn(true);
+
         $this->gate
             ->shouldReceive('denies')
             ->with('view', $this->page)
             ->andReturn(false);
+
+        $closure = function () use (&$nextCalled) {
+            $nextCalled = true;
+        };
+
+        $this->middleware->handle(new Request(), $closure);
+
+        $this->assertTrue($nextCalled);
+    }
+
+    public function testAclDisabled()
+    {
+        $this->page->setAclEnabled(false);
+        $nextCalled = false;
+
+        $this->guard
+            ->shouldReceive('check')
+            ->never();
+
+        $this->gate
+            ->shouldReceive('denies')
+            ->with('view', $this->page)
+            ->never();
 
         $closure = function () use (&$nextCalled) {
             $nextCalled = true;
