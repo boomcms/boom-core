5e76a4df8db03ad76609a01a3f89de19d64bb13f
Rob Taylor <rob@uxblondon.com>
2016-12-12 17:51:41 +0000

    Added a ListsPages trait for querying pages

--- src/BoomCMS/Http/Controllers/Page/PageController.php
+++ src/BoomCMS/Http/Controllers/Page/PageController.php
@@ -7,14 +7,16 @@ use BoomCMS\Database\Models\Site;
 use BoomCMS\Http\Controllers\Controller;
 use BoomCMS\Jobs\CreatePage;
 use BoomCMS\Support\Facades\Page as PageFacade;
-use BoomCMS\Support\Helpers;
+use BoomCMS\Support\Traits\ListsPages;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
+    use ListsPages;
+
     public function getIndex(Request $request)
     {
-        return Helpers::getPages($request->input());
+        return $this->getPages($request->input());
     }
 
     /**
---
+++ src/BoomCMS/Support/Traits/ListsPages.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace BoomCMS\Support\Traits;
+
+use BoomCMS\Support\Facades\Helpers;
+use Illuminate\Database\Eloquent\Collection;
+
+trait ListsPages
+{
+
+    /**
+     * Returns a collection of pages based on the given criteria
+     *
+     * @param array $params
+     *
+     * @return Collection
+     */
+    public function getPages(array $params): Collection
+    {
+        return Helpers::getPages($params);
+    }
+}
