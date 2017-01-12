84b98b25cc56eb78a36968ded1c410e25d02d870
Rob Taylor <rob@uxblondon.com>
2017-01-12 14:51:22 +0000

    Library chunk: Fixed CSS issue in editor

--- public/js/cms.js
+++ public/js/cms.js
@@ -48796,7 +48796,7 @@ if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 }
 }());
 ;//! moment-timezone.js
-//! version : 0.5.10
+//! version : 0.5.11
 //! Copyright (c) JS Foundation and other contributors
 //! license : MIT
 //! github.com/moment/moment-timezone
@@ -48816,12 +48816,12 @@ if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
 	"use strict";
 
 	// Do not load moment-timezone a second time.
-	if (moment.tz !== undefined) {
-		logError('Moment Timezone ' + moment.tz.version + ' was already loaded ' + (moment.tz.dataVersion ? 'with data from ' : 'without any data') + moment.tz.dataVersion);
-		return moment;
-	}
+	// if (moment.tz !== undefined) {
+	// 	logError('Moment Timezone ' + moment.tz.version + ' was already loaded ' + (moment.tz.dataVersion ? 'with data from ' : 'without any data') + moment.tz.dataVersion);
+	// 	return moment;
+	// }
 
-	var VERSION = "0.5.10",
+	var VERSION = "0.5.11",
 		zones = {},
 		links = {},
 		names = {},
