f1c45ea2e3cdefbdc7c52f120bc76b5148c02a6d
Rob Taylor <rob@uxblondon.com>
2017-02-15 12:57:00 +0000

    CMS menu: Added asset upload to menu

--- src/lang/en/menu.php
+++ src/lang/en/menu.php
@@ -1,13 +1,14 @@
 <?php
 
 return [
-    'account'   => 'Manage account',
-    'assets'    => 'Asset manager',
-    'dashboard' => 'Dashboard',
-    'logout'    => 'Logout',
-    'pages'     => 'Pages',
-    'people'    => 'People manager',
-    'settings'  => 'Settings',
-    'templates' => 'Template manager',
-    'view-site' => 'View site',
+    'account'      => 'Manage account',
+    'assets'       => 'Asset manager',
+    'asset-upload' => 'Asset upload',
+    'dashboard'    => 'Dashboard',
+    'logout'       => 'Logout',
+    'pages'        => 'Pages',
+    'people'       => 'People manager',
+    'settings'     => 'Settings',
+    'templates'    => 'Template manager',
+    'view-site'    => 'View site',
 ];
