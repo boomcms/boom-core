c94362826f17da3de2493c189064faf588c8a12c
Rob Taylor <rob@uxblondon.com>
2017-07-18 16:47:28 +0100

    Routes: Match digits only for asset dimensions in route definition

--- src/routes.php
+++ src/routes.php
@@ -204,7 +204,9 @@ Route::group([
             return App::make(AssetHelper::controller($asset))->$action($width, $height);
         },
     ])->where([
-        'action'    => 'view|thumb|crop',
+        'action' => 'view|thumb|crop',
+        'width'  => '\d+',
+        'height' => '\d+',
     ]);
 });
 
