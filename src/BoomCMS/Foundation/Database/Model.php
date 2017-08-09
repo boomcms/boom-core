d64c59c83f686d48a78b5e290792c7484d1ed174
Rob Taylor <rob@uxblondon.com>
2017-08-09 17:03:25 +0100

    Added the macroable trait to model classes

--- src/BoomCMS/Foundation/Database/Model.php
+++ src/BoomCMS/Foundation/Database/Model.php
@@ -3,9 +3,12 @@
 namespace BoomCMS\Foundation\Database;
 
 use Illuminate\Database\Eloquent\Model as BaseModel;
+use Illuminate\Support\Traits\Macroable;
 
 abstract class Model extends BaseModel
 {
+    use Macroable;
+
     const ATTR_ID = 'id';
 
     public $guarded = [
