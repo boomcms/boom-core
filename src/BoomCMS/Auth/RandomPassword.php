55243db38f930d23b0ff993b60536d6ab690e3d7
Rob Taylor <rob@uxblondon.com>
2017-08-21 12:44:05 +0100

    Updated random password generator to use only EFF Diceware list
    Fixes potential for offensive phrases generated by the default English list

--- src/BoomCMS/Auth/RandomPassword.php
+++ src/BoomCMS/Auth/RandomPassword.php
@@ -2,25 +2,65 @@
 
 namespace BoomCMS\Auth;
 
+use GenPhrase\Loader;
+use GenPhrase\Password;
+
+/**
+ * Wrapper around GenPhrase password generator
+ *
+ * Uses the EFF Diceware list to generate a six word password
+ *
+ * @see https://github.com/timoh6/GenPhrase
+ * @see https://www.eff.org/deeplinks/2016/07/new-wordlists-random-passphrases
+ */
 class RandomPassword
 {
+    /**
+     * @var type Password
+     */
+    protected $generator;
+
     protected $password;
 
     public function __construct()
     {
-        $loader = new \GenPhrase\Loader('GenPhrase');
+        $loader = new Loader('GenPhrase');
         $loader->register();
 
-        $gen = new \GenPhrase\Password();
-        $this->password = $gen->generate();
+        $this->generator = new Password();
+        $this->generator->removeWordlist('default');
+        $this->generator->disableSeparators(true);
+        $this->generator->disableWordModifier(true);
+
+        $this->addWordList(base_path('vendor/genphrase/genphrase/library/GenPhrase/Wordlists/diceware.lst'), 'diceware');
     }
 
-    public function getPassword()
+    /**
+     * Add a word list to the generator
+     *
+     * @param string $path
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function addWordList(string $path, $name): self
     {
-        return $this->password;
+        $this->generator->addWordList($path, $name);
+
+        return $this;
     }
 
-    public function __toString()
+    /**
+     * Returns a password with 65 bits of entropy (6 words)
+     *
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->generator->generate(65);
+    }
+
+    public function __toString(): string
     {
         return $this->getPassword();
     }
--- tests/Auth/RandomPasswordTest.php
+++ tests/Auth/RandomPasswordTest.php
@@ -7,17 +7,26 @@ use BoomCMS\Tests\AbstractTestCase;
 
 class RandomPasswordTest extends AbstractTestCase
 {
+    /**
+     * @var RandomPassword
+     */
+    protected $password;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->password = new RandomPassword();
+        $this->password->addWordList(realpath(__DIR__.'/../../vendor/genphrase/genphrase/library/GenPhrase/Wordlists/diceware.lst'), 'diceware');
+    }
+
     public function testGetPasswordReturnsString()
     {
-        $password = new RandomPassword();
-
-        $this->assertInternalType('string', $password->getPassword());
+        $this->assertInternalType('string', $this->password->getPassword());
     }
 
     public function testGetPasswordAsString()
     {
-        $password = new RandomPassword();
-
-        $this->assertInternalType('string', (string) $password);
+        $this->assertInternalType('string', (string) $this->password);
     }
 }
