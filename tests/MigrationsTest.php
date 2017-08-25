93793119197021ec16a8b6346c7a38d09a7ce9a3
Rob Taylor <rob@uxblondon.com>
2017-08-25 14:54:19 +0100

    Another attemp to fix tests

--- tests/MigrationsTest.php
+++ tests/MigrationsTest.php
@@ -15,6 +15,7 @@ class MigrationsTest extends AbstractTestCase
         $pdo = new PDO(env('DB_DRIVER').':host='.env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'));
         $pdo->exec('drop database if exists '.env('DB_DATABASE'));
         $pdo->exec('create database '.env('DB_DATABASE'));
+        $pdo->exec('set global innodb_large_prefix = `ON`;');
 
         $app = App::getFacadeRoot();
         $app['migration.repository']->createRepository();
