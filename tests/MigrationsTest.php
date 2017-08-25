69ae2b829de2a0c6cec845bfdfa3fddb2f633ca8
Rob Taylor <rob@uxblondon.com>
2017-08-25 14:48:59 +0100

    Attempting to fix migration which only fails on Travis CI

--- tests/MigrationsTest.php
+++ tests/MigrationsTest.php
@@ -3,12 +3,15 @@
 namespace BoomCMS\Tests;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
 use PDO;
 
 class MigrationsTest extends AbstractTestCase
 {
     public function testMigrationsComplete()
     {
+        Schema::defaultStringLength(2050);
+
         $pdo = new PDO(env('DB_DRIVER').':host='.env('DB_HOST'), env('DB_USERNAME'), env('DB_PASSWORD'));
         $pdo->exec('drop database if exists '.env('DB_DATABASE'));
         $pdo->exec('create database '.env('DB_DATABASE'));
