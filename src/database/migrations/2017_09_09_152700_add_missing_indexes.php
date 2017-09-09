55bc30d53b60ad39e65e238ce205e43cb1c82aca
Rob Taylor <rob@uxblondon.com>
2017-09-09 15:52:03 +0100

    Added missing DB indexes to improve asset manager performance

---
+++ src/database/migrations/2017_09_09_152700_add_missing_indexes.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+class AddMissingIndexes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('create index sites_default on sites(`default`)');
+        DB::statement('create index album_asset_asset_id on album_asset(asset_id)');
+        DB::statement('create index assets_created_at_asc on assets(created_at asc)');
+        DB::statement('create index assets_created_at_desc on assets(created_at desc)');
+        DB::statement('create index albums_deleted_at on albums(deleted_at)');
+        DB::statement('create index albums_name_asc_deleted_at on albums(name asc, deleted_at)');
+        DB::statement('alter table assets drop index asset_v_rid');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::statement('alter table sites drop index sites_default');
+        DB::statement('alter table album_asset drop index album_asset_asset_id');
+        DB::statement('alter table assets drop index assets_created_at_asc');
+        DB::statement('alter table assets drop index assets_created_at_desc');
+        DB::statement('alter table albums drop index albums_deleted_at');
+        DB::statement('alter table albums drop index albums_name_asc_deleted_at');
+    }
+}
