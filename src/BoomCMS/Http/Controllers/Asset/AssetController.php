fe90d866889abae7156bdbdb8d15760140b6cc4c
Rob Taylor <rob@uxblondon.com>
2017-09-10 19:57:50 +0100

    Fixed typo

--- src/BoomCMS/Http/Controllers/Asset/AssetController.php
+++ src/BoomCMS/Http/Controllers/Asset/AssetController.php
@@ -8,7 +8,7 @@ use BoomCMS\Foundation\Http\ValidatesAssetUpload;
 use BoomCMS\Http\Controllers\Controller;
 use BoomCMS\Support\Facades\Asset as AssetFacade;
 use BoomCMS\Support\Helpers;
-use Illuinate\Support\Facades\Response;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
