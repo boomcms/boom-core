7c8dad30a392bb63221e0498e05fc997e23d5033
Rob Taylor <rob@robbytaylor.co.uk>
2017-11-16 21:12:06 +0000

    Tests: Removed redundant tests for Asset helpers

--- tests/Support/Helpers/AssetTest.php
+++ tests/Support/Helpers/AssetTest.php
@@ -8,30 +8,6 @@ use BoomCMS\Tests\AbstractTestCase;
 
 class AssetTest extends AbstractTestCase
 {
-    protected $types;
-
-    public function setUp()
-    {
-        parent::setUp();
-
-        $config = include __DIR__.'/../../../src/config/boomcms/assets.php';
-        $this->types = $config['assets']['types'];
-    }
-
-    public function testTypes()
-    {
-        $this->AssertEquals($this->types, AssetHelper::types());
-    }
-
-    public function testTypeFromMimetype()
-    {
-        foreach ($this->types as $type => $mimetypes) {
-            foreach ($mimetypes as $mimetype) {
-                $this->assertEquals($type, AssetHelper::typeFromMimetype($mimetype));
-            }
-        }
-    }
-
     public function testController()
     {
         $namespace = 'BoomCMS\Http\Controllers\ViewAsset\\';
