392b572307277668933809365da8560701ac7c4c
Hasan Tareque <hmtareque@gmail.com>
2018-01-09 12:24:12 +0000

    bug fixed for multiple page excluding functionality of page query

--- src/BoomCMS/Page/Finder/IgnorePages.php
+++ src/BoomCMS/Page/Finder/IgnorePages.php
@@ -2,19 +2,25 @@
 
 namespace BoomCMS\Page\Finder;
 
+use BoomCMS\Foundation\Finder\Filter;
 use Illuminate\Database\Eloquent\Builder;
 
 /**
  * Set Page IDs to remove from results.
  */
-class IgnorePages extends PageId
+class IgnorePages extends Filter
 {
+    protected $pages;
+    
+    public function __construct($pages) {
+        $this->pages = $pages;
+    }
     public function build(Builder $query)
     {
-        if (is_array($this->pageId)) {
-            return $query->where('pages.id', 'not in', $this->pageId);
+        if (is_array($this->pages)) {
+            return $query->whereNotIn('pages.id', $this->pages);
         } else {
-            return $query->where('pages.id', '!=', $this->pageId);
+            return $query->where('pages.id', '!=', $this->pages);
         }
     }
 }
