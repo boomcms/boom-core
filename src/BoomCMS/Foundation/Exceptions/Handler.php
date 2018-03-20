462b51b98bbf14d400a9c91199750fa01600ef2e
Hasan Tareque <hasan@uxblondon.com>
2018-03-20 16:58:26 +0000

    handler app env updated

--- src/BoomCMS/Foundation/Exceptions/Handler.php
+++ src/BoomCMS/Foundation/Exceptions/Handler.php
@@ -52,7 +52,7 @@ class Handler extends ExceptionHandler
             }
         }
 
-        if(App::environment('local')) {
+        if(App::environment('production')) {
             if ($e instanceof FileNotFoundException || $e instanceof ModelNotFoundException) {
                 return response()->view('boomcms::errors.404', ['title' => '404']);
             }
