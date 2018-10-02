e9ab183415f54114f5d3b2c9874ce40e2a1ec4f3
Karl Mark Balagtey <karl.balagtey@gmail.com>
2018-10-02 13:08:18 +0100

    Added page status and buttons in page-manager

--- public/js/cms.js
+++ public/js/cms.js
@@ -51167,7 +51167,7 @@ if (!console) {
 }
 ;$.widget( 'boom.pageManager', {
     addActionButtons: function($li) {
-        $li.append('<div><a href=\'#\' class=\'fa fa-plus b-pages-add\'><span>Add page</span></a><a href=\'#\' class=\'fa fa-trash-o b-pages-delete\'><span>Delete page</span></a><a href=\'#\' class=\'fa fa-cog b-pages-settings\'><span>Settings</span></a><a href=\'#\' class=\'fa fa-eye b-pages-visibility\'><span>Visible</span></a></div>');
+        $li.append('<div><a href=\'#\' class=\'fa fa-plus b-pages-add\'><span>Add page</span></a><a href=\'#\' class=\'fa fa-trash-o b-pages-delete\'><span>Delete page</span></a><a href=\'#\' class=\'fa fa-cog b-pages-settings\'><span>Settings</span></a><a href=\'#\' class=\'fa fa-eye b-pages-visibility\'><span>Visible</span></a><a href=\'#\' class=\'fa fa-thumbs-o-up b-pages-version-status\'><span>Published</span></a></div>');
     },
 
     addPage: function($el) {
@@ -51195,6 +51195,7 @@ if (!console) {
                 add: function(e, $li) {
                     pageManager.addActionButtons($li);
                     pageManager.showVisibilityIcons($li);
+                    pageManager.showStatusIcons($li);
                 },
                 onPageSelect: function(link) {
                     window.open(link.getUrl());
@@ -51235,6 +51236,11 @@ if (!console) {
                 e.preventDefault();
 
                 pageManager.showVisibility($(this).closest('li'));
+            })
+            .on('click', '.b-pages-version-status', function(e) {
+                e.preventDefault();
+
+                pageManager.showPageStatus($(this).closest('li'));
             });
     },
 
@@ -51270,7 +51276,40 @@ if (!console) {
         }
     },
 
+    showStatusIcons: function($el) {
+        var page = $el.data('page');
+
+        $.get('/boomcms/editor/toolbar?page_id='+page.attributes.id, function(response, status) {
+            var status = $(response).find('#b-page-version-status').data('status'),
+                icon = $el.find('.b-pages-version-status');
+
+            switch (status) {
+                case 'draft':
+                    icon.removeClass('fa-thumbs-o-up');
+                    icon.addClass('fa-thumbs-o-down');
+                    icon.addClass('text-pink');
+                    break;
+                case 'embargoed':
+                    icon.removeClass('fa-thumbs-o-up');
+                    icon.addClass('fa-clock-o');
+                    icon.addClass('text-pink');
+                    break;
+                case 'pending approval':
+                    icon.removeClass('fa-thumbs-o-up');
+                    icon.addClass('fa-product-hunt');
+                    icon.addClass('text-blue');
+                    break;
+                default:
+                    icon.addClass('fa-thumbs-o-up');
+            }
+        });
+    },
+
     showVisibility: function($el) {
         this.showPageSettings($el, 'visibility');
+    },
+
+    showPageStatus: function($el) {
+        this.showPageSettings($el, 'drafts');
     }
 });
