1b6aa8310f71dee82c7340c381eb2e790c593c07
Hasan Tareque <hmtareque@gmail.com>
2020-06-02 15:16:22 +0100

    leaflet library updated

--- src/js/boomcms/chunk/location/editor.js
+++ src/js/boomcms/chunk/location/editor.js
@@ -1,7 +1,7 @@
-(function(BoomCMS) {
+(function (BoomCMS) {
     'use strict';
 
-    BoomCMS.ChunkLocationEditor = function(pageId, slotname, options) {
+    BoomCMS.ChunkLocationEditor = function (pageId, slotname, options) {
         this.pageId = pageId;
         this.slotname = slotname;
         this.deferred = new $.Deferred();
@@ -9,42 +9,47 @@
         this.options = options;
         this.title = 'Edit location';
 
-        BoomCMS.ChunkLocationEditor.prototype.bind = function() {
+        BoomCMS.ChunkLocationEditor.prototype.bind = function () {
             var locationEditor = this;
 
-            L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-                attribution: 'Wikimedia maps beta | Map data &copy; <a href="http://openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+            L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 15,
+                id: 'mapbox/streets-v11',
+                tileSize: 512,
+                zoomOffset: -1,
+                accessToken: 'pk.eyJ1IjoidXhibG9uZG9uIiwiYSI6ImNrYXh4MDRyazAzd28ycm1pYm84Z256cGQifQ.T41skOkuE0sxLyDRuuxorw',
             }).addTo(this.map);
 
             if (this.mapElement.attr('data-lat') != 0 && this.mapElement.attr('data-lng') != 0) {
                 this.setMapLocation(this.mapElement.attr('data-lat'), this.mapElement.attr('data-lng'));
             }
 
-            this.map.on('click', function(e) {
+            this.map.on('click', function (e) {
                 locationEditor.setMapLocation(e.latlng.lat, e.latlng.lng);
             });
 
             this.element
-                .on('click', '#b-location-set', function(e) {
+                .on('click', '#b-location-set', function (e) {
                     e.preventDefault();
 
                     locationEditor.setMapLocationFromAddress();
                 })
-                .on('click', '#b-location-latlng', function(e) {
+                .on('click', '#b-location-latlng', function (e) {
                     e.preventDefault();
 
                     locationEditor.setMapLocationFromLatLng();
                 })
-                .on('click', '#b-location-remove', function() {
+                .on('click', '#b-location-remove', function () {
                     locationEditor.removeLocation();
                 });
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.getAddress = function() {
+        BoomCMS.ChunkLocationEditor.prototype.getAddress = function () {
             return this.element.find('textarea').val();
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.getData = function() {
+        BoomCMS.ChunkLocationEditor.prototype.getData = function () {
             var latLng = this.getLocation();
 
             return {
@@ -55,23 +60,23 @@
             };
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.getDMS = function() {
+        BoomCMS.ChunkLocationEditor.prototype.getDMS = function () {
             return this.element.find('input[name=dms]').val();
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.getLocation = function() {
-            return (this.marker)? this.marker.getLatLng() : {lat: 0, lng: 0};
+        BoomCMS.ChunkLocationEditor.prototype.getLocation = function () {
+            return (this.marker) ? this.marker.getLatLng() : { lat: 0, lng: 0 };
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.getSearchAddress = function() {
+        BoomCMS.ChunkLocationEditor.prototype.getSearchAddress = function () {
             return this.element.find('input[name=search-address]').val();
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.getTitle = function() {
+        BoomCMS.ChunkLocationEditor.prototype.getTitle = function () {
             return this.element.find('input[name=title]').val();
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.geocode = function(location) {
+        BoomCMS.ChunkLocationEditor.prototype.geocode = function (location) {
             return $.get('//nominatim.openstreetmap.org/search', {
                 q: location,
                 format: 'json',
@@ -79,15 +84,15 @@
             });
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.open = function() {
+        BoomCMS.ChunkLocationEditor.prototype.open = function () {
             var locationEditor = this;
 
             this.dialog = new BoomCMS.Dialog({
-                url : '/boomcms/page/' + this.pageId + '/chunk/edit?slotname=' + this.slotname + '&type=location',
-                id : 'b-location-editor',
+                url: '/boomcms/page/' + this.pageId + '/chunk/edit?slotname=' + this.slotname + '&type=location',
+                id: 'b-location-editor',
                 width: 920,
                 title: this.title,
-                open: function() {
+                open: function () {
                     locationEditor.mapElement = locationEditor.dialog.contents.find('#b-location-map');
 
                     locationEditor.map = L.map(locationEditor.mapElement[0])
@@ -98,17 +103,17 @@
                     locationEditor.bind();
                 }
             })
-            .done(function() {
-                locationEditor.deferred.resolve(locationEditor.getData());
-            })
-            .fail(function() {
-                locationEditor.deferred.reject();
-            });
+                .done(function () {
+                    locationEditor.deferred.resolve(locationEditor.getData());
+                })
+                .fail(function () {
+                    locationEditor.deferred.reject();
+                });
 
             return this.deferred;
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.removeLocation = function() {
+        BoomCMS.ChunkLocationEditor.prototype.removeLocation = function () {
             if (this.marker) {
                 this.map
                     .removeLayer(this.marker)
@@ -120,27 +125,27 @@
             }
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.setMapLocation = function(lat, lng) {
+        BoomCMS.ChunkLocationEditor.prototype.setMapLocation = function (lat, lng) {
             var locationEditor = this,
                 marker;
 
-            L.Icon.Default.imagePath = '/vendor/boomcms/boom-core/images';
+            L.Icon.Default.imagePath = '/vendor/boomcms/boom-core/images/';
 
             if (!this.marker) {
                 marker = this.marker = L.marker([lat, lng], {
                     draggable: true
                 })
-                .addTo(this.map)
-                .on('dragend', function() {
-                    var latlng = marker.getLatLng();
+                    .addTo(this.map)
+                    .on('dragend', function () {
+                        var latlng = marker.getLatLng();
 
-                    locationEditor.setMapLocation(latlng.lat, latlng.lng);
-                });
+                        locationEditor.setMapLocation(latlng.lat, latlng.lng);
+                    });
             } else {
                 this.marker.setLatLng([lat, lng]);
             }
 
-            this.map.setView([lat, lng], 16);
+            this.map.setView([lat, lng], 13);
 
             this.element
                 .find('.b-lat input')
@@ -153,19 +158,19 @@
                 .show();
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.setMapLocationFromLatLng = function() {
+        BoomCMS.ChunkLocationEditor.prototype.setMapLocationFromLatLng = function () {
             var lat = this.element.find('.b-lat input').val(),
                 lng = this.element.find('.b-lng input').val();
 
             this.setMapLocation(Dms.parseDMS(lat), Dms.parseDMS(lng));
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.setMapLocationFromAddress = function() {
+        BoomCMS.ChunkLocationEditor.prototype.setMapLocationFromAddress = function () {
             var locationEditor = this,
                 address = this.getSearchAddress();
 
             this.geocode(address)
-                .done(function(response) {
+                .done(function (response) {
                     if (response.length) {
                         locationEditor.setMapLocation(response[0].lat, response[0].lon);
                     } else {
@@ -174,7 +179,7 @@
                 });
         };
 
-        BoomCMS.ChunkLocationEditor.prototype.toggleElements = function(options) {
+        BoomCMS.ChunkLocationEditor.prototype.toggleElements = function (options) {
             if (!options.title && !options.address) {
                 this.element.find('#b-location-details').hide();
                 return;
